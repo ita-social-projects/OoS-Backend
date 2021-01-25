e0acd66f9782b7e95807527b8d3c2fec765be10e
Bohdan-Khasanov <bohdan.khasanov@gmail.com>
2021-01-25 19:29:05 +0200

    Added simple tests

--- OutOfSchool/OutOfSchool.Tests/OrganizationControllerTest.cs
+++ OutOfSchool/OutOfSchool.Tests/OrganizationControllerTest.cs
@@ -1,9 +1,19 @@
+using Microsoft.Extensions.Logging;
+using Moq;
 using NUnit.Framework;
+using OutOfSchool.Controllers;
 
 namespace OutOfSchool.Tests
 {
     public class Tests
     {
+        private readonly ILogger<OrganizationController> _logger;
+        public OrganizationController CreateOrganizationController => new OrganizationController(_logger);
+
+        public Tests()
+        {
+            _logger = new Mock<ILogger<OrganizationController>>().Object;
+        }
         [SetUp]
         public void Setup()
         {
@@ -12,6 +22,7 @@ namespace OutOfSchool.Tests
         [Test]
         public void Test1()
         {
+            CreateOrganizationController.TestOk();
             Assert.Pass();
         }
     }
