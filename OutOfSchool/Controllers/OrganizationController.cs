346e437f6685296af55831c505ee6b8e3c0a04b0
Hensorskyy <vovchukgensorskyy@gmail.com>
2021-01-25 14:56:16 +0200

    Add Out of School skeleton project. Connect Swagger packages. Create an empty Organization controller (#8)

---
+++ OutOfSchool/Controllers/OrganizationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OutOfSchool.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly ILogger<OrganizationController> _logger;
+
+        public OrganizationController(ILogger<OrganizationController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
