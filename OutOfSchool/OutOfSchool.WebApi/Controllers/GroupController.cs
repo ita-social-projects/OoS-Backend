88f9366b419f6fcacb2b69942bce39a754b78cb2
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-02-04 14:03:08 +0200

    Create GroupController

---
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/GroupController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OutOfSchool.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    [Authorize]
+    public class GroupController
+    {
+        public Ш
+    }
+}
