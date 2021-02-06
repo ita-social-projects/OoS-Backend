2193870907cfb80cab8946006e085d7eb667f4dc
Bohdan-Khasanov <bohdan.khasanov@gmail.com>
2021-02-06 21:54:12 +0200

    Updated symbol hint for register viev model

--- OutOfSchool/IdentityServer/ViewModels/RegisterViewModel.cs
+++ OutOfSchool/IdentityServer/ViewModels/RegisterViewModel.cs
@@ -12,7 +12,7 @@ namespace IdentityServer.Controllers
         public string Username { get; set; }
         [Required(ErrorMessage = "Password is required")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-            ErrorMessage = "Password must contain at least one capital, number and symbol.")]
+            ErrorMessage = "Password must contain at least one capital, number and symbol(@$!%*?&).")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required(ErrorMessage = "Password confirmation is required")]
