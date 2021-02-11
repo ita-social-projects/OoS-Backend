7f2c3dc23ed37b0e0cfbad99d7d1ec10aa5075c7
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-02-11 15:10:25 +0200

    Add AddressDto

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ModelsDto/AddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OutOfSchool.WebApi.Models.ModelsDto
+{
+    public class AddressDto
+    {
+        public long Id { get; set; }
+        public string Region { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+    }
+}
