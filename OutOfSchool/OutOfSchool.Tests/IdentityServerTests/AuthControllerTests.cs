f59c731024e8b9bc0e2d2bf1eaa710e4c1fdecd3
Bohdan-Khasanov <bohdan.khasanov@gmail.com>
2021-02-12 13:42:07 +0200

    Preliminary auth controller tests

--- OutOfSchool/OutOfSchool.Tests/IdentityServerTests/AuthControllerTests.cs
+++ OutOfSchool/OutOfSchool.Tests/IdentityServerTests/AuthControllerTests.cs
@@ -1,12 +1,54 @@
-﻿using IdentityServer.Controllers;
+﻿using System;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
+using OutOfSchool.IdentityServer.Controllers;
+using OutOfSchool.Services.Models;
 
 namespace OutOfSchool.Tests.IdentityServerTests
 {
     [TestFixture]
     public class AuthControllerTests
     {
-        public AuthController CreateAuthControllerTests => new Aut;
+        private readonly Mock<FakeUserManager> _mockUserManager;
+        private readonly Mock<FakeSignInManager> _mockSignInManager;
+        private readonly Mock<IIdentityServerInteractionService> _mockInteractionService;
+        public AuthController CreateAuthController => new AuthController(_mockUserManager.Object, _mockSignInManager.Object, _mockInteractionService.Object);
 
+        public AuthControllerTests()
+        {
+            _mockUserManager = new Mock<FakeUserManager>();
+            _mockInteractionService = new Mock<IIdentityServerInteractionService>();
+            _mockSignInManager = new Mock<FakeSignInManager>();
+        }
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //[TestCase("", typeof(RedirectToActionResult))]
+        //[TestCase("True logout id", typeof(RedirectResult))]
+        [Test]
+        public async Task LogoutWithLogoutId()
+        {
+            // Arrange
+            var authController = CreateAuthController;
+            var logoutRequest = new Mock<LogoutRequest>();
+            logoutRequest.Setup(request => request.PostLogoutRedirectUri)
+                .Returns("PostLogoutUrl");
+            _mockSignInManager.Setup(manager => manager.SignOutAsync())
+                .Returns(Task.CompletedTask);
+            _mockInteractionService.Setup(service => service.GetLogoutContextAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(logoutRequest.Object));
+            // Act
+            var result =  await CreateAuthController.Logout("PostLogoutUrl");
+            
+            // Assert
+            Assert.IsInstanceOf(typeof(RedirectResult), result);
+        }   
     }
 }
---
+++ OutOfSchool/OutOfSchool.Tests/IdentityServerTests/FakeSignInManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.Tests.IdentityServerTests
+{
+    public class FakeSignInManager : SignInManager<User>
+    {
+        public FakeSignInManager()
+            : base(new Mock<FakeUserManager>().Object,
+                new HttpContextAccessor(),
+                new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<ILogger<SignInManager<User>>>().Object,
+                new Mock<IAuthenticationSchemeProvider>().Object,
+                new Mock<IUserConfirmation<User>>().Object
+                )
+        { }
+    }
+}
