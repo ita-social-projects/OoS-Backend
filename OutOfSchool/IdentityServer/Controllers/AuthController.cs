a4fe4762e1e796cea3a5be8c346a800ba2b93f78
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2021-02-12 14:06:33 +0200

    Added notimplemented exceptions

--- OutOfSchool/IdentityServer/Controllers/AuthController.cs
+++ OutOfSchool/IdentityServer/Controllers/AuthController.cs
@@ -172,5 +172,10 @@ namespace OutOfSchool.IdentityServer.Controllers
         {
             throw new NotImplementedException();
         }
+        
+        public Task<IActionResult> ExternalLogin(string provider, string returnUrl)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
