6f08f895c7c045be52f2e8223ef8bab4d933a68f
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-02-14 21:33:13 +0200

    Remove unused namespaces from ChildDTO

--- OutOfSchool/OutOfSchool.WebApi/Models/ModelsDto/ChildDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ModelsDto/ChildDTO.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace OutOfSchool.WebApi.Models
+namespace OutOfSchool.WebApi.Models.ModelsDto
 {
     public class ChildDTO
     {
@@ -12,7 +9,7 @@ namespace OutOfSchool.WebApi.Models
         public string LastName { get; set; }
         public string MiddleName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public int ParentId { get; set; }
-        public int SocialGroupId { get; set; }
+        public long ParentId { get; set; }
+        public long SocialGroupId { get; set; }
     }
 }
