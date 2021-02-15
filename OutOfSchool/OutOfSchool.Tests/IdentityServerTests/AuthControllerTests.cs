87cc635db13ecf6ef146384a85093605149bbdd3
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2021-02-15 11:55:55 +0200

    Added test skeleton for login

--- OutOfSchool/OutOfSchool.Tests/IdentityServerTests/AuthControllerTests.cs
+++ OutOfSchool/OutOfSchool.Tests/IdentityServerTests/AuthControllerTests.cs
@@ -6,9 +6,11 @@ using IdentityServer4.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Moq;
 using NUnit.Framework;
 using OutOfSchool.IdentityServer.Controllers;
+using OutOfSchool.IdentityServer.ViewModels;
 using OutOfSchool.Services.Models;
 
 namespace OutOfSchool.Tests.IdentityServerTests
@@ -16,20 +18,20 @@ namespace OutOfSchool.Tests.IdentityServerTests
     [TestFixture]
     public class AuthControllerTests
     {
-        private readonly Mock<FakeUserManager> _mockUserManager;
-        private readonly Mock<FakeSignInManager> _mockSignInManager;
-        private readonly Mock<IIdentityServerInteractionService> _mockInteractionService;
-        private readonly Mock<RoleManager<IdentityRole>> _mockRoleManager;
+        private readonly Mock<FakeUserManager> mockUserManager;
+        private readonly Mock<FakeSignInManager> mockSignInManager;
+        private readonly Mock<IIdentityServerInteractionService> mockInteractionService;
+        private readonly Mock<RoleManager<IdentityRole>> mockRoleManager;
 
-        public AuthController CreateAuthController => new AuthController(_mockUserManager.Object,
-            _mockSignInManager.Object, _mockRoleManager.Object, _mockInteractionService.Object);
+        public AuthController CreateAuthController => new AuthController(mockUserManager.Object,
+            mockSignInManager.Object, mockRoleManager.Object, mockInteractionService.Object);
 
         public AuthControllerTests()
         {
-            _mockRoleManager = GetMockRoleManager();
-            _mockUserManager = new Mock<FakeUserManager>();
-            _mockInteractionService = new Mock<IIdentityServerInteractionService>();
-            _mockSignInManager = new Mock<FakeSignInManager>();
+            mockRoleManager = GetMockRoleManager();
+            mockUserManager = new Mock<FakeUserManager>();
+            mockInteractionService = new Mock<IIdentityServerInteractionService>();
+            mockSignInManager = new Mock<FakeSignInManager>();
         }
 
         [SetUp]
@@ -44,9 +46,9 @@ namespace OutOfSchool.Tests.IdentityServerTests
             var authController = CreateAuthController;
             var logoutRequest = new LogoutRequest("iFrameUrl", new LogoutMessage());
             logoutRequest.PostLogoutRedirectUri = "True logout id";
-            _mockSignInManager.Setup(manager => manager.SignOutAsync())
+            mockSignInManager.Setup(manager => manager.SignOutAsync())
                 .Returns(Task.CompletedTask);
-            _mockInteractionService.Setup(service => service.GetLogoutContextAsync(It.IsAny<string>()))
+            mockInteractionService.Setup(service => service.GetLogoutContextAsync(It.IsAny<string>()))
                 .Returns(Task.FromResult(logoutRequest));
             // Act
             var result = await CreateAuthController.Logout("Any logout ID");
@@ -62,9 +64,9 @@ namespace OutOfSchool.Tests.IdentityServerTests
             var authController = CreateAuthController;
             var logoutRequest = new LogoutRequest("iFrameUrl", new LogoutMessage());
             logoutRequest.PostLogoutRedirectUri = "";
-            _mockSignInManager.Setup(manager => manager.SignOutAsync())
+            mockSignInManager.Setup(manager => manager.SignOutAsync())
                 .Returns(Task.CompletedTask);
-            _mockInteractionService.Setup(service => service.GetLogoutContextAsync(It.IsAny<string>()))
+            mockInteractionService.Setup(service => service.GetLogoutContextAsync(It.IsAny<string>()))
                 .Returns(Task.FromResult(logoutRequest));
             // Act
 
@@ -73,22 +75,59 @@ namespace OutOfSchool.Tests.IdentityServerTests
                 () => CreateAuthController.Logout("Any logout ID"));
         }
 
-        [Test]
-        public async Task LoginWithReturnUrl()
+        [TestCase("")]
+        [TestCase("Return url")]
+        public async Task LoginWithReturnUrl(string returnUrl)
         {
             // Arrange
             var authController = CreateAuthController;
             var logoutRequest = new LogoutRequest("iFrameUrl", new LogoutMessage());
-            var authenticationSchemes = new List<AuthenticationScheme>();
             logoutRequest.PostLogoutRedirectUri = "";
-            _mockSignInManager.Setup(manager => manager.GetExternalAuthenticationSchemesAsync())
-                .Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(authenticationSchemes));
+            // mockSignInManager.Setup(manager => manager.GetExternalAuthenticationSchemesAsync())
+            //     .Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(new List<AuthenticationScheme>()
+            //         {new AuthenticationScheme("Scheme", "Display type", typeof(HandleRequestContext<>))}));
+
             // Act
-            var viewResult = await authController.Login("Return url");
+            var viewResult = await authController.Login(returnUrl);
+
             // Assert
             Assert.IsInstanceOf(typeof(ViewResult), viewResult);
         }
-        
+
+        [TestCaseSource(nameof(GetViewModels))]
+        public async Task LoginWithLoginVMWithModelError(LoginViewModel loginViewModel)
+        {
+            // Arrange
+            var authController = CreateAuthController;
+            authController.ModelState.AddModelError(string.Empty, "Error");
+            // mockSignInManager.Setup(manager => manager.GetExternalAuthenticationSchemesAsync())
+            //     .Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(new List<AuthenticationScheme>()));
+            // Act
+            var externalAuthenticationSchemesAsync = await mockSignInManager.Object.GetExternalAuthenticationSchemesAsync();
+            var result = await authController.Login(loginViewModel);
+            // Assert
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        public static IEnumerable<TestCaseData> GetViewModels
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new LoginViewModel {ReturnUrl = "Return url", ExternalProviders = GetSchemes});
+                yield return new TestCaseData(
+                    new LoginViewModel {ExternalProviders = GetSchemes});
+                yield return new TestCaseData(
+                    new LoginViewModel {ReturnUrl = "Return url"});
+                yield return new TestCaseData(
+                    new LoginViewModel());
+            }
+        }
+
+        public static IEnumerable<AuthenticationScheme> GetSchemes
+        {
+            get { return new List<AuthenticationScheme>(); }
+        }
 
         public static Mock<RoleManager<IdentityRole>> GetMockRoleManager()
         {
