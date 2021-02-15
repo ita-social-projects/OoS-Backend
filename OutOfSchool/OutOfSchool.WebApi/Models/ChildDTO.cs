3361dfaa8d536ad63868ee9220ab6f4449ceac96
Elizabeth129 <45245513+Elizabeth129@users.noreply.github.com>
2021-02-15 12:22:12 +0200

    Add data annotations to Child and SocialGroup models

--- OutOfSchool/OutOfSchool.WebApi/Models/ChildDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ChildDTO.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OutOfSchool.WebApi.Models
 {
--- OutOfSchool/OutOfSchool.WebApi/Models/SocialGroupDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/SocialGroupDTO.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OutOfSchool.WebApi.Models
 {
