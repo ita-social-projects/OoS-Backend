dd93568b8ffa129870185754cb04b1208b7203f6
Elizabeth129 <45245513+Elizabeth129@users.noreply.github.com>
2021-02-17 14:15:19 +0200

    Change method definition in EntityRepository

--- OutOfSchool/OutOfSchool.DataAccess/Repository/IEntityRepository.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IEntityRepository.cs
@@ -56,11 +56,11 @@ namespace OutOfSchool.Services.Repository
         Task<T> GetById(long id);
 
         /// <summary>
-        ///Get element by id with details.
+        /// Get element by id with details.
         /// </summary>
         /// <param name="predicate">Filter with key.</param>
         /// <param name="includeProperties">Name of properties which should be included.</param>
         /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
-        Task<T> GetByIdWIthDetails(Expression<Func<T, bool>> predicate, string includeProperties = "");
+        Task<IEnumerable<T>> GetAllWIthDetails(Expression<Func<T, bool>> predicate, string includeProperties = "");
     }
 }
