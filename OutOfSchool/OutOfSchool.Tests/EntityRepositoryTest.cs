5f2be038f998a871cdcad0655e150723af10ba8c
Elizabeth129 <45245513+Elizabeth129@users.noreply.github.com>
2021-02-24 11:04:30 +0200

    Add UnitTestHelper class with initialization InMemory database

--- OutOfSchool/OutOfSchool.Tests/EntityRepositoryTest.cs
+++ OutOfSchool/OutOfSchool.Tests/EntityRepositoryTest.cs
@@ -21,25 +21,9 @@ namespace OutOfSchool.Tests
         [TestCase(1)]
         public void GetAllWIthDetails_FilterWithId_ReturnSingleEntity(long id)
         {
-            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
-            var options = new DbContextOptionsBuilder<OutOfSchoolDbContext>()
-             .UseInMemoryDatabase(databaseName: myDatabaseName)
-             .Options;
 
-
-            using (var context = new OutOfSchoolDbContext(options))
+            using (var context = new OutOfSchoolDbContext(UnitTestHelper.GetUnitTestDbOptions()))
             {
-                context.SocialGroups.Add(new SocialGroup { SocialGroupId = 1, Name = "sg1" });
-                context.SocialGroups.Add(new SocialGroup { SocialGroupId = 2, Name = "sg2" });
-                context.SocialGroups.Add(new SocialGroup { SocialGroupId = 3, Name = "sg3" });
-                context.Parents.Add(new Parent { ParentId = 1, FirstName = "fn1", LastName = "ln1" });
-                context.Parents.Add(new Parent { ParentId = 2, FirstName = "fn2", LastName = "ln2" });
-                context.Parents.Add(new Parent { ParentId = 3, FirstName = "fn3", LastName = "ln3" });
-                context.Children.Add(new Child { ChildId = 1, FirstName = "fn1", LastName = "ln1", MiddleName = "mn1", DateOfBirth = new DateTime(2003, 11, 9), Gender = Gender.Male, ParentId = 1, SocialGroupId = 2 });
-                context.Children.Add(new Child { ChildId = 2, FirstName = "fn2", LastName = "ln2", MiddleName = "mn2", DateOfBirth = new DateTime(2004, 11, 8), Gender = Gender.Female, ParentId = 2, SocialGroupId = 1 });
-                context.Children.Add(new Child { ChildId = 3, FirstName = "fn3", LastName = "ln3", MiddleName = "mn3", DateOfBirth = new DateTime(2006, 11, 2), Gender = Gender.Male, ParentId = 1, SocialGroupId = 1 });
-                context.SaveChanges();
-
 
                 var repository = new EntityRepository<Child>(context);
                 Expression<Func<Child, bool>> filter = child => child.ChildId == id;
---
+++ OutOfSchool/OutOfSchool.Tests/UnitTestHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OutOfSchool.Services;
+using OutOfSchool.Services.Enums;
+using OutOfSchool.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutOfSchool.Tests
+{
+    public static class UnitTestHelper
+    {
+        public static DbContextOptions<OutOfSchoolDbContext> GetUnitTestDbOptions()
+        {
+            var options = new DbContextOptionsBuilder<OutOfSchoolDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using (var context = new OutOfSchoolDbContext(options))
+            {
+                SeedData(context);
+            }
+            return options;
+        }
+
+        public static void SeedData(OutOfSchoolDbContext context)
+        {
+            context.SocialGroups.Add(new SocialGroup { SocialGroupId = 1, Name = "sg1" });
+            context.SocialGroups.Add(new SocialGroup { SocialGroupId = 2, Name = "sg2" });
+            context.SocialGroups.Add(new SocialGroup { SocialGroupId = 3, Name = "sg3" });
+            context.Parents.Add(new Parent { ParentId = 1, FirstName = "fn1", LastName = "ln1" });
+            context.Parents.Add(new Parent { ParentId = 2, FirstName = "fn2", LastName = "ln2" });
+            context.Parents.Add(new Parent { ParentId = 3, FirstName = "fn3", LastName = "ln3" });
+            context.Children.Add(new Child { ChildId = 1, FirstName = "fn1", LastName = "ln1", MiddleName = "mn1", DateOfBirth = new DateTime(2003, 11, 9), Gender = Gender.Male, ParentId = 1, SocialGroupId = 2 });
+            context.Children.Add(new Child { ChildId = 2, FirstName = "fn2", LastName = "ln2", MiddleName = "mn2", DateOfBirth = new DateTime(2004, 11, 8), Gender = Gender.Female, ParentId = 2, SocialGroupId = 1 });
+            context.Children.Add(new Child { ChildId = 3, FirstName = "fn3", LastName = "ln3", MiddleName = "mn3", DateOfBirth = new DateTime(2006, 11, 2), Gender = Gender.Male, ParentId = 1, SocialGroupId = 1 });
+            context.SaveChanges();
+        }
+    }
+}
