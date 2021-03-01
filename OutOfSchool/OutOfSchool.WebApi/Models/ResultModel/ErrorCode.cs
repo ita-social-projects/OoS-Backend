6f8586f5ce94e60ddbe4bca879595c0d82cd8cfb
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-01 07:18:35 +0200

    add exceptions handlers files

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ResultModel/ErrorCode.cs
@@ -0,0 +1,13 @@
+﻿namespace OutOfSchool.WebApi.Models.ResultModel
+{
+    public enum ErrorCode
+    {
+        ValidationError,
+        Unauthorized,
+        InternalServerError,
+        NotFound,
+        UnprocessableEntity,
+        Conflict,
+        ForgotPasswordExpired
+    }
+}
