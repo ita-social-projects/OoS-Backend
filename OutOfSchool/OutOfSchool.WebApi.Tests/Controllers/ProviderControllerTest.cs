819b884957724e0c8d5f11aadcb90a8482d1fa37
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-03-02 20:11:21 +0200

    Rename OrganizationControllerTest to ProviderControllerTest

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Controllers/ProviderControllerTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using OutOfSchool.WebApi.Controllers;
+using OutOfSchool.WebApi.Services;
+
+namespace OutOfSchool.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private readonly ILogger<ProviderController> logger;
+        private IProviderService providerService;
+        public ProviderController CreateOrganizationController => new ProviderController(logger, providerService);
+
+        public Tests()
+        {
+            logger = new Mock<ILogger<ProviderController>>().Object;
+            providerService = new Mock<IProviderService>().Object;
+        }
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
