925ad72b9506dc839acfb93c239b01b1d72f7c1c
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-03 13:50:06 +0200

    regex validation added

--- OutOfSchool/OutOfSchool.WebApi/Models/ChildDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ChildDTO.cs
@@ -7,14 +7,31 @@ namespace OutOfSchool.WebApi.Models
     {
         public long Id { get; set; }
 
+        [Required(ErrorMessage = "First name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "First name cannot contains digits")]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Last name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "Last name cannot contains digits")]
         public string LastName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Patronymic name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "Patronymic name cannot contains digits")]
         public string Patronymic { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Birthday is required")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; } = default;
 
+        [Required(ErrorMessage = "Gender is required")] 
+        public Gender Gender { get; set; }
+        
         public long ParentId { get; set; } = default;
 
         public long SocialGroupId { get; set; } = default;
