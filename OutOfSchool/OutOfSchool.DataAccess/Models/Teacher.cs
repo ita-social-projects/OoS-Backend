6bbe5fe84cdb28409692969476dfed71ff84b7ac
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-03 13:50:37 +0200

    minor fixes

--- OutOfSchool/OutOfSchool.DataAccess/Models/Teacher.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Teacher.cs
@@ -5,8 +5,8 @@ namespace OutOfSchool.Services.Models
 {
     public class Teacher
     {
-        public long TeacherId { get; set; }
-
+        public long Id { get; set; }
+        
         [Required(ErrorMessage = "First name is required")]
         [DataType(DataType.Text)]
         [MaxLength(30)]
