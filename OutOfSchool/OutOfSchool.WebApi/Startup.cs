094c558cbcd2917986c5ada086e518c15a004b58
unknown <cheat.book20@gmail.com>
2021-03-11 14:50:53 +0200

    Adding localization

--- OutOfSchool/OutOfSchool.WebApi/Startup.cs
+++ OutOfSchool/OutOfSchool.WebApi/Startup.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,7 @@ namespace OutOfSchool.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.AddAuthentication("Bearer")
                 .AddIdentityServerAuthentication("Bearer", options =>
                 {
@@ -76,6 +79,21 @@ namespace OutOfSchool.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var supportedCultures = new[]
+           {
+                  new CultureInfo("en"),
+                  new CultureInfo("uk")
+            };
+
+            var requestLocalization = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            app.UseRequestLocalization(requestLocalization);
 
             app.UseCors("AllowAll");
 
