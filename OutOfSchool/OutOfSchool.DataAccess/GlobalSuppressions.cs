20c58aedeb5b5428ebe84a4062f3ea81620bd4e3
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-17 19:07:37 +0200

    Update GlobalSuppressions.cs

--- OutOfSchool/OutOfSchool.DataAccess/GlobalSuppressions.cs
+++ OutOfSchool/OutOfSchool.DataAccess/GlobalSuppressions.cs
@@ -10,3 +10,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Model is a simple self-documenting class.")]
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "We don't have to apply keyword this to every element.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "We don't have to use headers in the project")]
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1028:Code must not contain trailing whitespace", Justification = "New rule that requires too much work to implement")]
