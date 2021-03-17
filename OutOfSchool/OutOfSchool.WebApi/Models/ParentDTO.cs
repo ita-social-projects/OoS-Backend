3a1269fb1fd36ce086a7b32d8a371a7feb838246
unknown <cheat.book20@gmail.com>
2021-03-17 18:39:31 +0200

    Adding ParentDTO

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ParentDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.WebApi.Models
+{
+    public class ParentDTO
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "First name cannot contains digits")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Middle name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Middle name cannot contains digits")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Last name cannot contains digits")]
+        public string LastName { get; set; }
+
+        public long UserId { get; set; }
+    }
+}
