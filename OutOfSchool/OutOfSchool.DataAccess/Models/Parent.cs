5acbc068d4294564e39be38ee9e555dfb8a5e128
unknown <cheat.book20@gmail.com>
2021-03-17 18:37:37 +0200

    Changing domain model

--- OutOfSchool/OutOfSchool.DataAccess/Models/Parent.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Parent.cs
@@ -12,16 +12,28 @@ namespace OutOfSchool.Services.Models
 
         public long Id { get; set; }
 
-        [DataType(DataType.Text)]
         [Required(ErrorMessage = "First name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "First name cannot contains digits")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Middle name is required")]
         [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Middle name cannot contains digits")]
+        public string MiddleName { get; set; }
+
         [Required(ErrorMessage = "Last name is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Last name cannot contains digits")]
         public string LastName { get; set; }
 
         public virtual IReadOnlyCollection<Child> Children { get; set; }
 
-        public User User { get; set; }
+        public long UserId { get; set; }
+
+        public virtual User User { get; set; }
     }
 }
