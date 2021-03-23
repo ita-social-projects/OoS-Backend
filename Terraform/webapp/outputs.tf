2235c0eb3d38306d13c4f01ac863d022fe516d48
Dmytro M <dmytro.minochkin@gmail.com>
2021-03-23 16:24:56 +0200

    add app service module

---
+++ Terraform/webapp/outputs.tf
@@ -0,0 +1,19 @@
+output "service_name" {
+  value = azurerm_app_service.dev.name
+}
+
+output "credentials" {
+  value = azurerm_app_service.dev.site_credential
+}
+
+output "connection_string" {
+  value = azurerm_app_service.dev.connection_string
+}
+
+output "verification_id" {
+  value = azurerm_app_service.dev.custom_domain_verification_id
+}
+
+output "service_url" {
+  value = var.hostname != "none" ? "https://${var.hostname}" : "https://${azurerm_app_service.dev.default_site_hostname}"
+}
