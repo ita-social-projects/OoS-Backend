728c0a06b68096cb82d0c75de194456dbe2b1bde
unknown <cheat.book20@gmail.com>
2021-03-24 13:40:30 +0200

    Adding pagination helper class and interface

---
+++ OutOfSchool/OutOfSchool.WebApi/Util/IPaginationHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.WebApi.Util
+{
+    /// <summary>
+    /// Interface for pagination helper.
+    /// </summary>
+    /// <typeparam name="T">Entity wich list we want to paginate for.</typeparam>
+    public interface IPaginationHelper<T>
+    {
+        /// <summary>
+        /// Get general amount of pages with definite watchsize with specific filter or without it.
+        /// </summary>
+        /// <param name="watchSize">The amount of records on the one page.</param>
+        /// <param name="where">The filter that we want to apply to the recorrds list.</param>
+        /// <returns>The amount of pages.</returns>
+        Task<int> GetAmountOfPages(int watchSize, Expression<Func<T, bool>> where = null);
+
+        /// <summary>
+        /// Get the ordered, filtered records for the page.
+        /// </summary>
+        /// <typeparam name="TOrderKey">The type that we want to order list with.</typeparam>
+        /// <param name="pageNumber">The number of the page that we want to get.</param>
+        /// <param name="watchSize">The amount of records on the one page.</param>
+        /// <param name="includeProperties">What Properties we want to include to objects that we will receive.</param>
+        /// <param name="where">Filter.</param>
+        /// <param name="orderBy">Filter that defines by wich property we want to order by.</param>
+        /// <param name="ascending">Ascending or descending ordering.</param>
+        /// <returns>Ordered, filtered list of elements.</returns>
+        Task<List<T>> GetPageExpanded<TOrderKey>(int pageNumber, int watchSize, string includeProperties = "", Expression<Func<T, bool>> where = null, Expression<Func<T, TOrderKey>> orderBy = null, bool ascending = true);
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Util/PaginationHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Repository;
+
+namespace OutOfSchool.WebApi.Util
+{
+    /// <summary>
+    /// Helper for pagination.
+    /// </summary>
+    /// <typeparam name="T">Entity wich list we want to paginate for.</typeparam>
+    public class PaginationHelper<T> : IPaginationHelper<T>
+        where T : class, new()
+    {
+        private readonly IEntityRepository<T> repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaginationHelper{T}"/> class.
+        /// </summary>
+        /// <param name="entityRepository">Repository for entity wich list we want to paginate for.</param>
+        public PaginationHelper(IEntityRepository<T> entityRepository)
+        {
+            repository = entityRepository;
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> GetAmountOfPages(int watchSize, Expression<Func<T, bool>> where = null)
+        {
+            if (watchSize < 1)
+            {
+                throw new ArgumentException("Wrong limit", nameof(watchSize));
+            }
+
+            int listOfPages = await repository.Count(where).ConfigureAwait(false);
+            int amountOfPages = (int)Math.Ceiling((double)(listOfPages / watchSize));
+            return amountOfPages;
+        }
+
+        /// <inheritdoc/>
+        public async Task<List<T>> GetPageExpanded<TOrderKey>(int pageNumber, int watchSize, string includeProperties = "", Expression<Func<T, bool>> where = null, Expression<Func<T, TOrderKey>> orderBy = null, bool ascending = true)
+        {
+            if (watchSize < 1)
+            {
+                throw new ArgumentException("Wrong watchSize", nameof(watchSize));
+            }
+
+            int realAmount = await GetAmountOfPages(watchSize).ConfigureAwait(false);
+            if (realAmount < pageNumber)
+            {
+                throw new ArgumentException("Wrong page number", nameof(watchSize));
+            }
+
+            List<T> selectedPages = repository.Get<TOrderKey>(watchSize * (pageNumber - 1), watchSize, includeProperties, where, orderBy, ascending).ToList();
+            return selectedPages;
+        }
+    }
+}
