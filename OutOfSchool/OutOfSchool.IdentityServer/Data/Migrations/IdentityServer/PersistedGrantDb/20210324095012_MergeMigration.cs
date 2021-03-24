23f3e03f5a2e018b9eb5b3801a177bdae68b7ed5
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-24 13:29:25 +0200

    fix migrations

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/IdentityServer/PersistedGrantDb/20210324095012_MergeMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.IdentityServer.PersistedGrantDb
+{
+    public partial class MergeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
