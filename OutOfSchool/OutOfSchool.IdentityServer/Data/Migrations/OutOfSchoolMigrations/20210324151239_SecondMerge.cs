a385ba5dfb3e4d8f2271af272d81988faf75abad
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-26 11:39:04 +0200

    add new migrations

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20210324151239_SecondMerge.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class SecondMerge : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BuildingNumb",
+                table: "Addresses",
+                newName: "BuildingNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BuildingNumber",
+                table: "Addresses",
+                newName: "BuildingNumb");
+        }
+    }
+}
