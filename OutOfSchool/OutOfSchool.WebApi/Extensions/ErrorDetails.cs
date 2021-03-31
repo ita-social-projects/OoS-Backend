5a13e25c8e4c0e6c639b3d66aa56e728e64291b7
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-03-31 09:09:41 +0300

    Create ErrorDetails.cs

---
+++ OutOfSchool/OutOfSchool.WebApi/Extensions/ErrorDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace OutOfSchool.WebApi.Extensions
+{
+    internal class ErrorDetails
+    {
+        public ErrorDetails()
+        {
+        }
+
+        public string Message { get; set; }
+    }
+}
