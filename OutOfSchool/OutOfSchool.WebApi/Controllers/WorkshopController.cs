aa3e385d58048b5a3b87b5bd2470e2de686371fb
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-04-04 14:56:55 +0300

    Update WorkshopController.cs

--- OutOfSchool/OutOfSchool.WebApi/Controllers/WorkshopController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/WorkshopController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OutOfSchool.Services;
-using OutOfSchool.Services.Repository;
+using OutOfSchool.Services.ViewModels;
 using OutOfSchool.WebApi.Models;
 using OutOfSchool.WebApi.Services;
 
