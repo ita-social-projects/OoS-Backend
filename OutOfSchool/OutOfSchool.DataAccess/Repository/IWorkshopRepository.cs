22e79763f31a3a4efafec5ad675769003eb8ab25
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-04-04 14:57:04 +0300

    add WorkshopRepository

---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IWorkshopRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.ViewModels;
+
+namespace OutOfSchool.Services.Repository
+{
+    public interface IWorkshopRepository : IEntityRepository<Workshop>
+    {
+        Task<IEnumerable> Search(SearchViewModel searchModel);
+    }
+}
---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/WorkshopRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.ViewModels;
+
+namespace OutOfSchool.Services.Repository
+{
+    public class WorkshopRepository : EntityRepository<Workshop>, IWorkshopRepository
+    {
+        private readonly OutOfSchoolDbContext context;
+        
+        public WorkshopRepository(OutOfSchoolDbContext context)
+            : base(context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable> Search(SearchViewModel searchModel)
+        {
+            IEnumerable workshops = null;
+            
+            if (!string.IsNullOrEmpty(searchModel.Title.ToLower()))
+            {
+                workshops = await GetByFilter(w => w.Title.Contains(searchModel.Title.ToLower())).ConfigureAwait(false);
+            }
+
+            return workshops;
+        }
+    }
+}
