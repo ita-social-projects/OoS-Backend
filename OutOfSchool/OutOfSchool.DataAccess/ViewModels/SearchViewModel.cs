11745dfc472dc288e64e08e5705af3ac281ba394
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-04-04 14:58:21 +0300

    Create SearchViewModel.cs

---
+++ OutOfSchool/OutOfSchool.DataAccess/ViewModels/SearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OutOfSchool.Services.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string Title { get; set; }
+        public bool IsFree { get; set; }
+        public int Age { get; set; }
+    }
+}
