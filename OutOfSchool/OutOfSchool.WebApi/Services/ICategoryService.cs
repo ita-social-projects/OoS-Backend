589d5daa1b1e843dbe95bc2910854867e7e19903
unknown <cheat.book20@gmail.com>
2021-04-06 12:42:43 +0300

    adding Category Interface

---
+++ OutOfSchool/OutOfSchool.WebApi/Services/ICategoryService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OutOfSchool.WebApi.Models;
+
+namespace OutOfSchool.WebApi.Services
+{
+    /// <summary>
+    /// Defines interface for CRUD functionality for Category entity.
+    /// </summary>
+    public interface ICategoryService
+    {
+        /// <summary>
+        /// Add new Category to the DB.
+        /// </summary>
+        /// <param name="category">CategoryDTO element.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<CategoryDTO> Create(CategoryDTO category);
+
+        /// <summary>
+        /// Get all Category objects from DB.
+        /// </summary>
+        /// <returns>List of Category objects.</returns>
+        Task<IEnumerable<CategoryDTO>> GetAll();
+
+        /// <summary>
+        /// To recieve the Category object with define id.
+        /// </summary>
+        /// <param name="id">Key in the table.</param>
+        /// <returns>Category object.</returns>
+        Task<CategoryDTO> GetById(long id);
+
+        /// <summary>
+        /// To Update our object in DB.
+        /// </summary>
+        /// <param name="category">Category with new properties.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        Task<CategoryDTO> Update(CategoryDTO category);
+
+        /// <summary>
+        /// To delete the object from DB.
+        /// </summary>
+        /// <param name="id">Key in table.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        Task Delete(long id);
+    }
+}
