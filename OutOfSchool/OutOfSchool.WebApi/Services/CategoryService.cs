b5af2a6d555ada42a3ef551f366ef342b25710ac
unknown <cheat.book20@gmail.com>
2021-04-06 12:43:14 +0300

    Adding Category Service Implementation

---
+++ OutOfSchool/OutOfSchool.WebApi/Services/CategoryService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.Repository;
+using OutOfSchool.WebApi.Extensions;
+using OutOfSchool.WebApi.Models;
+using Serilog;
+
+namespace OutOfSchool.WebApi.Services
+{
+    /// <summary>
+    /// Service with business logic for CategoryController.
+    /// </summary>
+    public class CategoryService : ICategoryService
+    {
+        private readonly IEntityRepository<Category> repository;
+        private readonly ILogger logger;
+        private readonly IStringLocalizer<SharedResource> localizer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryService"/> class.
+        /// </summary>
+        /// <param name="entityRepository">Repository for some entity.</param>
+        /// <param name="logger">Logger.</param>
+        /// <param name="localizer">Localizer.</param>
+        public CategoryService(IEntityRepository<Category> entityRepository, ILogger logger, IStringLocalizer<SharedResource> localizer)
+        {
+            this.localizer = localizer;
+            this.repository = entityRepository;
+            this.logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public async Task<CategoryDTO> Create(CategoryDTO dto)
+        {
+            logger.Information("Category creating was started");
+
+            var category = dto.ToDomain();
+
+            var newCategory = await repository.Create(category).ConfigureAwait(false);
+
+            logger.Information("Parent created successfully.");
+
+            return newCategory.ToModel();
+        }
+
+        /// <inheritdoc/>
+        public async Task Delete(long id)
+        {
+            logger.Information("Categoty deleting was launched.");
+
+            var entity = new Category() { Id = id };
+
+            try
+            {
+                await repository.Delete(entity).ConfigureAwait(false);
+
+                logger.Information("Category succesfully deleted.");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                logger.Error("Deleting failed.There is no category in the Db with such an id.");
+                throw;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<CategoryDTO>> GetAll()
+        {
+            logger.Information("Process of getting all Categories started.");
+
+            var categories = await this.repository.GetAll().ConfigureAwait(false);
+
+            logger.Information(!categories.Any()
+                ? "Parent table is empty."
+                : "Successfully got all records from the Parent table.");
+
+            return categories.Select(parent => parent.ToModel()).ToList();
+        }
+
+        /// <inheritdoc/>
+        public async Task<CategoryDTO> GetById(long id)
+        {
+            logger.Information("Process of getting Category by id started.");
+
+            var category = await repository.GetById((int)id).ConfigureAwait(false);
+
+            if (category == null)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer["The id cannot be greater than number of table entities."]);
+            }
+
+            logger.Information($"Successfuly got a category with id = {id}.");
+
+            return category.ToModel();
+        }
+
+        /// <inheritdoc/>
+        public async Task<CategoryDTO> Update(CategoryDTO dto)
+        {
+            logger.Information("Category updating was launched.");
+
+            try
+            {
+                var category = await repository.Update(dto.ToDomain()).ConfigureAwait(false);
+
+                logger.Information("Parent succesfully updated.");
+
+                return category.ToModel();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                logger.Error("Updating failed. There is no parent in the Db with such an id.");
+                throw;
+            }
+        }
+    }
+}
