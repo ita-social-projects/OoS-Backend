581fcd1d3919c2d29deb409f0d68170476786097
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-04-19 00:12:14 +0300

    add birthcertificate table

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20210418194923_AddBirthCertificateTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class AddBirthCertificateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BirthCertificates",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    SvidSer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SvidNum = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SvidNumMD5 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SvidWho = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SvidDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BirthCertificates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BirthCertificates_Children_Id",
+                        column: x => x.Id,
+                        principalTable: "Children",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BirthCertificates");
+        }
+    }
+}
