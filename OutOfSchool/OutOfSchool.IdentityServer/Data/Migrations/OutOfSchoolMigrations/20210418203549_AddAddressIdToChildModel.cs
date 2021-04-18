28f8c04d7f11cdbaf96e3ed435e6497fc925734b
Mykhailo Polishchuk <mmpolishchuk@gmail.com>
2021-04-19 00:12:38 +0300

    add addressid to child model

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20210418203549_AddAddressIdToChildModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class AddAddressIdToChildModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AddressId",
+                table: "Children",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Children");
+        }
+    }
+}
