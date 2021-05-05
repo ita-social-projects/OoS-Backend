1e9ecd791c72bcf3f2fbe75c6186f5f87009319c
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-05 21:50:57 +0300

    Add Rating model and DTO

---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using OutOfSchool.Services.Enums;
+
+namespace OutOfSchool.Services.Models
+{
+    public class Rating
+    {
+        public long Id { get; set; }
+
+        public int Rate { get; set; } = default;
+
+        [Required]
+        public RatingType Type { get; set; }
+
+        [Required]
+        public long EntityId { get; set; }
+
+        [Required]
+        public virtual Parent Parent { get; set; }
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Models/RatingDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using OutOfSchool.Services.Enums;
+
+namespace OutOfSchool.WebApi.Models
+{
+    public class RatingDTO
+    {
+        public long Id { get; set; }
+
+        public int Rate { get; set; } = default;
+
+        [Required]
+        public RatingType Type { get; set; }
+
+        [Required]
+        public long EntityId { get; set; }
+
+        [Required]
+        public virtual ParentDTO Parent { get; set; }
+    }
+}
