58a14bfae9424c8d9b01761c6344cdb793afe703
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-06 15:58:22 +0300

    Add Rating controller

---
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/RatingController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using OutOfSchool.WebApi.Models;
+using OutOfSchool.WebApi.Services;
+
+namespace OutOfSchool.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class RatingController : ControllerBase
+    {
+        private readonly IRatingService service;
+        private readonly IStringLocalizer<SharedResource> localizer;
+
+        public RatingController(IRatingService ratingService, IStringLocalizer<SharedResource> localizer)
+        {
+            this.service = ratingService;
+            this.localizer = localizer;
+        }
+
+        [Authorize(Roles = "admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var ratings = await service.GetAll().ConfigureAwait(false);
+
+            if (!ratings.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(ratings);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer["The id cannot be less than 1."]);
+            }
+
+            return Ok(await service.GetById(id).ConfigureAwait(false));
+        }
+
+        [Authorize(Roles = "parent,admin")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost]
+        public async Task<IActionResult> Create(RatingDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var rating = await service.Create(dto).ConfigureAwait(false);
+
+            if (rating == null)
+            {
+                return BadRequest("Rating with such parameters already exists");
+            }
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = rating.Id, },
+                rating);
+        }
+
+        [Authorize(Roles = "parent,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut]
+        public async Task<IActionResult> Update(RatingDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var rating = await service.Update(dto).ConfigureAwait(false);
+
+            if (rating == null)
+            {
+                return BadRequest("Can't change Rating with such parameters");
+            }
+
+            return Ok(rating);
+        }
+    }
+}
