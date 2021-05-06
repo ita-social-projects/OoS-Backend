db9af99f13173de20434970d155e9a3ed0e042a5
unknown <cheat.book20@gmail.com>
2021-05-06 12:15:28 +0300

    Changing SubcategoryDTO model

--- OutOfSchool/OutOfSchool.WebApi/Models/SubcategoryDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/SubcategoryDTO.cs
@@ -1,10 +1,22 @@
-﻿namespace OutOfSchool.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.WebApi.Models
 {
     public class SubcategoryDTO
     {
         public long Id { get; set; }
 
-        public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Title is required")]
+        [DataType(DataType.Text)]
+        [MaxLength(60)]
+        [MinLength(1)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public long CategoryId { get; set; }
 
         public virtual CategoryDTO Category { get; set; }
     }
