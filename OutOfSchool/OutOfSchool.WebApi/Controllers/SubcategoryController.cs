227cce3a64c0ec354b28939fc93e9a917450616a
unknown <cheat.book20@gmail.com>
2021-05-06 14:13:59 +0300

    Adding SubcategoryController

---
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/SubcategoryController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using OutOfSchool.WebApi.Models;
+using OutOfSchool.WebApi.Services;
+
+namespace OutOfSchool.WebApi.Controllers
+{
+    /// <summary>
+    /// Controller with CRUD operations for subcategory entity.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class SubcategoryController : ControllerBase
+    {
+        private readonly ISubcategoryService service;
+        private readonly IStringLocalizer<SharedResource> localizer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubcategoryController"/> class.
+        /// Initialization of SubcategoryController.
+        /// </summary>
+        /// <param name="service">Service for SubcategoryCOntroller.</param>
+        /// <param name="localizer">Localizer.</param>
+        public SubcategoryController(ISubcategoryService service, IStringLocalizer<SharedResource> localizer)
+        {
+            this.localizer = localizer;
+            this.service = service;
+        }
+
+        /// <summary>
+        /// To get all Subcategories from DB.
+        /// </summary>
+        /// <returns>List of Subcategories.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<SubcategoryDTO>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await service.GetAll().ConfigureAwait(false);
+
+            if (!categories.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(categories);
+        }
+
+        /// <summary>
+        /// To recieve Subcategory with define id.
+        /// </summary>
+        /// <param name="id">Key in table.</param>
+        /// <returns>Subcategory with define id.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SubcategoryDTO))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(long id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer["The id cannot be less than 1."]);
+            }
+
+            return Ok(await service.GetById(id).ConfigureAwait(false));
+        }
+
+        /// <summary>
+        /// To get all Subcategories from DB with some CategoryId.
+        /// </summary>
+        /// <param name="id">The category Id.</param>
+        /// <returns>List of Categories.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<SubcategoryDTO>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetByCategoryId(long id)
+        {
+            try
+            {
+                var categories = await service.GetByCategoryId(id).ConfigureAwait(false);
+
+                if (!categories.Any())
+                {
+                    return NoContent();
+                }
+
+                return Ok(categories);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// To create new Subcategory and add to the DB.
+        /// </summary>
+        /// <param name="dto">SubcategoryDTO object that we want to add.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(SubcategoryDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                dto.Id = default;
+
+                var category = await service.Create(dto).ConfigureAwait(false);
+
+                return CreatedAtAction(
+                     nameof(GetById),
+                     new { id = category.Id, },
+                     category);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// To update Subcategory entity that already exists.
+        /// </summary>
+        /// <param name="categoryDTO">SubcategoryDTO object with new properties.</param>
+        /// <returns>Subcategory's key.</returns>
+        [Authorize(Roles = "admin")]
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SubcategoryDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Update(SubcategoryDTO categoryDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(await service.Update(categoryDTO).ConfigureAwait(false));
+        }
+
+        /// <summary>
+        /// Delete Subcategory entity from DB.
+        /// </summary>
+        /// <param name="id">The key in table.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        [Authorize(Roles = "admin")]
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Delete(long id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer["The id cannot be less than 1."]);
+            }
+
+            await service.Delete(id).ConfigureAwait(false);
+
+            return NoContent();
+        }
+    }
+}
