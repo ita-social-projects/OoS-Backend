26dee81e8be4f359fdea45174cbad7b2979231e7
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-08 20:31:18 +0300

    Add summaries, delete method, comments to Ratting controller

--- OutOfSchool/OutOfSchool.WebApi/Controllers/RatingController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/RatingController.cs
@@ -18,15 +18,25 @@ namespace OutOfSchool.WebApi.Controllers
         private readonly IRatingService service;
         private readonly IStringLocalizer<SharedResource> localizer;
 
-        public RatingController(IRatingService ratingService, IStringLocalizer<SharedResource> localizer)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RatingController"/> class.
+        /// </summary>
+        /// <param name="service">Service for Rating model.</param>
+        /// <param name="localizer">Localizer.</param>
+        public RatingController(IRatingService service, IStringLocalizer<SharedResource> localizer)
         {
-            this.service = ratingService;
+            this.service = service;
             this.localizer = localizer;
         }
 
+        /// <summary>
+        /// Get all ratings from the database.
+        /// </summary>
+        /// <returns>List of all ratings.</returns>
         [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -41,7 +51,14 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(ratings);
         }
 
+        /// <summary>
+        /// Get rating by it's id.
+        /// </summary>
+        /// <param name="id">Rating's id.</param>
+        /// <returns>Rating.</returns>
+        [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(long id)
@@ -56,23 +73,25 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(await service.GetById(id).ConfigureAwait(false));
         }
 
+        /// <summary>
+        /// Add a new rating to the database.
+        /// </summary>
+        /// <param name="dto">Rating entity to add.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         [Authorize(Roles = "parent,admin")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
         public async Task<IActionResult> Create(RatingDTO dto)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
             var rating = await service.Create(dto).ConfigureAwait(false);
 
             if (rating == null)
             {
-                return BadRequest("Rating with such parameters already exists");
+                return BadRequest("Can't create a Rating with such parameters!\n" +
+                    "Please check that entity, parent, type information are valid and don't exist in the system yet.");
             }
 
             return CreatedAtAction(
@@ -81,26 +100,52 @@ namespace OutOfSchool.WebApi.Controllers
                 rating);
         }
 
+        /// <summary>
+        /// Update info about a specific rating in the database.
+        /// </summary>
+        /// <param name="dto">Rating to update.</param>
+        /// <returns>Rating.</returns>
         [Authorize(Roles = "parent,admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPut]
         public async Task<IActionResult> Update(RatingDTO dto)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
             var rating = await service.Update(dto).ConfigureAwait(false);
 
             if (rating == null)
             {
-                return BadRequest("Can't change Rating with such parameters");
+                return BadRequest("Can't change Rating with such parameters\n" +
+                    "Please check that id, entity, parent, type information are valid and exist in the system.");
             }
 
             return Ok(rating);
+        }
+
+        /// <summary>
+        /// Delete a specific Rating entity from the database.
+        /// </summary>
+        /// <param name="id">Rating's id.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        [Authorize(Roles = "admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer["The id cannot be less than 1."]);
+            }
+
+            await service.Delete(id).ConfigureAwait(false);
+
+            return NoContent();
         }
     }
 }
