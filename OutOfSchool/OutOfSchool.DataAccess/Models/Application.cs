ec8d668e8b098a4bda886e8588b67387fcbe26e5
P0linux <pollykr2000@gmail.com>
2021-05-08 15:39:15 +0300

    added Application entity and ApplicationStatus enum

---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Application.cs
@@ -0,0 +1,26 @@
+﻿using OutOfSchool.Services.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutOfSchool.Services.Models
+{
+    public class Application
+    {
+        public long Id { get; set; }
+
+        public ApplicationStatus Status { get; set; }
+
+        public long WorkshopId { get; set; }
+
+        public long ChildId { get; set; }
+
+        public string UserId { get; set; }
+        
+        public virtual Workshop Workshop { get; set; }
+
+        public virtual Child Child { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
