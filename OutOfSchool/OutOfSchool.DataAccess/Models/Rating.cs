7983be8f43ebc34daa18852ccedbaa2ec6b97279
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-08 20:28:16 +0300

    Update Rating model and DTO with validation attributes

--- OutOfSchool/OutOfSchool.DataAccess/Models/Rating.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Rating.cs
@@ -11,12 +11,17 @@ namespace OutOfSchool.Services.Models
         public int Rate { get; set; } 
 
         [Required]
+        [Range(1, 2, ErrorMessage = "The type field should be 1 or 2")]
         public RatingType Type { get; set; }
 
         [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "The EntityId field should be bigger than 0")]
         public long EntityId { get; set; }
 
         [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "The ParentId field should be bigger than 0")]
+        public long ParentId { get; set; }
+
         public virtual Parent Parent { get; set; }
     }
 }
--- OutOfSchool/OutOfSchool.WebApi/Models/RatingDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/RatingDTO.cs
@@ -11,12 +11,15 @@ namespace OutOfSchool.WebApi.Models
         public int Rate { get; set; }
 
         [Required]
+        [Range(1, 2, ErrorMessage = "The type field should be 1 or 2")]
         public RatingType Type { get; set; }
 
         [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "The EntityId field should be bigger than 0")]
         public long EntityId { get; set; }
 
         [Required]
-        public virtual ParentDTO Parent { get; set; }
+        [Range(1, long.MaxValue, ErrorMessage = "The ParentId field should be bigger than 0")]
+        public long ParentId { get; set; }
     }
 }
