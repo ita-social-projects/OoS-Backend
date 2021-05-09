b12e1e5eb5c862b4b4b80c185cd44270ac4a7df3
P0linux <pollykr2000@gmail.com>
2021-05-09 17:27:30 +0300

    added ApplicationDTO

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ApplicationDTO.cs
@@ -0,0 +1,21 @@
+﻿using OutOfSchool.Services.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.WebApi.Models
+{
+    public class ApplicationDTO
+    {
+        public long Id { get; set; }
+
+        public ApplicationStatus Status { get; set; }
+
+        public long WorkshopId { get; set; }
+
+        public long ChildId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
