69695be622cbf6db0965af49c3b92864f5268889
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-11 12:12:43 +0300

    Remove Rating property in Provider and Workshop (migration)

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20210511090729_UpdateRatingAndWorkshop.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class UpdateRatingAndWorkshop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Workshops");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Providers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Workshops",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Providers",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
