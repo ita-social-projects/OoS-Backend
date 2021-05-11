6a487a342f826208db51146a15142d7d52e896b0
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-11 12:00:25 +0300

    Revert "Remove migrations done in this branch"
    This reverts commit 6c00e87b68504d4f58137ab50a3e6b6ea8dc8f45.

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20210505132605_updateProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class updateProvider : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Providers",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Providers");
+        }
+    }
+}
