4a86f6f66ceb2eb9fb5ad97d513ead26734ca71c
P0linux <pollykr2000@gmail.com>
2021-05-12 13:11:29 +0300

    updated Application model

--- OutOfSchool/OutOfSchool.WebApi/Models/ApplicationDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ApplicationDTO.cs
@@ -1,6 +1,7 @@
 ﻿using OutOfSchool.Services.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,8 +13,10 @@ namespace OutOfSchool.WebApi.Models
 
         public ApplicationStatus Status { get; set; } = ApplicationStatus.Pending;
 
+        [Range(1, long.MaxValue, ErrorMessage = "Workshop id should be grater than 0")]
         public long WorkshopId { get; set; }
 
+        [Range(1, long.MaxValue, ErrorMessage = "Child id should be grater than 0")]
         public long ChildId { get; set; }
 
         public string UserId { get; set; }
