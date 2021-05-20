c5157c52435563d603d107c11698ca445201b79f
Vitalii <v.ivanchuk1313@gmail.com>
2021-05-20 20:32:38 +0300

    Add Id validation

--- OutOfSchool/OutOfSchool.WebApi/Extensions/ControllerExtension.cs
+++ OutOfSchool/OutOfSchool.WebApi/Extensions/ControllerExtension.cs
@@ -14,6 +14,14 @@ namespace OutOfSchool.WebApi.Extensions
             return controller.User.Claims.FirstOrDefault(c => c.Type == claimName)?.Value;
         }
 
+        public static void IdValidation(this ControllerBase controller, long id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), "The Id cannot be less than 1.");
+            }
+        }
+
         private static void ExtensionValidation(ControllerBase controller)
         {
             if (controller == null)
