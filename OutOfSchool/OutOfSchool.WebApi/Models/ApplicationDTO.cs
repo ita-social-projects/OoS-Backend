d2fc9cc1a66c712f5290ed4cd64f0f2adafd9416
P0linux <pollykr2000@gmail.com>
2021-05-23 21:15:45 +0300

    updated Application model with CreationTime

--- OutOfSchool/OutOfSchool.WebApi/Models/ApplicationDTO.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ApplicationDTO.cs
@@ -10,6 +10,9 @@ namespace OutOfSchool.WebApi.Models
 
         public ApplicationStatus Status { get; set; } = ApplicationStatus.Pending;
 
+        [Required]
+        public DateTime CreationTime { get; set; }
+
         [Range(1, long.MaxValue, ErrorMessage = "Workshop id should be grater than 0")]
         public long WorkshopId { get; set; }
 
