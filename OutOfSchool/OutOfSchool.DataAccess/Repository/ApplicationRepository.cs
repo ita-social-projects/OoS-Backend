d1f74993571e4990277d500e9ef6e8229c9e0f27
P0linux <pollykr2000@gmail.com>
2021-05-25 11:39:58 +0300

    added Application repository

---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/ApplicationRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.Services.Repository
+{
+    public class ApplicationRepository : EntityRepository<Application>, IApplicationRepository
+    {
+        private readonly OutOfSchoolDbContext db;
+        private readonly DbSet<Application> dbSet;
+
+        public ApplicationRepository(OutOfSchoolDbContext dbContext)
+            : base(dbContext)
+        {
+            db = dbContext;
+            dbSet = db.Set<Application>();
+        }
+
+        public async Task<IEnumerable<Application>> Create(IEnumerable<Application> applications)
+        {
+            await dbSet.AddRangeAsync(applications);
+            await db.SaveChangesAsync();
+            return await Task.FromResult(applications);
+        }
+    }
+}
---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IApplicationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.Services.Repository
+{
+    public interface IApplicationRepository : IEntityRepository<Application>
+    {
+        Task<IEnumerable<Application>> Create(IEnumerable<Application> applications);
+    }
+}
