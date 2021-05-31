c603c3dedbae48bb75c42637d09c19d7866ce999
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-05-31 13:14:57 +0300

    Added new endpoint for User

--- OutOfSchool/OutOfSchool.WebApi/Controllers/UserController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OutOfSchool.WebApi.Models;
 using OutOfSchool.WebApi.Services;
 
 namespace OutOfSchool.WebApi.Controllers
@@ -58,6 +59,28 @@ namespace OutOfSchool.WebApi.Controllers
             {
                 return BadRequest(ex.Message);
             }                  
+        }
+
+        /// <summary>
+        /// Update info about the User.
+        /// </summary>
+        /// <param name="dto">Entity to update.</param>
+        /// <returns>Updated Provider.</returns>
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut]
+        public async Task<IActionResult> Update(UserDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(await userService.Update(dto).ConfigureAwait(false));
         }
     }
 }
--- OutOfSchool/OutOfSchool.WebApi/Services/IUserService.cs
+++ OutOfSchool/OutOfSchool.WebApi/Services/IUserService.cs
@@ -18,5 +18,12 @@ namespace OutOfSchool.WebApi.Services
         /// <param name="id">Key in the table.</param>
         /// <returns>User.</returns>
         Task<UserDto> GetById(string id);
+
+        /// <summary>
+        /// Update entity.
+        /// </summary>
+        /// <param name="dto">User entity to add.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<UserDto> Update(UserDto dto);
     }
 }
