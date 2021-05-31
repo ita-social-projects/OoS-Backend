d652d512a5011d76afd6501422b1796ec4495548
Sergey Novitsky <novitckij.s@gmail.com>
2021-05-31 14:13:34 +0300

    Added async method

--- OutOfSchool/OutOfSchool.EmailService/EmailService.cs
+++ OutOfSchool/OutOfSchool.EmailService/EmailService.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using MailKit.Net.Smtp;
 using MimeKit;
 using MimeKit.Text;
@@ -14,7 +13,17 @@ namespace OutOfSchool.EmailService
         {
             _emailConfiguration = emailConfiguration;
         }
-        public void Send (EmailMessage emailMessage)
+        public void Send(EmailMessage emailMessage)
+        {
+            var mimeMessage = CreateMimeMessage(emailMessage);
+            Send(mimeMessage);
+        }
+        public async Task SendAsync(EmailMessage emailMessage)
+        {
+            var mimeMessage = CreateMimeMessage(emailMessage);
+            await SendAsync(mimeMessage);
+        }
+        private MimeMessage CreateMimeMessage(EmailMessage emailMessage)
         {
             var message = new MimeMessage();
             message.To.AddRange(emailMessage.ToAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
@@ -24,13 +33,28 @@ namespace OutOfSchool.EmailService
             {
                 Text = emailMessage.Content
             };
+            return message;
+        }
+        public void Send(MimeMessage mimeMessage)
+        {
             using(var emailClient = new SmtpClient())
             {
                 emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, true);
                 emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
                 emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
-                emailClient.Send(message);
+                emailClient.Send(mimeMessage);
                 emailClient.Disconnect(true);
+            }
+        }
+        public async Task SendAsync(MimeMessage mimeMessage)
+        {
+            using (var emailClient = new SmtpClient())
+            {
+                await emailClient.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, true);
+                emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+                await emailClient.AuthenticateAsync(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+                await emailClient.SendAsync(mimeMessage);
+                await emailClient.DisconnectAsync(true);
             }
         }
     }
