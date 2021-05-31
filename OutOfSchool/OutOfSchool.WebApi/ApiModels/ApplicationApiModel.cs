270bdd1f2c136a887bb6aecee98b59cb8ea247f1
P0linux <pollykr2000@gmail.com>
2021-05-31 14:18:22 +0300

    added ApplicationApiModel to be used in Create method

---
+++ OutOfSchool/OutOfSchool.WebApi/ApiModels/ApplicationApiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Enums;
+using OutOfSchool.WebApi.Models;
+
+namespace OutOfSchool.WebApi.ApiModels
+{
+    public class ApplicationApiModel
+    {
+        public long WorkshopId { get; set; }
+
+        public IEnumerable<ChildDTO> Children { get; set; }
+    }
+}
