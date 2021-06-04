65989f95227bb8473804ce922a2f941bab54992f
P0linux <pollykr2000@gmail.com>
2021-06-04 14:24:02 +0300

    added tests for StatisticService

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/StatisticServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OutOfSchool.Services;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.Repository;
+using OutOfSchool.Tests;
+using OutOfSchool.WebApi.Extensions;
+using OutOfSchool.WebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.WebApi.Tests.Services
+{
+    [TestFixture]
+    public class StatisticServiceTest
+    {
+        private StatisticService service;
+        private IEntityRepository<Application> applicationRepository;
+        private IEntityRepository<Workshop> workshopRepository;
+        private OutOfSchoolDbContext context;
+
+        [SetUp]
+        public void SetUp()
+        {
+            context = new OutOfSchoolDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            applicationRepository = new EntityRepository<Application>(context);
+            workshopRepository = new EntityRepository<Workshop>(context);
+            service = new StatisticService(applicationRepository, workshopRepository);
+        }
+
+        [Test]
+        public async Task GetPopularWorkshops_ShouldReturnWorkshops()
+        {
+            // Arrange
+            var expected = context.Workshops;
+
+            // Act
+            var result = await service.GetPopularWorkshops(2).ConfigureAwait(false);
+
+            // Assert
+            result.Should().HaveCount(2);
+            result.Should().ContainEquivalentOf(expected.First().ToModel());
+        }
+
+        [Test]
+        public async Task GetPopularCategories_ShouldReturnCategories()
+        {
+            // Arrange
+            var expected = context.Categories;
+
+            // Act
+            var result = await service.GetPopularCategories(2).ConfigureAwait(false);
+
+            // Assert
+            result.Should().HaveCount(2);
+            result.Should().ContainEquivalentOf(expected.First().ToModel());
+        }
+    }
+}
--- OutOfSchool/OutOfSchool.WebApi.Tests/UnitTestHelper.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/UnitTestHelper.cs
@@ -13,6 +13,7 @@ namespace OutOfSchool.Tests
         {
             var options = new DbContextOptionsBuilder<OutOfSchoolDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .UseLazyLoadingProxies()
                 .Options;
 
             using (var context = new OutOfSchoolDbContext(options))
@@ -28,18 +29,30 @@ namespace OutOfSchool.Tests
             context.SocialGroups.Add(new SocialGroup { Id = 1, Name = "sg1" });
             context.SocialGroups.Add(new SocialGroup { Id = 2, Name = "sg2" });
             context.SocialGroups.Add(new SocialGroup { Id = 3, Name = "sg3" });
-            context.Parents.Add(new Parent { Id = 1, });
-            context.Parents.Add(new Parent { Id = 2, });
-            context.Parents.Add(new Parent { Id = 3, });
-            context.Children.Add(new Child { Id = 1, FirstName = "fn1", LastName = "ln1", MiddleName = "mn1", DateOfBirth = new DateTime(2003, 11, 9), Gender = Gender.Male, ParentId = 1, SocialGroupId = 2 });
-            context.Children.Add(new Child { Id = 2, FirstName = "fn2", LastName = "ln2", MiddleName = "mn2", DateOfBirth = new DateTime(2004, 11, 8), Gender = Gender.Female, ParentId = 2, SocialGroupId = 1 });
-            context.Children.Add(new Child { Id = 3, FirstName = "fn3", LastName = "ln3", MiddleName = "mn3", DateOfBirth = new DateTime(2006, 11, 2), Gender = Gender.Male, ParentId = 1, SocialGroupId = 1 });
+            context.Parents.Add(new Parent { Id = 1, FirstName = "fn1", LastName = "ln1" });
+            context.Parents.Add(new Parent { Id = 2, FirstName = "fn2", LastName = "ln2" });
+            context.Parents.Add(new Parent { Id = 3, FirstName = "fn3", LastName = "ln3" });
+            context.Children.Add(new Child { Id = 1, FirstName = "fn1", LastName = "ln1", Patronymic = "mn1", DateOfBirth = new DateTime(2003, 11, 9), Gender = Gender.Male, ParentId = 1, SocialGroupId = 2 });
+            context.Children.Add(new Child { Id = 2, FirstName = "fn2", LastName = "ln2", Patronymic = "mn2", DateOfBirth = new DateTime(2004, 11, 8), Gender = Gender.Female, ParentId = 2, SocialGroupId = 1 });
+            context.Children.Add(new Child { Id = 3, FirstName = "fn3", LastName = "ln3", Patronymic = "mn3", DateOfBirth = new DateTime(2006, 11, 2), Gender = Gender.Male, ParentId = 1, SocialGroupId = 1 });
 
-            context.Workshops.Add(new Workshop { Id = 1, Title = "w1" });
-            context.Workshops.Add(new Workshop { Id = 2, Title = "w2" });
+            context.Categories.Add(new Category { Id = 1, Title = "c1" });
+            context.Categories.Add(new Category { Id = 2, Title = "c2" });
 
-            context.Applications.Add(new Application() { Id = 1, ChildId = 1, Status = ApplicationStatus.Pending, WorkshopId = 1, ParentId = 1, CreationTime = new DateTime(2021, 7, 8), Child = context.Children.Find(1L), Parent = context.Parents.Find(1L), Workshop = context.Workshops.Find(1L) });
-            context.Applications.Add(new Application() { Id = 3, ChildId = 1, Status = ApplicationStatus.Pending, WorkshopId = 1, ParentId = 1, CreationTime = new DateTime(2021, 7, 7), Child = context.Children.Find(1L), Parent = context.Parents.Find(1L), Workshop = context.Workshops.Find(1L) });
+            context.Subcategories.Add(new Subcategory { Id = 1, Title = "sc1", CategoryId = 1 });
+            context.Subcategories.Add(new Subcategory { Id = 2, Title = "sc2", CategoryId = 2 });
+
+            context.Subsubcategories.Add(new Subsubcategory { Id = 1, Title = "ssc1", SubcategoryId = 1 });
+            context.Subsubcategories.Add(new Subsubcategory { Id = 2, Title = "ssc2", SubcategoryId = 1 });
+            context.Subsubcategories.Add(new Subsubcategory { Id = 3, Title = "ssc3", SubcategoryId = 2 });
+
+            context.Workshops.Add(new Workshop { Id = 1, Title = "w1", SubsubcategoryId = 1 });
+            context.Workshops.Add(new Workshop { Id = 2, Title = "w2", SubsubcategoryId = 2 });
+            context.Workshops.Add(new Workshop { Id = 3, Title = "w3", SubsubcategoryId = 3 });
+
+            context.Applications.Add(new Application() { Id = 1, ChildId = 1, Status = ApplicationStatus.Pending, WorkshopId = 1, UserId = "de909f35-5eb7-4b7a-bda8-40a5bfdaEEa6" });
+            context.Applications.Add(new Application() { Id = 3, ChildId = 1, Status = ApplicationStatus.Pending, WorkshopId = 1, UserId = "de909f35-5eb7-4b7a-bda8-40a5bfdaEEa6" });
+            context.Applications.Add(new Application() { Id = 10, ChildId = 1, Status = ApplicationStatus.Pending, WorkshopId = 3, UserId = "de909f35-5eb7-4b7a-bda8-40a5bfdaEEa6" });
 
             context.SaveChanges();
         }
