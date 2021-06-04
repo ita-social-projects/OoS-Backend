f152fadd22d7ae470703eca0d2eb4d76ee1200a4
Vitalii <v.ivanchuk1313@gmail.com>
2021-06-04 09:45:48 +0300

    Add initial data for Social Group

---
+++ OutOfSchool/OutOfSchool.DataAccess/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.Services.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        /// <summary>
+        /// Add initial data for Social Group.
+        /// </summary>
+        /// <param name="modelBuilder">Model Builder.</param>
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SocialGroup>().HasData(
+                new SocialGroup
+                {
+                    Id = 1,
+                    Name = "Діти із багатодітних сімей",
+                },
+                new SocialGroup
+                {
+                    Id = 2,
+                    Name = "Діти із малозабезпечених сімей",
+                },
+                new SocialGroup
+                {
+                    Id = 3,
+                    Name = "Діти з інвалідністю",
+                },
+                new SocialGroup
+                {
+                    Id = 4,
+                    Name = "Діти-сироти",
+                },
+                new SocialGroup
+                {
+                    Id = 5,
+                    Name = "Діти, позбавлені батьківського піклування",
+                });
+        }
+    }
+}
