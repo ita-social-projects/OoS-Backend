53b954e679a28b92f7030ef9e9c8919f2683f047
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-06-09 12:57:26 +0300

    Added logic of setting value to property IsRegistered in enitity User , by method Create. Fixed unit test.

--- OutOfSchool/OutOfSchool.DataAccess/Repository/ProviderRepository.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/ProviderRepository.cs
@@ -59,6 +59,11 @@ namespace OutOfSchool.Services.Repository
             await db.Providers.AddAsync(entity);
             await db.SaveChangesAsync();
 
+            var user = await db.Users.FirstOrDefaultAsync(x => x.Id == entity.UserId).ConfigureAwait(false);
+            user.IsRegistered = true;
+            db.Entry(user).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+
             return await Task.FromResult(entity);
         }
 
