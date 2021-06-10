5617b33d6145e2938c9d47a2fe047434d062513c
P0linux <pollykr2000@gmail.com>
2021-06-10 20:08:56 +0300

    code cleanup

--- OutOfSchool/OutOfSchool.WebApi/Controllers/StatisticController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/StatisticController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Localization;
 using OutOfSchool.WebApi.Models;
 using OutOfSchool.WebApi.Services;
 
