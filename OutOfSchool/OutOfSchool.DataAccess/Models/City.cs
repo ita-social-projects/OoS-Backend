949b9bed2f34ed7a5e4a594f25b5bdc965c993d4
Vitalii <v.ivanchuk1313@gmail.com>
2021-06-23 19:03:45 +0300

    Add City model and DTO

---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.Services.Models
+{
+    public class City
+    {
+        public long Id { get; set; }
+
+        public string Region { get; set; } = string.Empty;
+
+        public string District { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        public double Latitude { get; set; } = default;
+
+        public double Longitude { get; set; } = default;
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Models/CityDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.WebApi.Models
+{
+    public class CityDto
+    {
+        public long Id { get; set; }
+
+        public string Region { get; set; } = string.Empty;
+
+        public string District { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        public double Latitude { get; set; } = default;
+
+        public double Longitude { get; set; } = default;
+    }
+}
