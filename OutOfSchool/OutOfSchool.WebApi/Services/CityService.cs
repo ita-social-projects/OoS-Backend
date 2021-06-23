5bc839a5175cb11670bf33e65925539b07053283
Vitalii <v.ivanchuk1313@gmail.com>
2021-06-23 19:04:16 +0300

    Add City services

---
+++ OutOfSchool/OutOfSchool.WebApi/Services/CityService.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.Repository;
+using OutOfSchool.WebApi.Extensions;
+using OutOfSchool.WebApi.Models;
+using Serilog;
+
+namespace OutOfSchool.WebApi.Services
+{
+    /// <summary>
+    /// Implements the interface with CRUD functionality for for City entity.
+    /// </summary>
+    public class CityService : ICityService
+    {
+        private readonly IEntityRepository<City> repository;
+        private readonly ILogger logger;
+        private readonly IStringLocalizer<SharedResource> localizer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CityService"/> class.
+        /// </summary>
+        /// <param name="repository">Repository.</param>
+        /// <param name="logger">Logger.</param>
+        /// <param name="localizer">Localizer.</param>
+        public CityService(IEntityRepository<City> repository, ILogger logger, IStringLocalizer<SharedResource> localizer)
+        {
+            this.localizer = localizer;
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<CityDto>> GetAll()
+        {
+            logger.Information("Getting all Cities started.");
+
+            var cities = await repository.GetAll().ConfigureAwait(false);
+
+            logger.Information(!cities.Any()
+                ? "City table is empty."
+                : $"All {cities.Count()} records were successfully received from the City table");
+
+            return cities.Select(city => city.ToModel()).ToList();
+        }
+
+        /// <inheritdoc/>
+        public async Task<CityDto> GetById(long id)
+        {
+            logger.Information($"Getting City by Id started. Looking Id = {id}.");
+
+            var city = await repository.GetById(id).ConfigureAwait(false);
+
+            if (city == null)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer["The id cannot be greater than number of table entities."]);
+            }
+
+            logger.Information($"Successfully got a City with Id = {id}.");
+
+            return city.ToModel();
+        }
+
+        /// <inheritdoc/>
+        public async Task<CityDto> Create(CityDto dto)
+        {
+            logger.Information("City creating was started.");
+
+            var city = dto.ToDomain();
+
+            var newcity = await repository.Create(city).ConfigureAwait(false);
+
+            logger.Information($"City with Id = {newcity?.Id} created successfully.");
+
+            return newcity.ToModel();
+        }
+
+        /// <inheritdoc/>
+        public async Task<CityDto> Update(CityDto dto)
+        {
+            logger.Information($"Updating City with Id = {dto?.Id} started.");
+
+            try
+            {
+                var city = await repository.Update(dto.ToDomain()).ConfigureAwait(false);
+
+                logger.Information($"City with Id = {city?.Id} updated succesfully.");
+
+                return city.ToModel();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                logger.Error($"Updating failed. City with Id = {dto?.Id} doesn't exist in the system.");
+                throw;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task Delete(long id)
+        {
+            logger.Information($"Deleting City with Id = {id} started.");
+
+            var city = await repository.GetById(id).ConfigureAwait(false);
+
+            if (city == null)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(id),
+                    localizer[$"City with Id = {id} doesn't exist in the system"]);
+            }
+
+            await repository.Delete(city).ConfigureAwait(false);
+
+            logger.Information($"City with Id = {id} succesfully deleted.");
+        }
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Services/ICityService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OutOfSchool.WebApi.Models;
+
+namespace OutOfSchool.WebApi.Services
+{
+    /// <summary>
+    /// Defines interface for CRUD functionality for City entity.
+    /// </summary>
+    public interface ICityService
+    {
+        /// <summary>
+        /// Get all entities.
+        /// </summary>
+        /// <returns>List of all Cities.</returns>
+        Task<IEnumerable<CityDto>> GetAll();
+
+        /// <summary>
+        /// Get entity by it's key.
+        /// </summary>
+        /// <param name="id">Key in the table.</param>
+        /// <returns>City.</returns>
+        Task<CityDto> GetById(long id);
+
+        /// <summary>
+        /// Add entity.
+        /// </summary>
+        /// <param name="dto">City entity to add.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<CityDto> Create(CityDto dto);
+
+        /// <summary>
+        /// Update entity.
+        /// </summary>
+        /// <param name="dto">City entity to add.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<CityDto> Update(CityDto dto);
+
+        /// <summary>
+        ///  Delete entity.
+        /// </summary>
+        /// <param name="id">City key.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        Task Delete(long id);
+    }
+}
