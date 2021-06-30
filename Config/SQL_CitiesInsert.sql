a2e17d7e2b3a96a0e9a2eca399a0b4c640788306
Vitalii <v.ivanchuk1313@gmail.com>
2021-06-30 14:37:47 +0300

    Add information to script

--- Config/SQL_CitiesInsert.sql
+++ Config/SQL_CitiesInsert.sql
@@ -1,3 +1,10 @@
+--------------------------------------------------------------------------
+-- Author               Vitalii Ivanchuk
+-- Created              30.06.2021
+-- Task                 Create endpoints for Cities #163
+-- Addition             Please follow the attached in the task instruction
+--------------------------------------------------------------------------
+
   SET IDENTITY_INSERT Cities ON
   INSERT INTO Cities (Id, Region, District, Name, Latitude, Longitude)
   SELECT Id, Region, District, Name, Cast(Latitude as float), Cast(Longitude as float) FROM [Test]
