125f374ea090544d1a01dafc47e3766f97d10ec9
Sergey Novitsky <novitckij.s@gmail.com>
2021-07-05 18:53:44 +0300

    Add migration for changing lengths of nvarchars

--- OutOfSchool/OutOfSchool.WebApi/Models/CityDto.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/CityDto.cs
@@ -6,11 +6,14 @@ namespace OutOfSchool.WebApi.Models
     {
         public long Id { get; set; }
 
+        [MaxLength(30)]
         public string Region { get; set; } = string.Empty;
 
+        [MaxLength(30)]
         public string District { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "City Name is required")]
+        [MaxLength(30)]
         public string Name { get; set; } = string.Empty;
 
         public double Latitude { get; set; } = default;
