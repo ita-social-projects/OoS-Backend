b198b44db86dc4e26393bc0c24816952ed4e4b7d
P0linux <pollykr2000@gmail.com>
2021-07-06 12:28:35 +0300

    recreated migration to change Application entity

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20210706090957_ApplicationParentUpdate.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class ApplicationParentUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Applications_AspNetUsers_UserId",
+                table: "Applications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_UserId",
+                table: "Applications");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Applications");
+
+            migrationBuilder.AddColumn<long>(
+                name: "ParentId",
+                table: "Applications",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_ParentId",
+                table: "Applications",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Applications_Parents_ParentId",
+                table: "Applications",
+                column: "ParentId",
+                principalTable: "Parents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Applications_Parents_ParentId",
+                table: "Applications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_ParentId",
+                table: "Applications");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Applications");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Applications",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_UserId",
+                table: "Applications",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Applications_AspNetUsers_UserId",
+                table: "Applications",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
