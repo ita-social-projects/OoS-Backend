5b603805d7803cbf9e0f6efd8629ef9925a0d49d
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-07-09 13:26:18 +0300

    Added new dto models CardDto and ParentCardDto

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ParentCardDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.WebApi.Models
+{
+    public class ParentCardDto : CardDto
+    {
+        [Required]
+        public long ChildId { get; set; }
+
+        public int Status { get; set; }
+    }
+}
