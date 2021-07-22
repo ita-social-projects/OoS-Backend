d6c66a9ce278a229bfef19d7c3f71c720e1088cd
Vitalii <v.ivanchuk1313@gmail.com>
2021-07-22 17:53:32 +0300

    Add model and Dto

---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Favorite.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.Services.Models
+{
+    public class Favorite
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "Workshop id should be grater than 0")]
+        public long WorkshopId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual Workshop Workshop { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
