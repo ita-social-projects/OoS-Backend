aa57c61cef932741e1a33189e900a187f21f2b1a
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-07-27 13:01:42 +0300

    Added description to endpoints and change accessibility to them.

--- OutOfSchool/OutOfSchool.WebApi/Controllers/PhotoController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/PhotoController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using OutOfSchool.Services.Enums;
@@ -15,7 +16,7 @@ namespace OutOfSchool.WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
-    [AllowAnonymous]
+    [Authorize(AuthenticationSchemes = "Bearer")]
     public class PhotoController : ControllerBase
     {
         private readonly IPhotoStorage photoService;
@@ -35,7 +36,18 @@ namespace OutOfSchool.WebApi.Controllers
 
         private string Path { get; set; }
 
+        /// <summary>
+        /// Get files by it's keys.
+        /// </summary>
+        /// <param name="entityId">Id of the some entity.</param>
+        /// <param name="entityType">Type of the some entity.</param>
+        /// <returns>List of photos.</returns>
         [HttpGet("{entityId}/{entityType}")]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetFiles(long entityId, EntityType entityType)
         {
             this.ValidateId(entityId, localizer);
@@ -43,7 +55,18 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(await photoService.GetFiles(entityId, entityType).ConfigureAwait(false));
         }
 
+        /// <summary>
+        /// Get paths of the files by it's keys.
+        /// </summary>
+        /// <param name="entityId">Id of the some entity.</param>
+        /// <param name="entityType">Type of the some entity.</param>
+        /// <returns>List of paths.</returns>
         [HttpGet("{entityId}/{entityType}")]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetFilesPaths(long entityId, EntityType entityType)
         {
             this.ValidateId(entityId, localizer);
@@ -51,7 +74,18 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(await photoService.GetFilesPaths(entityId, entityType).ConfigureAwait(false));
         }
 
+        /// <summary>
+        /// Get path of the file by it's keys.
+        /// </summary>
+        /// <param name="entityId">Id of the some entity.</param>
+        /// <param name="entityType">Type of the some entity.</param>
+        /// <returns>Path of file.</returns>
         [HttpGet("{entityId}/{entityType}")]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetFilePath(long entityId, EntityType entityType)
         {
             this.ValidateId(entityId, localizer);
@@ -59,7 +93,17 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(await photoService.GetFilePath(entityId, entityType).ConfigureAwait(false));
         }
 
+        /// <summary>
+        /// Create new photo.
+        /// </summary>
+        /// <param name="photoInfo">Photo entity.</param>
+        /// <returns>Created photo info.</returns>
         [HttpPost]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateFile(PhotoDto photoInfo)
         {
             if (!ModelState.IsValid)
@@ -81,7 +125,17 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Create new photos.
+        /// </summary>
+        /// <param name="photosInfo">Photo entities.</param>
+        /// <returns>Created photos info.</returns>
         [HttpPost]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateFiles(List<PhotoDto> photosInfo)
         {
             if (photosInfo is null)
@@ -96,7 +150,17 @@ namespace OutOfSchool.WebApi.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Delete a specific Photo entity.
+        /// </summary>
+        /// <param name="filePath">Photo path.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         [HttpDelete]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteFile(string filePath)
         {
             if (filePath is null)
@@ -109,7 +173,17 @@ namespace OutOfSchool.WebApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Delete a range of Photo entities.
+        /// </summary>
+        /// <param name="filesPaths">Paths of the photos.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         [HttpDelete]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteFiles(List<string> filesPaths)
         {
             if (filesPaths is null)
@@ -122,7 +196,17 @@ namespace OutOfSchool.WebApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Update info about the Photo.
+        /// </summary>
+        /// <param name="photo">Entity to update.</param>
+        /// <returns>Updated Photo.</returns>
         [HttpPut]
+        [Authorize(Roles = "parent,provider,admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> UpdateFile(PhotoDto photo)
         {
             if (!ModelState.IsValid)
