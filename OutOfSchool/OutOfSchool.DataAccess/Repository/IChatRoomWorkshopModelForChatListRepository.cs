09a02249294500d5c691ca29d0a49875e49be1a0
Olha Holiak <067183buh@gmail.com>
2021-08-26 17:51:39 +0300

    removed code duplication in chat repository

--- OutOfSchool/OutOfSchool.DataAccess/Repository/IChatRoomWorkshopModelForChatListRepository.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IChatRoomWorkshopModelForChatListRepository.cs
@@ -6,12 +6,36 @@ namespace OutOfSchool.Services.Repository
 {
     public interface IChatRoomWorkshopModelForChatListRepository
     {
+        /// <summary>
+        /// Get a Chat room for workshop by specified Chat room key.
+        /// </summary>
+        /// <param name="chatRoomId">Chat rooms key in the system.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.
+        /// The task result contains a <see cref="ChatRoomWorkshopForChatList"/> that was faund, or null.</returns>
         Task<ChatRoomWorkshopForChatList> GetByChatRoomIdAsync(long chatRoomId);
 
+        /// <summary>
+        /// Get a List of chat rooms for workshops by specified Parent key.
+        /// </summary>
+        /// <param name="parentId">Parent key in the system.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.
+        /// The task result contains a <see cref="ICollection{ChatRoomWorkshopForChatList}"/> that contains faund elements.</returns>
         Task<ICollection<ChatRoomWorkshopForChatList>> GetByParentIdAsync(long parentId);
 
+        /// <summary>
+        /// Get a List of chat rooms for workshops by specified Provider key.
+        /// </summary>
+        /// <param name="providerId">Provider key in the system.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.
+        /// The task result contains a <see cref="ICollection{ChatRoomWorkshopForChatList}"/> that contains faund elements.</returns>
         Task<ICollection<ChatRoomWorkshopForChatList>> GetByProviderIdAsync(long providerId);
 
+        /// <summary>
+        /// Get a List of chat rooms for workshops by specified Workshop key.
+        /// </summary>
+        /// <param name="workshopId">Workshop key in the system.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.
+        /// The task result contains a <see cref="ICollection{ChatRoomWorkshopForChatList}"/> that contains faund elements.</returns>
         Task<ICollection<ChatRoomWorkshopForChatList>> GetByWorkshopIdAsync(long workshopId);
     }
 }
