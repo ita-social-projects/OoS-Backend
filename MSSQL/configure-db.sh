b430e4bfdcdc36cb14b726dec9dcc4e688a48e5b
Oleksandra Lytvynets <Oleksandra.Lytvynets@davita.com>
2021-09-14 12:24:15 +0300

    config

--- MSSQL/configure-db.sh
+++ MSSQL/configure-db.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Wait 60 seconds for SQL Server to start up by ensuring that 
+# Wait 60 seconds for SQL Server to start up by ensuring that
 # calling SQLCMD does not return an error code, which will ensure that sqlcmd is accessible
 # and that system and user databases return "0" which means all databases are in an "online" state
-# https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017 
+# https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017
 
 DBSTATUS=1
 ERRCODE=1
@@ -16,7 +16,7 @@ while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
 	sleep 10
 done
 
-if [ $DBSTATUS -ne 0 ] OR [ $ERRCODE -ne 0 ]; then 
+if [ $DBSTATUS -ne 0 ] OR [ $ERRCODE -ne 0 ]; then
 	echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
 	exit 1
 fi
