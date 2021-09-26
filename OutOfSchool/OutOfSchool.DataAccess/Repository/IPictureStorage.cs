a36e5da42e6758600336c02b67fe0c266edc5e50
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-09-26 21:06:47 +0300

    Added ability to upload and delete picture from MongoDb. Added PictureStorageException.

--- OutOfSchool/OutOfSchool.DataAccess/Repository/IPictureStorage.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IPictureStorage.cs
@@ -1,10 +1,16 @@
 ﻿using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace OutOfSchool.Services.Repository
 {
     public interface IPictureStorage
     {
         Task<Stream> GetPictureByIdAsync(string pictureId);
+
+        Task<string> UploadPicture(Stream contentStream, CancellationToken cancellationToken);
+
+        Task DeletePicture(ObjectId objectId, CancellationToken cancellationToken);
     }
 }
--- OutOfSchool/OutOfSchool.DataAccess/Repository/PictureStorageMongo.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/PictureStorageMongo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.GridFS;
+using OutOfSchool.Services.Common.Exceptions;
 using OutOfSchool.Services.Configuration;
 
 namespace OutOfSchool.Services.Repository
@@ -23,6 +25,24 @@ namespace OutOfSchool.Services.Repository
         }
 
         /// <summary>
+        /// Delete picture by it`s Id.
+        /// </summary>
+        /// <param name="objectId">Picture id.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task DeletePicture(ObjectId objectId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await greedFSBucket.DeleteAsync(objectId, cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new PictureStorageException(ex);
+            }
+        }
+
+        /// <summary>
         /// Returns picture by picture Id.
         /// </summary>
         /// <param name="pictureId">Picture id.</param>
@@ -30,6 +50,28 @@ namespace OutOfSchool.Services.Repository
         public async Task<Stream> GetPictureByIdAsync(string pictureId)
             // Note: await should be here bacause of type conversion: Task<GridFSDownloadStream> cannot be automaticaly cast to the Task<Stream> ...
             => await greedFSBucket.OpenDownloadStreamAsync(new ObjectId(pictureId));
+
+        /// <summary>
+        /// Returns picture Id.
+        /// </summary>
+        /// <param name="contentStream">Content stream.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task<string> UploadPicture(Stream contentStream, CancellationToken cancellationToken)
+        {
+            try
+            {
+                contentStream.Position = uint.MinValue;
+
+                var objectId = await greedFSBucket.UploadFromStreamAsync(Guid.NewGuid().ToString(), contentStream, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+                return objectId.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new PictureStorageException(ex);
+            }
+        }
 
         private IGridFSBucket GetGreedFSBucket()
         {
