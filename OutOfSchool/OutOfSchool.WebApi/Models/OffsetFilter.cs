77f260940e2b3cfbf8fd677c41d5766e6263a54e
Olha Holiak <067183buh@gmail.com>
2021-09-27 19:06:07 +0300

    refactored child service

--- OutOfSchool/OutOfSchool.WebApi/Models/OffsetFilter.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/OffsetFilter.cs
@@ -1,7 +1,10 @@
-﻿namespace OutOfSchool.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.WebApi.Models
 {
     public class OffsetFilter
     {
+        [Range(1, long.MaxValue)]
         public int Size { get; set; } = 12;
 
         public int From { get; set; } = 0;
