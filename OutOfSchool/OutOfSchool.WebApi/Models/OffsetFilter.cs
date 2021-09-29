3de801c9e06b90df0d03357af9213a71126ade78
Olha Holiak <067183buh@gmail.com>
2021-09-29 11:57:55 +0300

    fixed misstypes

--- OutOfSchool/OutOfSchool.WebApi/Models/OffsetFilter.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/OffsetFilter.cs
@@ -4,9 +4,10 @@ namespace OutOfSchool.WebApi.Models
 {
     public class OffsetFilter
     {
-        [Range(1, long.MaxValue)]
+        [Range(0, int.MaxValue)]
         public int Size { get; set; } = 12;
 
+        [Range(0, int.MaxValue)]
         public int From { get; set; } = 0;
     }
 }
