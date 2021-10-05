7b043851060082e92676852fc22ab1ecab77cd08
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2021-10-05 18:28:47 +0300

    Added documentation for daysBitMask helper

--- OutOfSchool/OutOfSchool.WebApi/Util/DaysBitMaskHelper.cs
+++ OutOfSchool/OutOfSchool.WebApi/Util/DaysBitMaskHelper.cs
@@ -7,16 +7,26 @@ namespace OutOfSchool.WebApi.Util
 {
     public static class DaysBitMaskHelper
     {
+        /// <summary>
+        /// Maps <see cref="IEnumerable{DaysBitMask}"/> of <see cref="DaysBitMask"/> into single enum using bitwise OR.
+        /// </summary>
+        /// <param name="daysList">Source.</param>
+        /// <returns>Merged enum.</returns>
         public static DaysBitMask ToDaysBitMask(this IEnumerable<DaysBitMask> daysList)
         {
             return daysList.Aggregate((prev, next) => prev | next);
         }
 
-        public static List<DaysBitMask> ToDaysBitMaskList(this DaysBitMask daysBitMask)
+        /// <summary>
+        /// Splits <see cref="DaysBitMask"/> into <see cref="IEnumerable{DaysBitMask}"/>.
+        /// </summary>
+        /// <param name="daysBitMask">Source.</param>
+        /// <returns>Splitted enumerable.</returns>
+        public static IEnumerable<DaysBitMask> ToDaysBitMaskEnumarable(this DaysBitMask daysBitMask)
         {
             return Enum.GetValues(typeof(DaysBitMask))
                 .Cast<DaysBitMask>().ToList()
-                .Where(amenity => amenity != 0 && daysBitMask.HasFlag(amenity)).ToList();
+                .Where(amenity => amenity != 0 && daysBitMask.HasFlag(amenity));
         }
     }
 }
