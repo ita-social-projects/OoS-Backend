dc469939dfafbab61a0cb3938d4a974d1a4e0a39
tohermann <tth4ik@gmail.com>
2021-10-14 16:08:39 +0300

    configured test files and wrote first tests for status service and controller

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Controllers/InstitutionStatusControllerTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Moq;
+using NUnit.Framework;
+using OutOfSchool.WebApi.Controllers.V1;
+using OutOfSchool.WebApi.Models;
+using OutOfSchool.WebApi.Services;
+
+namespace OutOfSchool.WebApi.Tests.Controllers
+{
+    [TestFixture]
+    public class InstitutionStatusControllerTests
+    {
+        private InstitutionStatusController controller;
+        private Mock<IStatusService> service;
+        private Mock<IStringLocalizer<SharedResource>> localizer;
+
+        private IEnumerable<InstitutionStatusDTO> institutionStatuses;
+        private InstitutionStatusDTO institutionStatus;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            service = new Mock<IStatusService>();
+            localizer = new Mock<IStringLocalizer<SharedResource>>();
+
+            controller = new InstitutionStatusController(service.Object, localizer.Object);
+
+            institutionStatuses = FakeInstitutionStatuses();
+            institutionStatus = FakeInstitutionStatus();
+        }
+
+        [Test]
+        public async Task GetInstitutionStatuses_WhenCalled_ReturnsOkResultObject()
+        {
+            // Arrange
+            service.Setup(x => x.GetAll()).ReturnsAsync(institutionStatuses);
+
+            // Act
+            var result = await controller.Get().ConfigureAwait(false) as OkObjectResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+        private InstitutionStatusDTO FakeInstitutionStatus()
+        {
+            return new InstitutionStatusDTO()
+            {
+                Id = 1,
+                Name = "Test",
+            };
+        }
+
+        private IEnumerable<InstitutionStatusDTO> FakeInstitutionStatuses()
+        {
+            return new List<InstitutionStatusDTO>()
+            {
+                new InstitutionStatusDTO()
+                {
+                Id = 1,
+                Name = "NoName",
+                },
+                new InstitutionStatusDTO()
+                {
+                Id = 2,
+                Name = "HaveName",
+                },
+                new InstitutionStatusDTO()
+                {
+                Id = 3,
+                Name = "MissName",
+                },
+            };
+        }
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/StatusServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using OutOfSchool.Services;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.Repository;
+using OutOfSchool.WebApi.Services;
+
+namespace OutOfSchool.WebApi.Tests.Services
+{
+    [TestFixture]
+    public class StatusServiceTests
+    {
+        private IStatusService service;
+        private OutOfSchoolDbContext context;
+        private IEntityRepository<InstitutionStatus> repository;
+        private Mock<IStringLocalizer<SharedResource>> localizer;
+        private Mock<ILogger<StatusService>> logger;
+        private DbContextOptions<OutOfSchoolDbContext> options;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var builder =
+                new DbContextOptionsBuilder<OutOfSchoolDbContext>().UseInMemoryDatabase(
+                    databaseName: "OutOfSchoolTestDB");
+
+            options = builder.Options;
+            context = new OutOfSchoolDbContext(options);
+            localizer = new Mock<IStringLocalizer<SharedResource>>();
+            repository = new EntityRepository<InstitutionStatus>(context);
+            logger = new Mock<ILogger<StatusService>>();
+            service = new StatusService(repository, logger.Object, localizer.Object);
+
+            SeedDatabase();
+        }
+
+        [Test]
+        public async Task GetAll_WhenCalled_ReturnsAllInstitutionStatuses()
+        {
+            // Arrange
+            var expected = await repository.GetAll();
+
+            // Act
+            var result = await service.GetAll().ConfigureAwait(false);
+
+            // Assert
+            Assert.AreEqual(result.ToList().Count(), expected.Count());
+        }
+
+
+
+        private void SeedDatabase()
+        {
+            using var context = new OutOfSchoolDbContext(options);
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var institutionStatuses = new List<InstitutionStatus>()
+                {
+                    new InstitutionStatus()
+                    {
+                    Id = 1,
+                    Name = "NoName",
+                    },
+                    new InstitutionStatus()
+                    {
+                    Id = 2,
+                    Name = "HaveName",
+                    },
+                    new InstitutionStatus()
+                    {
+                    Id = 3,
+                    Name = "MissName",
+                    },
+                };
+
+                context.InstitutionStatuses.AddRangeAsync(institutionStatuses);
+                context.SaveChangesAsync();
+            }
+        }
+    }
+}
