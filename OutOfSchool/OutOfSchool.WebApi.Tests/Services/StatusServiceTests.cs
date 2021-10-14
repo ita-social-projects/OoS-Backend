24b6cdb773f4e1b4c0d34884606e05dede6c0314
tohermann <tth4ik@gmail.com>
2021-10-14 18:59:07 +0300

    unit tests for status service added

--- OutOfSchool/OutOfSchool.WebApi.Tests/Services/StatusServiceTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/StatusServiceTests.cs
@@ -11,6 +11,7 @@ using NUnit.Framework;
 using OutOfSchool.Services;
 using OutOfSchool.Services.Models;
 using OutOfSchool.Services.Repository;
+using OutOfSchool.WebApi.Models;
 using OutOfSchool.WebApi.Services;
 
 namespace OutOfSchool.WebApi.Tests.Services
@@ -56,6 +57,101 @@ namespace OutOfSchool.WebApi.Tests.Services
         }
 
 
+        [Test]
+        [TestCase(2)]
+        public async Task GetById_WhenIdIsValid_ReturnsInstitutionStatus(long id)
+        {
+            // Arrange
+            var expected = await repository.GetById(id);
+
+            // Act
+            var result = await service.GetById(id).ConfigureAwait(false);
+
+            // Assert
+            Assert.AreEqual(expected.Id, result.Id);
+        }
+
+        [Test]
+        [TestCase(13)]
+        public void GetById_WhenIdIsInvalid_ThrowsArgumentOutOfRangeException(long id)
+        {
+            // Act and Assert
+            Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+                async () => await service.GetById(id).ConfigureAwait(false));
+        }
+
+        [Test]
+        public async Task Create_WhenEntityIsValid_ReturnsCreatedEntity()
+        {
+            // Arrange
+            var expected = new InstitutionStatusDTO() { Name = "TestName", };
+
+            // Act
+            var result = await service.Create(expected).ConfigureAwait(false);
+
+            // Assert
+            Assert.AreEqual(expected.Name, result.Name);
+        }
+
+        [Test]
+        public async Task Update_WhenEntityIsValid_UpdatesExistedEntity()
+        {
+            // Arrange
+            var changedEntity = new InstitutionStatusDTO()
+            {
+                Id = 1,
+                Name = "TestName",
+            };
+
+            // Act
+            var result = await service.Update(changedEntity).ConfigureAwait(false);
+
+            // Assert
+            Assert.That(changedEntity.Name, Is.EqualTo(result.Name));
+        }
+
+        [Test]
+        public void Update_WhenEntityIsInvalid_ThrowsDbUpdateConcurrencyException()
+        {
+            // Arrange
+            var changedEntity = new InstitutionStatusDTO()
+            {
+                Name = "TestName",
+            };
+
+            // Act and Assert
+            Assert.ThrowsAsync<DbUpdateConcurrencyException>(
+                async () => await service.Update(changedEntity).ConfigureAwait(false));
+        }
+
+        [Test]
+        [TestCase(1)]
+        public async Task Delete_WhenIdIsValid_DeletesEntity(long id)
+        {
+            // Arrange
+            var expected = await context.InstitutionStatuses.CountAsync();
+
+            // Act
+            await service.Delete(id).ConfigureAwait(false);
+
+            var result = (await service.GetAll().ConfigureAwait(false)).Count();
+
+            // Assert
+            Assert.AreEqual(expected - 1, result);
+        }
+
+        [Test]
+        [TestCase(13)]
+        public void Delete_WhenIdIsInvalid_ThrowsArgumentOutOfRangeException(long id)
+        {
+            // Act and Assert
+            Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+                async () => await service.Delete(id).ConfigureAwait(false));
+        }
+
+        /// <summary>
+        /// method to seed repository with entities to test.
+        /// </summary>
 
         private void SeedDatabase()
         {
