8640d2d7bb0d484dc225917f72901cd8b7ea447b
tohermann <tth4ik@gmail.com>
2021-10-29 17:17:46 +0300

    added blueprint for models and implemented service with crud operations

---
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/V1/PermissionsForRoleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.WebApi.Controllers.V1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]
+    [Route("[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class PermissionsForRoleController : ControllerBase
+    {
+    }
+}
