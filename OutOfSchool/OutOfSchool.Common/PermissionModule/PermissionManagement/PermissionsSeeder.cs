27bddff22a64ae8e86833c5454a9eed2c2cd3aea
tohermann <tth4ik@gmail.com>
2021-10-31 10:18:28 +0200

    updated permissionsforrole models and mapping profile, added migration, implemented "get" methods

--- OutOfSchool/OutOfSchool.Common/PermissionModule/PermissionManagement/PermissionsSeeder.cs
+++ OutOfSchool/OutOfSchool.Common/PermissionModule/PermissionManagement/PermissionsSeeder.cs
@@ -44,7 +44,6 @@ namespace OutOfSchool.Common.PermissionsModule
         // method which returns set of packed permissions for specific role
         public static string SeedPermissions(string role)
         {
-            string result = string.Empty;
             switch (role)
             {
                 case "admin":
--- OutOfSchool/OutOfSchool.WebApi/Controllers/V1/PermissionsForRoleController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/V1/PermissionsForRoleController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
