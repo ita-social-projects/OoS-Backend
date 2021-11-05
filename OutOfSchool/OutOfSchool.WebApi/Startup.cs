654dcce1933e808892bdc8042ed73edfb5b26095
Dmytro M <dmytro.minochkin@gmail.com>
2021-11-05 14:22:56 +0200

    fix applications and teachers

--- OutOfSchool/OutOfSchool.WebApi/Startup.cs
+++ OutOfSchool/OutOfSchool.WebApi/Startup.cs
@@ -70,7 +70,10 @@ namespace OutOfSchool.WebApi
 
             app.UseSwaggerWithVersioning(provider, proxyOptions);
 
-            app.UseHttpsRedirection();
+            if (!env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseSerilogRequestLogging();
 
