778a56fd2858e45400ab0eaaa9a881a34cff3992
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2021-11-08 13:48:13 +0200

    Unused using statements

--- OutOfSchool/Tests/OutOfSchool.WebApi.IntegrationTests/ProviderServiceIntergrationTests/ProviderServiceUpdate.cs
+++ OutOfSchool/Tests/OutOfSchool.WebApi.IntegrationTests/ProviderServiceIntergrationTests/ProviderServiceUpdate.cs
@@ -1,9 +1,6 @@
 namespace OutOfSchool.WebApi.IntegrationTests.ProviderServiceIntergrationTests
 {
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Linq.Expressions;
     using System.Threading.Tasks;
     using AutoMapper;
     using FluentAssertions.Common;
