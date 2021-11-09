8eee511dcacac4fd418b28c364f87aa1ffed34b5
Olha Holiak <067183buh@gmail.com>
2021-11-09 23:14:19 +0200

    created new models

---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/ChatWorkshop/ChatRoomWorkshop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.Services.Models.ChatWorkshop
+{
+    public class ChatRoomWorkshop
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid WorkshopId { get; set; }
+
+        [Required]
+        public Guid ParentId { get; set; }
+
+        public virtual Workshop Workshop { get; set; }
+
+        public virtual Parent Parent { get; set; }
+
+        public virtual ICollection<ChatMessageWorkshop> ChatMessages { get; set; }
+    }
+}
---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Configurations/ChatMessageWorkshopConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using OutOfSchool.Services.Models.ChatWorkshop;
+
+namespace OutOfSchool.Services.Models.Configurations
+{
+    internal class ChatMessageWorkshopConfiguration : IEntityTypeConfiguration<ChatMessageWorkshop>
+    {
+        public void Configure(EntityTypeBuilder<ChatMessageWorkshop> builder)
+        {
+            builder.HasKey(x => x.Id)
+                .IsClustered(false);
+
+            builder
+                .HasOne(m => m.ChatRoom)
+                .WithMany(r => r.ChatMessages)
+                .HasForeignKey(r => r.ChatRoomId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
