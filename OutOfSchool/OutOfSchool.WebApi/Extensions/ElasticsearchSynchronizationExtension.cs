66dd0618c5bbeeaf9ba1d26d53ee90ed7c4d58c0
Sergey Novitsky <novitckij.s@gmail.com>
2022-01-14 12:55:22 +0200

    Add options for elasticsearch synchronization (#487)
    * Add options for elasticsearch synchronization
    
    * Change structure for elasticsearch settings
    
    * Change settings after solve conflicts after rebase
    
    * fix productivity
    
    Co-authored-by: Sergii Novytskyi <snovi@softserveinc.com>

---
+++ OutOfSchool/OutOfSchool.WebApi/Extensions/ElasticsearchSynchronizationExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OutOfSchool.WebApi.Config;
+using OutOfSchool.WebApi.Services;
+
+namespace OutOfSchool.WebApi.Extensions
+{
+    public static class ElasticsearchSynchronizationExtension
+    {
+        public static IServiceCollection AddElasticsearchSynchronization(
+            this IServiceCollection services,
+            Action<OptionsBuilder<ElasticsearchSynchronizationSchedulerConfig>> elasticsearchSynchronizationSchedulerConfig)
+        {
+            services.AddHostedService<ElasticsearchSynchronizationHostedService>();
+            services.AddTransient<IElasticsearchSynchronizationService, ElasticsearchSynchronizationService>();
+            if (elasticsearchSynchronizationSchedulerConfig == null)
+            {
+                throw new ArgumentNullException(nameof(elasticsearchSynchronizationSchedulerConfig));
+            }
+
+            var elasticsearchSynchronizationSchedulerConfigBuilder = services.AddOptions<ElasticsearchSynchronizationSchedulerConfig>();
+            elasticsearchSynchronizationSchedulerConfig(elasticsearchSynchronizationSchedulerConfigBuilder);
+            return services;
+        }
+    }
+}
--- OutOfSchool/OutOfSchool.WebApi/Startup.cs
+++ OutOfSchool/OutOfSchool.WebApi/Startup.cs
@@ -169,8 +169,8 @@ namespace OutOfSchool.WebApi
             services.AddTransient<IElasticsearchProvider<WorkshopES, WorkshopFilterES>, ESWorkshopProvider>();
             services.AddTransient<IElasticsearchService<WorkshopES, WorkshopFilterES>, ESWorkshopService>();
 
-            services.AddHostedService<ElasticsearchSynchronizationHostedService>();
-            services.AddTransient<IElasticsearchSynchronizationService, ElasticsearchSynchronizationService>();
+            services.AddElasticsearchSynchronization(
+                builder => builder.Bind(Configuration.GetSection(ElasticsearchSynchronizationSchedulerConfig.SectionName)));
 
             // entities services
             services.AddTransient<IAddressService, AddressService>();
