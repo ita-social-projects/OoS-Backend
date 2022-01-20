fd0d74d892f6cb11fff1b0d29fee0a1e6ed0d6da
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2022-01-20 17:38:40 +0200

    Fix/support migrations (#522)
    * add logging to credentials store
    
    * add support info migration

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220120153319_AddSupportInformation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class AddSupportInformation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "GeoHash",
+                table: "Cities",
+                type: "bigint unsigned",
+                nullable: false,
+                defaultValue: 0ul);
+
+            migrationBuilder.CreateTable(
+                name: "SupportInformation",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "binary(16)", nullable: false),
+                    Title = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SectionName = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "varchar(3000)", maxLength: 3000, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupportInformation", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupportInformation");
+
+            migrationBuilder.DropColumn(
+                name: "GeoHash",
+                table: "Cities");
+        }
+    }
+}
