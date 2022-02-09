e5a7225216e050c8a15830f0a15863c44e4143ad
Dmytro M <dmytro.minochkin@gmail.com>
2022-02-09 10:57:35 +0200

    add access to GCR

--- Terraform/GCP/iam/group.tf
+++ Terraform/GCP/iam/group.tf
@@ -17,3 +17,15 @@ resource "google_project_iam_member" "group_roles" {
   member   = "group:${var.access_group_email}"
   project  = var.project
 }
+
+resource "google_container_registry" "registry" {
+  project = var.project
+  count   = "${var.access_group_email}" != "none" ? 1 : 0
+}
+
+resource "google_storage_bucket_iam_member" "viewer" {
+  bucket = google_container_registry.registry.id
+  role   = "roles/storage.objectViewer"
+  member = "group:${var.access_group_email}"
+  count  = "${var.access_group_email}" != "none" ? 1 : 0
+}
