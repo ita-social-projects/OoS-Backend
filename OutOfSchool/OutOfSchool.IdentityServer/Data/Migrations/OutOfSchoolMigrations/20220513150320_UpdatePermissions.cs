8d3877749e296e62e462a161c6c31a6363bbe1f0
Dmytro M <dmytro.minochkin@gmail.com>
2022-05-13 18:04:29 +0300

    update permission for role in DB

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220513150320_UpdatePermissions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class UpdatePermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "PackedPermissions",
+                value: "e\n26HGIFPQ[\\");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "PackedPermissions",
+                value: "e\n6HGIFPQ[\\");
+        }
+    }
+}
