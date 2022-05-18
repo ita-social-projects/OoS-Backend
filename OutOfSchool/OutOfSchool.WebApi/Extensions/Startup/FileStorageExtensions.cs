2114fb34adbfc902116235dabb42419e4c5784f1
provicevko <provicevk@gmail.com>
2022-05-18 16:39:03 +0300

    Fixed the lack of Gcp credential for all developers (#614)
    * Fixed the lack of Gcp credential for all developers
    
    * Added environment check

--- OutOfSchool/OutOfSchool.WebApi/Extensions/Startup/FileStorageExtensions.cs
+++ OutOfSchool/OutOfSchool.WebApi/Extensions/Startup/FileStorageExtensions.cs
@@ -1,17 +1,27 @@
+using System;
 using Google.Cloud.Storage.V1;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using OutOfSchool.Services.Contexts;
 using OutOfSchool.Services.Contexts.Configuration;
 using OutOfSchool.Services.Extensions;
 using OutOfSchool.Services.Repository.Files;
+using OutOfSchool.WebApi.Util.FakeImplementations;
 
 namespace OutOfSchool.WebApi.Extensions.Startup
 {
     public static class FileStorageExtensions
     {
-        public static IServiceCollection AddImagesStorage(this IServiceCollection services)
+        public static IServiceCollection AddImagesStorage(this IServiceCollection services, bool turnOnFakeStorage = false)
         {
+            var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
+
+            if (turnOnFakeStorage && isDevelopment)
+            {
+                return services.AddTransient<IImageFilesStorage, FakeImagesStorage>();
+            }
+
             return services.AddTransient<IImageFilesStorage, GcpImagesStorage>(provider =>
             {
                 var config = provider.GetRequiredService<IOptions<GcpStorageImagesSourceConfig>>();
