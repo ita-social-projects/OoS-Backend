b965d2be4577af8a0a71290c794e239f2df65b28
Dmytro M <dmytro.minochkin@gmail.com>
2022-05-23 21:53:46 +0300

    add connection string override

---
+++ OutOfSchool/OutOfSchool.Common/Extensions/Startup/ConnectionStringExtensions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Microsoft.Extensions.Configuration;
+using OutOfSchool.Common.Config;
+
+namespace OutOfSchool.Common.Extensions.Startup
+{
+    public static class ConnectionStringExtensions
+    {
+        public static string GetMySqlConnectionString<TOptions>(
+            this IConfiguration config,
+            string connectionStringKey,
+            Func<TOptions, DbConnectionStringBuilder> optionsToBuilder = null)
+            where TOptions : IMySqlConnectionOptions
+        {
+            var overrides = config
+                .GetSection("ConnectionStringsOverride")
+                .Get<Dictionary<string, TOptions>>();
+
+            DbConnectionStringBuilder connectionStringBuilder;
+
+            TOptions options;
+            if (overrides.TryGetValue(connectionStringKey, out options) && options.UseOverride)
+            {
+                if (optionsToBuilder == null)
+                {
+                    throw new ArgumentException("Please provide options to builder conversion if you are using overrides");
+                }
+
+                connectionStringBuilder = optionsToBuilder(options);
+            }
+            else
+            {
+                connectionStringBuilder = new DbConnectionStringBuilder()
+                {
+                    ConnectionString = config.GetConnectionString(connectionStringKey),
+                };
+            }
+
+            if (!connectionStringBuilder.ContainsKey("guidformat") || connectionStringBuilder["guidformat"].ToString().ToLower() != "binary16")
+            {
+                throw new Exception("The connection string should have a key: 'guidformat' and a value: 'binary16'");
+            }
+
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Config/WebApiConnectionOptions.cs
@@ -0,0 +1,21 @@
+using OutOfSchool.Common.Config;
+
+namespace OutOfSchool.WebApi.Config
+{
+    public class WebApiConnectionOptions : IMySqlConnectionOptions
+    {
+        public bool UseOverride { get; set; }
+
+        public string Server { get; set; }
+
+        public uint Port { get; set; }
+
+        public string Database { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsGuidFormatBinary16 { get; set; }
+    }
+}
