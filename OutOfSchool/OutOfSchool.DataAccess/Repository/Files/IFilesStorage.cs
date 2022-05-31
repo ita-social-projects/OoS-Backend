e66d1d297e30a9584c847a2c5731f61b89138a1f
provicevko <provicevk@gmail.com>
2022-06-01 00:25:56 +0300

    Added method to return Gcp list objects

--- OutOfSchool/OutOfSchool.DataAccess/Repository/Files/IFilesStorage.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/Files/IFilesStorage.cs
@@ -1,5 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Api.Gax;
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
 using OutOfSchool.Services.Models;
 using OutOfSchool.Services.Models.Images;
 
@@ -8,6 +13,8 @@ namespace OutOfSchool.Services.Repository.Files
     public interface IFilesStorage<TFile, TIdentifier>
         where TFile : FileModel
     {
+        Task<IAsyncEnumerable<Objects>> GetBulkListsOfObjectsAsync(string prefix = null, ListObjectsOptions options = null);
+
         /// <summary>
         /// Asynchronously gets a file by its id.
         /// </summary>
