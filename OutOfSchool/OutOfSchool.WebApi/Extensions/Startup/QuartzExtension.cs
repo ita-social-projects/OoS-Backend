975794b369ff8f5b7b9e0e2b8069256a4e1bca86
provicevko <provicevk@gmail.com>
2022-06-01 00:23:51 +0300

    Added quartz pool

---
+++ OutOfSchool/OutOfSchool.WebApi/Extensions/Startup/QuartzExtension.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+using OutOfSchool.Common.Extensions.Startup;
+using OutOfSchool.WebApi.Config;
+using OutOfSchool.WebApi.Services.Elasticsearch;
+using OutOfSchool.WebApi.Util;
+using Quartz;
+
+namespace OutOfSchool.WebApi.Extensions.Startup
+{
+    public static class QuartzExtension
+    {
+        public static IServiceCollection AddDefaultQuartz(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            _ = services ?? throw new ArgumentNullException(nameof(services));
+
+            services.AddQuartz(q =>
+            {
+                q.SchedulerId = "DefaultAppQuartz";
+                q.SchedulerName = "DefaultAppQuartz";
+                q.SetProperty("quartz.serializer.type", "json");
+                q.SetProperty("quartz.jobStore.type", "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz");
+                q.SetProperty("quartz.jobStore.dataSource", "default");
+                q.SetProperty("quartz.dataSource.default.provider", "MySql");
+                q.SetProperty(
+                    "quartz.dataSource.default.connectionString",
+                    configuration.GetMySqlConnectionString<QuartzConnectionOptions>(
+                        "QuartzConnection",
+                        options => new MySqlConnectionStringBuilder
+                        {
+                            Server = options.Server,
+                            Port = options.Port,
+                            UserID = options.UserId,
+                            Password = options.Password,
+                            Database = options.Database,
+                        }));
+                q.SetProperty("quartz.jobStore.clustered", "true");
+                q.SetProperty("quartz.jobStore.driverDelegateType", "Quartz.Impl.AdoJobStore.StdAdoDelegate, Quartz");
+                q.SetProperty("quartz.jobStore.useProperties", "true");
+                q.UseMicrosoftDependencyInjectionJobFactory();
+
+                foreach (var quartzAction in QuartzPool.GetQuartzConfigActions)
+                {
+                    quartzAction?.Invoke(q);
+                }
+            });
+
+            services.AddQuartzServer(options => { options.WaitForJobsToComplete = true; });
+
+            QuartzPool.Done();
+
+            return services;
+        }
+    }
+}
