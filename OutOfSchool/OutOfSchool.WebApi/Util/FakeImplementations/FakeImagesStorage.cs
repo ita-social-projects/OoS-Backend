006586cdca95bf62f493f101e7d7f9d21861ab0c
provicevko <provicevk@gmail.com>
2022-06-07 22:55:40 +0300

    Added preprocessor directives to FakeImagesStorage

--- OutOfSchool/OutOfSchool.WebApi/Util/FakeImplementations/FakeImagesStorage.cs
+++ OutOfSchool/OutOfSchool.WebApi/Util/FakeImplementations/FakeImagesStorage.cs
@@ -1,3 +1,5 @@
+#if DEBUG
+
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -37,3 +39,5 @@ namespace OutOfSchool.WebApi.Util.FakeImplementations
         }
     }
 }
+
+#endif
