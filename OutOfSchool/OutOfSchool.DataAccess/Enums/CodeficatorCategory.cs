8a9cd64c3f86cedabce54b1e8f090cd493a78021
RBiyovskiy <birud.home@gmail.com>
2022-06-14 11:59:10 +0300

    Codeficator model

---
+++ OutOfSchool/OutOfSchool.DataAccess/Enums/CodeficatorCategory.cs
@@ -0,0 +1,23 @@
+﻿namespace OutOfSchool.Services.Enums
+{
+    public static class CodeficatorCategory
+    {
+        public const string Region = "O"; // Автономна Республіка Крим, області.
+
+        public const string SpecialStatusCity = "K"; // Міста, що мають спеціальний статус.
+
+        public const string District = "P"; // Райони в областях та Автономній Республіці Крим.
+
+        public const string TerritorialCommunity = "H"; // Території територіальних громад (назви територіальних громад) в областях, територіальні громади Автономної Республіки Крим.
+
+        public const string City = "M"; // Міста.
+
+        public const string UrbanSettlement = "T"; // Селища міського типу.
+
+        public const string Village = "C"; // Села.
+
+        public const string Settlement = "X"; // Селища.
+
+        public const string CityDistrict = "B";  // Райони в містах.
+    }
+}
