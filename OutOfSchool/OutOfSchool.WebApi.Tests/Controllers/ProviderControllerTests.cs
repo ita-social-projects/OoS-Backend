49b9c7c65bf6c21dab4c0486e56dbe4398ecd2a6
IgorRakityanskiy <igor.rakityanskiy@gmail.com>
2022-06-20 12:14:54 +0300

    Add Provider controller tests

--- OutOfSchool/OutOfSchool.WebApi.Tests/Controllers/ProviderControllerTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Controllers/ProviderControllerTests.cs
@@ -33,25 +33,24 @@ namespace OutOfSchool.WebApi.Tests.Controllers
         private Mock<IProviderService> providerService;
         private List<Provider> providers;
         private Provider provider;
+        private string userId;
 
         [SetUp]
         public void Setup()
         {
-
+            userId = Guid.NewGuid().ToString();
             var localizer = new Mock<IStringLocalizer<SharedResource>>();
             var user = new ClaimsPrincipal
                 (new ClaimsIdentity(
                     new Claim[]
                     {
-                        new Claim(IdentityResourceClaimsTypes.Sub, Guid.NewGuid().ToString()),
+                        new Claim(IdentityResourceClaimsTypes.Sub, userId),
                         new Claim(IdentityResourceClaimsTypes.Role, Role.Provider.ToString()),
                     },
                     IdentityResourceClaimsTypes.Sub));
 
             providerService = new Mock<IProviderService>();
             providerController = new ProviderController(providerService.Object, localizer.Object, new Mock<ILogger<ProviderController>>().Object);
-
-
 
             providerController.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
             providers = ProvidersGenerator.Generate(10);
@@ -84,7 +83,6 @@ namespace OutOfSchool.WebApi.Tests.Controllers
             // Assert
             result.AssertResponseOkResultAndValidateValue(expected);
         }
-
 
         [Test]
         public async Task GetProviders_WhenCalled_ReturnsOkResultObject_WithExpectedCollectionDtos()
@@ -281,9 +279,110 @@ namespace OutOfSchool.WebApi.Tests.Controllers
             result.AssertExpectedResponseTypeAndCheckDataInside<BadRequestObjectResult>(expected);
         }
 
+        [Test]
+        public async Task StatusUpdate_WhenIdIsValid_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var provider = providers.FirstOrDefault();
 
+            var updateRequest = new ProviderStatusDto
+            {
+                ProviderId = provider.Id,
+                Status = ProviderStatus.Approved,
+            };
+            providerService.Setup(x => x.UpdateStatus(updateRequest, userId))
+                .ReturnsAsync(updateRequest);
 
+            // Act
+            var result = await providerController.StatusUpdate(updateRequest).ConfigureAwait(false);
 
+            // Assert
+            result.AssertResponseOkResultAndValidateValue(updateRequest);
+        }
+
+        [Test]
+        public async Task StatusUpdate_WhenIdDoesNotExist_ReturnsNotFoundResult()
+        {
+            // Arrange
+            var nonExistentProviderId = Guid.NewGuid();
+            var expected = new NotFoundObjectResult($"There is no Provider in DB with Id - {nonExistentProviderId}");
+            var updateRequest = new ProviderStatusDto
+            {
+                ProviderId = Guid.NewGuid(),
+                Status = ProviderStatus.Approved,
+            };
+            providerService.Setup(x => x.UpdateStatus(updateRequest, userId))
+                .ReturnsAsync(null as ProviderStatusDto);
+
+            // Act
+            var result = await providerController.StatusUpdate(updateRequest).ConfigureAwait(false);
+
+            // Assert
+            result.AssertExpectedResponseTypeAndCheckDataInside<NotFoundObjectResult>(expected);
+        }
+
+        [Test]
+        public async Task LicenseStatusUpdate_WhenIdIsValid_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var provider = providers.FirstOrDefault();
+
+            var updateRequest = new ProviderLicenseStatusDto
+            {
+                ProviderId = provider.Id,
+                LicenseStatus = ProviderLicenseStatus.Approved,
+            };
+            providerService.Setup(x => x.UpdateLicenseStatus(updateRequest, userId))
+                .ReturnsAsync(updateRequest);
+
+            // Act
+            var result = await providerController.LicenseStatusUpdate(updateRequest).ConfigureAwait(false);
+
+            // Assert
+            result.AssertResponseOkResultAndValidateValue(updateRequest);
+        }
+
+        [Test]
+        public async Task LicenseStatusUpdate_WhenIdDoesNotExist_ReturnsNotFoundResult()
+        {
+            // Arrange
+            var nonExistentProviderId = Guid.NewGuid();
+            var expected = new NotFoundObjectResult($"There is no Provider in DB with Id - {nonExistentProviderId}");
+            var updateRequest = new ProviderLicenseStatusDto
+            {
+                ProviderId = Guid.NewGuid(),
+                LicenseStatus = ProviderLicenseStatus.Approved,
+            };
+            providerService.Setup(x => x.UpdateLicenseStatus(updateRequest, userId))
+                .ReturnsAsync(null as ProviderLicenseStatusDto);
+
+            // Act
+            var result = await providerController.LicenseStatusUpdate(updateRequest).ConfigureAwait(false);
+
+            // Assert
+            result.AssertExpectedResponseTypeAndCheckDataInside<NotFoundObjectResult>(expected);
+        }
+
+        [Test]
+        public async Task LicenseStatusUpdate_WhenInvalidRequest_ReturnsBadRequestObjectResult()
+        {
+            // Arrange
+            var guid = Guid.NewGuid();
+            var errorMessage = TestDataHelper.GetRandomWords();
+            var expected = new BadRequestObjectResult(errorMessage);
+            var updateRequest = new ProviderLicenseStatusDto
+            {
+                ProviderId = Guid.NewGuid(),
+                LicenseStatus = ProviderLicenseStatus.Approved,
+            };
+            providerService.Setup(x => x.UpdateLicenseStatus(updateRequest, userId))
+                .ThrowsAsync(new ArgumentException(errorMessage));
+
+            // Act
+            var result = await providerController.LicenseStatusUpdate(updateRequest).ConfigureAwait(false);
+
+            // Assert
+            result.AssertExpectedResponseTypeAndCheckDataInside<BadRequestObjectResult>(expected);
+        }
     }
 }
-
