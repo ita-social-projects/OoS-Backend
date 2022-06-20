02b8cbb0f28078cd13175bb33e2f1b91c3309132
RBiyovskiy <birud.home@gmail.com>
2022-06-20 16:09:02 +0300

    Dto models, constants and CodeficatorCategory changes

--- OutOfSchool/OutOfSchool.DataAccess/Enums/CodeficatorCategory.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Enums/CodeficatorCategory.cs
@@ -6,13 +6,21 @@ namespace OutOfSchool.Services.Enums
     {
         public static readonly CodeficatorCategory Region = new CodeficatorCategory("O", 1); // Автономна Республіка Крим, області.
         public static readonly CodeficatorCategory SpecialStatusCity = new CodeficatorCategory("K", 2); // Міста, що мають спеціальний статус.
-        public static readonly CodeficatorCategory District = new CodeficatorCategory("P", 3); // Райони в областях та Автономній Республіці Крим.
-        public static readonly CodeficatorCategory TerritorialCommunity = new CodeficatorCategory("H", 4); // Території територіальних громад (назви територіальних громад) в областях, територіальні громади Автономної Республіки Крим.
-        public static readonly CodeficatorCategory City = new CodeficatorCategory("M", 5); // Міста.
-        public static readonly CodeficatorCategory UrbanSettlement = new CodeficatorCategory("T", 6); // Селища міського типу.
-        public static readonly CodeficatorCategory Village = new CodeficatorCategory("C", 7); // Села.
-        public static readonly CodeficatorCategory Settlement = new CodeficatorCategory("X", 8); // Селища.
-        public static readonly CodeficatorCategory CityDistrict = new CodeficatorCategory("B", 9); // Райони в містах.
+        public static readonly CodeficatorCategory Level1 = new CodeficatorCategory("OK", 3); // Автономна Республіка Крим, області та міста, що мають спеціальний статус.
+
+        public static readonly CodeficatorCategory District = new CodeficatorCategory("P", 4); // Райони в областях та Автономній Республіці Крим.
+
+        public static readonly CodeficatorCategory TerritorialCommunity = new CodeficatorCategory("H", 8); // Території територіальних громад (назви територіальних громад) в областях, територіальні громади Автономної Республіки Крим.
+        public static readonly CodeficatorCategory Level3 = new CodeficatorCategory("H", 8); // Території територіальних громад (назви територіальних громад) в областях, територіальні громади Автономної Республіки Крим.
+
+        public static readonly CodeficatorCategory City = new CodeficatorCategory("M", 16); // Міста.
+        public static readonly CodeficatorCategory UrbanSettlement = new CodeficatorCategory("T", 32); // Селища міського типу.
+        public static readonly CodeficatorCategory Village = new CodeficatorCategory("C", 64); // Села.
+        public static readonly CodeficatorCategory Settlement = new CodeficatorCategory("X", 128); // Селища.
+        public static readonly CodeficatorCategory Level4 = new CodeficatorCategory("MTCX", 208); // Міста, селища міського типу, села та селища
+
+        public static readonly CodeficatorCategory CityDistrict = new CodeficatorCategory("B", 256); // Райони в містах.
+        public static readonly CodeficatorCategory Level2 = new CodeficatorCategory("PB", 260); // Райони в областях та Автономній Республіці Крим та райони в містах.
 
         private CodeficatorCategory(string name, int value)
         : base(name, value)
---
+++ OutOfSchool/OutOfSchool.WebApi/Models/Codeficator/CodeficatorDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.WebApi.Models.Codeficator
+{
+    public class CodeficatorDto
+    {
+        public long Id { get; set; }
+
+        [MaxLength(20)]
+        public string Code { get; set; }
+
+        public long? ParentId { get; set; }
+
+        [MaxLength(3)]
+        public string Category { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public ulong GeoHash { get; set; }
+
+        public bool NeedCheck { get; set; }
+    }
+}
