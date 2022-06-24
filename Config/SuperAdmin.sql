7c318732c6b82822f651cdb67abc341b72e38571
provicevko <provicevk@gmail.com>
2022-06-24 16:20:40 +0300

    Added reset email, password

--- Config/SuperAdmin.sql
+++ Config/SuperAdmin.sql
@@ -45,11 +45,16 @@ NOW(), #CreatingTime
 NULL, #LockoutEnd
 1, #LockoutEnabled
 0, #AccessFailedCount
-1); #IsRegistered
+1) #IsRegistered
+ON DUPLICATE KEY UPDATE # to reset sensitive data   
+Email='admin@adm.com', #Email
+PasswordHash = 'AQAAAAEAACcQAAAAEB+AG8yyfG66fAkFIJ1mMdDx5ChM/wxoUHIwBuJt4cuO+jDCrPMw7D9BLYRtkvbrpg==', #PasswordHash  ->   Kf%ms3nAp7%aH
+SecurityStamp = 'G4GRJFN7ET7EVVDQ4NEQQOY6SETRULJB', #SecurityStamp
+ConcurrencyStamp = 'ed689760-8ab0-45fa-8b13-ec431c093627'; #ConcurrencyStamp
 
 INSERT INTO AspNetUserRoles
 (UserId, RoleId)
 VALUES
-('fe78ed30-7df9-412b-8f1c-a21f3175334f' #UserId (super admin)
+((SELECT Id FROM AspNetUsers WHERE Role = 'admin' LIMIT 1) #UserId (super admin)
 ,(SELECT Id FROM AspNetRoles WHERE Name = 'admin' LIMIT 1)) #RoleId (super admin)
 
