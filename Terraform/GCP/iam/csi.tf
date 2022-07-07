b1f33cb73a1051efb836d706d5984827e7d2f962
Dmytro M <dmytro.minochkin@gmail.com>
2022-07-07 17:32:21 +0300

    fix prisma errors

--- Terraform/GCP/iam/csi.tf
+++ Terraform/GCP/iam/csi.tf
@@ -15,16 +15,30 @@ resource "google_service_account_key" "csi" {
   }
 }
 
-resource "google_project_iam_member" "csi-storage" {
-  role    = "roles/compute.storageAdmin"
-  member  = "serviceAccount:${google_service_account.csi.email}"
-  project = var.project
+data "google_iam_role" "compute_storage" {
+  name = "roles/compute.storageAdmin"
 }
 
-resource "google_project_iam_member" "csi-sauser" {
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.csi.email}"
-  project = var.project
+resource "google_project_iam_custom_role" "csi_storage" {
+  role_id     = "gcp_compute_storage_admin_custom_role"
+  title       = "Custom Compute Storage Admin"
+  description = "Role with filtered resourcemanager.projects.get & resourcemanager.projects.list for Prisma Cloud Exception"
+  permissions = [
+    for p in data.google_iam_role.compute_storage.included_permissions : p if length(regexall("resourcemanager.projects.*", p)) == 0
+  ]
+}
+
+data "google_iam_role" "csi_sauser" {
+  name = "roles/iam.serviceAccountUser"
+}
+
+resource "google_project_iam_custom_role" "csi_sauser" {
+  role_id     = "gcp_sa_user_custom_role"
+  title       = "Custom Service Account User"
+  description = "Role with filtered resourcemanager.projects.get & resourcemanager.projects.list for Prisma Cloud Exception"
+  permissions = [
+    for p in data.google_iam_role.csi_sauser.included_permissions : p if length(regexall("resourcemanager.projects.*", p)) == 0
+  ]
 }
 
 resource "google_project_iam_custom_role" "csi" {
@@ -40,6 +54,18 @@ resource "google_project_iam_custom_role" "csi" {
 
 resource "google_project_iam_member" "gke-csi" {
   role    = google_project_iam_custom_role.csi.id
+  member  = "serviceAccount:${google_service_account.csi.email}"
+  project = var.project
+}
+
+resource "google_project_iam_member" "csi_storage" {
+  role    = google_project_iam_custom_role.csi_storage.id
+  member  = "serviceAccount:${google_service_account.csi.email}"
+  project = var.project
+}
+
+resource "google_project_iam_member" "csi_sauser" {
+  role    = google_project_iam_custom_role.csi_sauser.id
   member  = "serviceAccount:${google_service_account.csi.email}"
   project = var.project
 }
