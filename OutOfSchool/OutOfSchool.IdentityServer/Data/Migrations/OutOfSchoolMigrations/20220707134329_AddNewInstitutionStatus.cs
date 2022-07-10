68fcb7406d2e12a1f450a8bd634ca89eb1199925
IgorRakityanskiy <igor.rakityanskiy@gmail.com>
2022-07-10 09:33:37 +0300

    Fix namespace scope

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220707134329_AddNewInstitutionStatus.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220707134329_AddNewInstitutionStatus.cs
@@ -2,24 +2,23 @@
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
-{
-    public partial class AddNewInstitutionStatus : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.InsertData(
-                table: "InstitutionStatuses",
-                columns: new[] { "Id", "Name" },
-                values: new object[] { 4L, "Відсутній" });
-        }
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                table: "InstitutionStatuses",
-                keyColumn: "Id",
-                keyValue: 4L);
-        }
+public partial class AddNewInstitutionStatus : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.InsertData(
+            table: "InstitutionStatuses",
+            columns: new[] { "Id", "Name" },
+            values: new object[] { 4L, "Відсутній" });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DeleteData(
+            table: "InstitutionStatuses",
+            keyColumn: "Id",
+            keyValue: 4L);
     }
 }
