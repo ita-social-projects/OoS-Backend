c9199439e98800d5a1a5901c33b8bb822095d8ae
provicevko <provicevk@gmail.com>
2022-07-22 01:20:41 +0300

    Added using password regex from constants

--- OutOfSchool/OutOfSchool.IdentityServer/ViewModels/ChangePasswordViewModel.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/ViewModels/ChangePasswordViewModel.cs
@@ -10,7 +10,7 @@ public class ChangePasswordViewModel
 
     [Required(ErrorMessage = "Password is required")]
     [RegularExpression(
-        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        Constants.PasswordRegexViewModel,
         ErrorMessage = "Password must contain at least one capital, number and symbol(@$!%*?&).")]
     [DataType(DataType.Password)]
     public string NewPassword { get; set; }
