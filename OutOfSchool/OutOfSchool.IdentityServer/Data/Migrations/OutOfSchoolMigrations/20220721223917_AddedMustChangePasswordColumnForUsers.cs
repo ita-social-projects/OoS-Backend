4a974af427acc0e310a78751193c4fd31c0a6646
provicevko <provicevk@gmail.com>
2022-07-22 01:43:38 +0300

    namespace fix

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220721223917_AddedMustChangePasswordColumnForUsers.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220721223917_AddedMustChangePasswordColumnForUsers.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
-{
-    public partial class AddedMustChangePasswordColumnForUsers : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "MustChangePassword",
-                table: "AspNetUsers",
-                type: "tinyint(1)",
-                nullable: false,
-                defaultValue: false);
-        }
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "MustChangePassword",
-                table: "AspNetUsers");
-        }
+public partial class AddedMustChangePasswordColumnForUsers : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "MustChangePassword",
+            table: "AspNetUsers",
+            type: "tinyint(1)",
+            nullable: false,
+            defaultValue: false);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "MustChangePassword",
+            table: "AspNetUsers");
     }
 }
