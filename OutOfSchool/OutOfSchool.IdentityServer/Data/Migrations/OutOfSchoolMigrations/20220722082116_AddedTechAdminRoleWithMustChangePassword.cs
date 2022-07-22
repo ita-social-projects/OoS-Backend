b44089284dfc6c26c08b743f3379d853757e00ea
provicevko <provicevk@gmail.com>
2022-07-22 11:23:15 +0300

    fix migration issue

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220722082116_AddedTechAdminRoleWithMustChangePassword.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220722082116_AddedTechAdminRoleWithMustChangePassword.cs
@@ -2,39 +2,38 @@
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
+
+public partial class AddedTechAdminRoleWithMustChangePassword : Migration
 {
-    public partial class AddedTechAdminRoleWithMustChangePassword : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "MustChangePassword",
-                table: "AspNetUsers",
-                type: "tinyint(1)",
-                nullable: false,
-                defaultValue: false);
+        migrationBuilder.AddColumn<bool>(
+            name: "MustChangePassword",
+            table: "AspNetUsers",
+            type: "tinyint(1)",
+            nullable: false,
+            defaultValue: false);
 
-            migrationBuilder.UpdateData(
-                table: "PermissionsForRoles",
-                keyColumn: "Id",
-                keyValue: 1L,
-                columns: new[] { "Description", "RoleName" },
-                values: new object[] { "techadmin permissions", "TechAdmin" });
-        }
+        migrationBuilder.UpdateData(
+            table: "PermissionsForRoles",
+            keyColumn: "Id",
+            keyValue: 1L,
+            columns: new[] { "Description", "RoleName" },
+            values: new object[] { "techadmin permissions", "TechAdmin" });
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "MustChangePassword",
-                table: "AspNetUsers");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "MustChangePassword",
+            table: "AspNetUsers");
 
-            migrationBuilder.UpdateData(
-                table: "PermissionsForRoles",
-                keyColumn: "Id",
-                keyValue: 1L,
-                columns: new[] { "Description", "RoleName" },
-                values: new object[] { "admin permissions", "Admin" });
-        }
+        migrationBuilder.UpdateData(
+            table: "PermissionsForRoles",
+            keyColumn: "Id",
+            keyValue: 1L,
+            columns: new[] { "Description", "RoleName" },
+            values: new object[] { "admin permissions", "Admin" });
     }
 }
