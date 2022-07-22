148294f772b929d4739d5eeb6a22aea3539b9c25
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2022-07-22 23:39:04 +0300

    qq

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220719211513_OpenIdDictTables.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220719211513_OpenIdDictTables.cs
@@ -9,28 +9,41 @@ namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "OpenIddictApplications",
-                columns: table => new
-                {
-                    Id = table.Column<string>(nullable: false),
-                    ClientId = table.Column<string>(nullable: true),
-                    ClientSecret = table.Column<string>(nullable: true),
-                    DisplayName = table.Column<string>(nullable: true),
-                    LogoutRedirectUri = table.Column<string>(nullable: true),
-                    RedirectUri = table.Column<string>(nullable: true),
-                    Type = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_OpenIddictApplications", x => x.Id);
-                });
+            name: "OpenIddictApplications",
+            columns: table => new
+            {
+                Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                ClientId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                ClientSecret = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                ConcurrencyToken = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                ConsentType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                DisplayNames = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Permissions = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                PostLogoutRedirectUris = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Properties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                RedirectUris = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Requirements = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OpenIddictApplications", x => x.Id);
+            });
 
             migrationBuilder.CreateTable(
                 name: "OpenIddictScopes",
                 columns: table => new
                 {
-                    Id = table.Column<string>(nullable: false),
-                    Description = table.Column<string>(nullable: true)
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ConcurrencyToken = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descriptions = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayNames = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Properties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Resources = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -38,34 +51,47 @@ namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "OpenIddictAuthorizations",
-                columns: table => new
-                {
-                    Id = table.Column<string>(nullable: false),
-                    ApplicationId = table.Column<string>(nullable: true),
-                    Scope = table.Column<string>(nullable: true),
-                    Subject = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_OpenIddictAuthorizations", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId",
-                        column: x => x.ApplicationId,
-                        principalTable: "OpenIddictApplications",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
+            name: "OpenIddictAuthorizations",
+            columns: table => new
+            {
+                Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                ApplicationId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                ConcurrencyToken = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                CreationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                Properties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Scopes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Status = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                Subject = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true),
+                Type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OpenIddictAuthorizations", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId",
+                    column: x => x.ApplicationId,
+                    principalTable: "OpenIddictApplications",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Restrict);
+            });
 
             migrationBuilder.CreateTable(
                 name: "OpenIddictTokens",
                 columns: table => new
                 {
-                    Id = table.Column<string>(nullable: false),
-                    ApplicationId = table.Column<string>(nullable: true),
-                    AuthorizationId = table.Column<string>(nullable: true),
-                    Subject = table.Column<string>(nullable: true),
-                    Type = table.Column<string>(nullable: true)
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ApplicationId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    AuthorizationId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ConcurrencyToken = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Payload = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Properties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RedemptionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ReferenceId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
                 },
                 constraints: table =>
                 {
@@ -85,25 +111,40 @@ namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_OpenIddictApplications_ClientId",
-                table: "OpenIddictApplications",
-                column: "ClientId",
-                unique: true);
+            name: "IX_OpenIddictApplications_ClientId",
+            table: "OpenIddictApplications",
+            column: "ClientId",
+            unique: true,
+            filter: "[ClientId] IS NOT NULL");
 
             migrationBuilder.CreateIndex(
-                name: "IX_OpenIddictAuthorizations_ApplicationId",
+                name: "IX_OpenIddictAuthorizations_ApplicationId_Status_Subject_Type",
                 table: "OpenIddictAuthorizations",
-                column: "ApplicationId");
+                columns: new[] { "ApplicationId", "Status", "Subject", "Type" });
 
             migrationBuilder.CreateIndex(
-                name: "IX_OpenIddictTokens_ApplicationId",
+                name: "IX_OpenIddictScopes_Name",
+                table: "OpenIddictScopes",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictTokens_ApplicationId_Status_Subject_Type",
                 table: "OpenIddictTokens",
-                column: "ApplicationId");
+                columns: new[] { "ApplicationId", "Status", "Subject", "Type" });
 
             migrationBuilder.CreateIndex(
                 name: "IX_OpenIddictTokens_AuthorizationId",
                 table: "OpenIddictTokens",
                 column: "AuthorizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictTokens_ReferenceId",
+                table: "OpenIddictTokens",
+                column: "ReferenceId",
+                unique: true,
+                filter: "[ReferenceId] IS NOT NULL");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
