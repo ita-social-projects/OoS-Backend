0346570f7538be00b4f6dfe3645480d2754d4778
RBiyovskiy <birud.home@gmail.com>
2022-07-25 15:10:31 +0300

    ShortUserDto and ProiderAdminDto changes

--- OutOfSchool/OutOfSchool.WebApi/Models/ProviderAdminDto.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ProviderAdminDto.cs
@@ -4,20 +4,8 @@ using OutOfSchool.WebApi.Enums;
 
 namespace OutOfSchool.WebApi.Models;
 
-public class ProviderAdminDto
+public class ProviderAdminDto : BaseUserDto
 {
-    public string Id { get; set; }
-
-    public string FirstName { get; set; }
-
-    public string LastName { get; set; }
-
-    public string MiddleName { get; set; }
-
-    public string Email { get; set; }
-
-    public string PhoneNumber { get; set; }
-
     public bool IsDeputy { get; set; }
 
     [Required(ErrorMessage = "Gender is required")]
