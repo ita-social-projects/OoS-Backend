0b1c3f4832518af4f38efe0837581417eab34274
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2022-07-25 17:54:29 +0300

    Add migration

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220725145346_AddIsParentPropertyToChild.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class AddIsParentPropertyToChild : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsParent",
+                table: "Children",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsParent",
+                table: "Children");
+        }
+    }
+}
