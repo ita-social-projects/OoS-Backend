0c44aad72e69542fcd5f0b575ede1e9642fc7cce
RBiyovskiy <birud.home@gmail.com>
2022-07-25 15:07:21 +0300

    MinistryAdminDto changes

--- OutOfSchool/OutOfSchool.WebApi/Models/MinistryAdminDto.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/MinistryAdminDto.cs
@@ -4,20 +4,8 @@ using OutOfSchool.WebApi.Enums;
 
 namespace OutOfSchool.WebApi.Models;
 
-public class MinistryAdminDto
+public class MinistryAdminDto : BaseUserDto
 {
-    public string Id { get; set; }
-
-    public string FirstName { get; set; }
-
-    public string LastName { get; set; }
-
-    public string MiddleName { get; set; }
-
-    public string Email { get; set; }
-
-    public string PhoneNumber { get; set; }
-
     public AccountStatus AccountStatus { get; set; }
 
     public Guid InstitutionId { get; set; }
@@ -25,4 +13,6 @@ public class MinistryAdminDto
     [Required(ErrorMessage = "Gender is required")]
     [EnumDataType(typeof(Gender), ErrorMessage = Constants.EnumErrorMessage)]
     public Gender Gender { get; set; }
+    
+    public string InstitutionTitle { get; set; }
 }
