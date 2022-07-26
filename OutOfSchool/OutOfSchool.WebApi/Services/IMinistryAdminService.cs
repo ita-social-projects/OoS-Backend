28aea18a10cf2cfc6462356bafa98309adc5a02b
provicevko <provicevk@gmail.com>
2022-07-26 19:32:21 +0300

    Added ability to see providers' chats for ministry admins (#767)
    * Added ability to see providers' chats for ministry admins
    
    * Added null checks in the controller's constructor

--- OutOfSchool/OutOfSchool.WebApi/Services/IMinistryAdminService.cs
+++ OutOfSchool/OutOfSchool.WebApi/Services/IMinistryAdminService.cs
@@ -30,4 +30,13 @@ public interface IMinistryAdminService
     /// <param name="token">Valid token with MinistryAdminEdit permissions.</param>
     /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
     Task<ResponseDto> BlockMinistryAdminAsync(string ministryAdminId, string userId, string token);
+
+    /// <summary>
+    /// Determines whether provider is subordinate of the ministry admin.
+    /// </summary>
+    /// <param name="ministryAdminUserId">Ministry admin user id.</param>
+    /// <param name="providerId">Provider id.</param>
+    /// <returns>The task result contains <see langword="true" /> if provider is subordinate of the ministry admin
+    /// filter; otherwise, <see langword="false" />.</returns>
+    Task<bool> IsProviderSubordinateAsync(string ministryAdminUserId, Guid providerId);
 }
