4647784c47be80ba621e416a5844e89a78de26e4
provicevko <provicevk@gmail.com>
2022-07-27 21:47:56 +0300

    fix InstitutionHierarchyDto (#776)

--- OutOfSchool/OutOfSchool.WebApi/Models/SubordinationStructure/InstitutionHierarchyDto.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/SubordinationStructure/InstitutionHierarchyDto.cs
@@ -20,7 +20,7 @@ public class InstitutionHierarchyDto
     [Required]
     public Guid InstitutionId { get; set; }
 
-    public Institution Institution { get; set; }
+    public InstitutionDto Institution { get; set; }
 
     public List<DirectionDto> Directions { get; set; }
 }
