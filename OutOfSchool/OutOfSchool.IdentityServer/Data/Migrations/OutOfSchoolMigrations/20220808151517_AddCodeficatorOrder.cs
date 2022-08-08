90e4a1c0dd2036cefff090dbed8b635e64ca200c
Dmytro M <dmytro.minochkin@gmail.com>
2022-08-08 18:22:00 +0300

    add migration for order

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220808151517_AddCodeficatorOrder.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
+
+public partial class AddCodeficatorOrder : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "Order",
+            table: "CATOTTGs",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Order",
+            table: "CATOTTGs");
+    }
+}
