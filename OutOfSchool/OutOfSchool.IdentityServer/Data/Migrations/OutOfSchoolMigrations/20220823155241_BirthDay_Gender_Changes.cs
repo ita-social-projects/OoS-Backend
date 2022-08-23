eb8da2f049790832ac35eb2fc43a0e70a7e03c2c
provicevko <provicevk@gmail.com>
2022-08-23 19:19:11 +0300

    Fixed migration issue

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220823155241_BirthDay_Gender_Changes.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220823155241_BirthDay_Gender_Changes.cs
@@ -3,47 +3,46 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
+
+public partial class BirthDay_Gender_Changes : Migration
 {
-    public partial class BirthDay_Gender_Changes : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Gender",
-                table: "AspNetUsers");
+        migrationBuilder.DropColumn(
+            name: "Gender",
+            table: "AspNetUsers");
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "DateOfBirth",
-                table: "Parents",
-                type: "date",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        migrationBuilder.AddColumn<DateTime>(
+            name: "DateOfBirth",
+            table: "Parents",
+            type: "date",
+            nullable: false,
+            defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
 
-            migrationBuilder.AddColumn<int>(
-                name: "Gender",
-                table: "Parents",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
-        }
+        migrationBuilder.AddColumn<int>(
+            name: "Gender",
+            table: "Parents",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "DateOfBirth",
-                table: "Parents");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "DateOfBirth",
+            table: "Parents");
 
-            migrationBuilder.DropColumn(
-                name: "Gender",
-                table: "Parents");
+        migrationBuilder.DropColumn(
+            name: "Gender",
+            table: "Parents");
 
-            migrationBuilder.AddColumn<int>(
-                name: "Gender",
-                table: "AspNetUsers",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
-        }
+        migrationBuilder.AddColumn<int>(
+            name: "Gender",
+            table: "AspNetUsers",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
     }
 }
