cd49244a4bd34ca353e90a6d7dea6d1c22c77355
ZdorovenkoKyrylo <92879521+ZdorovenkoKyrylo@users.noreply.github.com>
2022-08-25 19:35:37 +0300

    Fixed Namespace

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220825163252_ChangeDateTime.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20220825163252_ChangeDateTime.cs
@@ -2,78 +2,77 @@
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
+
+public partial class ChangeDateTime : Migration
 {
-    public partial class ChangeDateTime : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.UpdateData(
-                table: "Providers",
-                keyColumn: "PhoneNumber",
-                keyValue: null,
-                column: "PhoneNumber",
-                value: "");
+        migrationBuilder.UpdateData(
+            table: "Providers",
+            keyColumn: "PhoneNumber",
+            keyValue: null,
+            column: "PhoneNumber",
+            value: "");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "PhoneNumber",
-                table: "Providers",
-                type: "varchar(15)",
-                maxLength: 15,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "varchar(15)",
-                oldMaxLength: 15,
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        migrationBuilder.AlterColumn<string>(
+            name: "PhoneNumber",
+            table: "Providers",
+            type: "varchar(15)",
+            maxLength: 15,
+            nullable: false,
+            oldClrType: typeof(string),
+            oldType: "varchar(15)",
+            oldMaxLength: 15,
+            oldNullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.UpdateData(
-                table: "Providers",
-                keyColumn: "Director",
-                keyValue: null,
-                column: "Director",
-                value: "");
+        migrationBuilder.UpdateData(
+            table: "Providers",
+            keyColumn: "Director",
+            keyValue: null,
+            column: "Director",
+            value: "");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Director",
-                table: "Providers",
-                type: "varchar(50)",
-                maxLength: 50,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "varchar(50)",
-                oldMaxLength: 50,
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Director",
+            table: "Providers",
+            type: "varchar(50)",
+            maxLength: 50,
+            nullable: false,
+            oldClrType: typeof(string),
+            oldType: "varchar(50)",
+            oldMaxLength: 50,
+            oldNullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "PhoneNumber",
-                table: "Providers",
-                type: "varchar(15)",
-                maxLength: 15,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(15)",
-                oldMaxLength: 15)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "PhoneNumber",
+            table: "Providers",
+            type: "varchar(15)",
+            maxLength: 15,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "varchar(15)",
+            oldMaxLength: 15)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Director",
-                table: "Providers",
-                type: "varchar(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(50)",
-                oldMaxLength: 50)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Director",
+            table: "Providers",
+            type: "varchar(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "varchar(50)",
+            oldMaxLength: 50)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
     }
 }
