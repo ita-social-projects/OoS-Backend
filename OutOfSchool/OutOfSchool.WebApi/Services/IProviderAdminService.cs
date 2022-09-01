5ea4486e4e546ee77954ea226e599f066243511d
Valeriy Baditsa <valerabaditsa@gmail.com>
2022-09-01 17:04:03 +0300

    Fix ManagedWorkshops returned value

--- OutOfSchool/OutOfSchool.WebApi/Services/IProviderAdminService.cs
+++ OutOfSchool/OutOfSchool.WebApi/Services/IProviderAdminService.cs
@@ -41,7 +41,7 @@ public interface IProviderAdminService
     /// <param name="userId">Key in the table.</param>
     /// <param name="isProviderDeputy">Is providerAdmin deputy or no.</param>
     /// <returns>List of the workshops that providerAdmin can manage.</returns>
-    Task<IEnumerable<WorkshopBaseCard>> GetWorkshopsThatProviderAdminCanManage(string userId, bool isProviderDeputy);
+    Task<IEnumerable<WorkshopProviderViewCard>> GetWorkshopsThatProviderAdminCanManage(string userId, bool isProviderDeputy);
 
     /// <summary>
     /// Get entity by it's key.
