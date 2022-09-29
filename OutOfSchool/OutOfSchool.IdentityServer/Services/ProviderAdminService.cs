77f0aee7530b29529b81946e6d21453db2fbee27
Valerii Baditsa <valerabaditsa@gmail.com>
2022-09-29 13:16:06 +0300

    Fix code smell issues

--- OutOfSchool/OutOfSchool.IdentityServer/Services/ProviderAdminService.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Services/ProviderAdminService.cs
@@ -314,7 +314,7 @@ public class ProviderAdminService : IProviderAdminService
                     : new List<Workshop>();
 
                 await providerAdminRepository.Update(providerAdmin).ConfigureAwait(false);
-                
+
                 await providerAdminChangesLogService.SaveChangesLogAsync(providerAdmin, userId, OperationType.Update)
                     .ConfigureAwait(false);
 
