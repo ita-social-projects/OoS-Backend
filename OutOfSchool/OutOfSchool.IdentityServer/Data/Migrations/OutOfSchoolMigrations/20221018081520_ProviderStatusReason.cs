78675ac07567f2061364c4a77f89b272d33cf128
Dmytro M <dmytro.minochkin@gmail.com>
2022-10-24 15:54:56 +0300

    add radius filter for ES

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20221018081520_ProviderStatusReason.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20221018081520_ProviderStatusReason.cs
@@ -2,26 +2,25 @@
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
+
+public partial class ProviderStatusReason : Migration
 {
-    public partial class ProviderStatusReason : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
+        migrationBuilder.AddColumn<string>(
                 name: "StatusReason",
                 table: "Providers",
                 type: "varchar(500)",
                 maxLength: 500,
                 nullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4");
-        }
+            .Annotation("MySql:CharSet", "utf8mb4");
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "StatusReason",
-                table: "Providers");
-        }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "StatusReason",
+            table: "Providers");
     }
 }
