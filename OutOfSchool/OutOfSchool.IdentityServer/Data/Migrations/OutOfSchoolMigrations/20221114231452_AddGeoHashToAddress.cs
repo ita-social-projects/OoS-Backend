46367e07bdc421b0ed811a8645f112e78c4300a6
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2022-11-20 02:41:22 +0200

    Fix bug

--- OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20221114231452_AddGeoHashToAddress.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Data/Migrations/OutOfSchoolMigrations/20221114231452_AddGeoHashToAddress.cs
@@ -4,25 +4,24 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations
-{
-    public partial class AddGeoHashToAddress : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<ulong>(
-                name: "GeoHash",
-                table: "Addresses",
-                type: "bigint unsigned",
-                nullable: false,
-                defaultValue: 0ul);
-        }
+namespace OutOfSchool.IdentityServer.Data.Migrations.OutOfSchoolMigrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "GeoHash",
-                table: "Addresses");
-        }
+public partial class AddGeoHashToAddress : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<ulong>(
+            name: "GeoHash",
+            table: "Addresses",
+            type: "bigint unsigned",
+            nullable: false,
+            defaultValue: 0ul);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "GeoHash",
+            table: "Addresses");
     }
 }
