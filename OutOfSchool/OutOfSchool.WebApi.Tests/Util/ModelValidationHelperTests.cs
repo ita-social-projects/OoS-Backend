259bc85fbe1fb059e7e94176c27c485f6c007fbb
irapopova <ira.dankovska@gmail.com>
2022-11-24 16:08:42 +0100

    added tests for ModelValidationHelperTests

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Util/ModelValidationHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using OutOfSchool.WebApi.Models;
+using OutOfSchool.WebApi.Util;
+
+namespace OutOfSchool.WebApi.Tests.Util;
+
+[TestFixture]
+public class ModelValidationHelperTests
+{
+    [Test]
+    public void ValidateExcludedIdFilter_WhenNull_Should_ThrowException()
+    {
+        // Act and Assert
+        Assert.Throws<ArgumentNullException>(() => ModelValidationHelper.ValidateExcludedIdFilter(null));
+    }
+
+    [Test]
+    public void ValidateExcludedIdFilter_WhenGuidIsEMpty_Should_ThrowException()
+    {
+        // Arrange
+        var filter = new ExcludeIdFilter() { ExcludedId = Guid.Empty };
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => ModelValidationHelper.ValidateExcludedIdFilter(filter));
+    }
+
+    [Test]
+    public void ValidateExcludedIdFilter_WhenOffsetFilterIsIncorrect_Should_ThrowException()
+    {
+        // Arrange
+        var filter = new ExcludeIdFilter() { From = -1 };
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => ModelValidationHelper.ValidateExcludedIdFilter(filter));
+    }
+
+
+    [Test]
+    public void ValidateExcludedIdFilter_WhenValidFilter_Should_Not_ThrowException()
+    {
+        // Arrange
+        var filter = new ExcludeIdFilter() { From = 1, ExcludedId = Guid.NewGuid() };
+
+        // Act and Assert
+        Assert.DoesNotThrow(() => ModelValidationHelper.ValidateExcludedIdFilter(filter));
+    }
+}
