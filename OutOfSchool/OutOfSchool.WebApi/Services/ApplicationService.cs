887d8f9fe0b891d3af0a61fb2313e0922bd98516
Dmytro M <dmytro.minochkin@gmail.com>
2022-12-30 17:00:04 +0200

    change search by parent to search by child Frontend did not specify correctly what they needed :)

--- OutOfSchool/OutOfSchool.WebApi/Services/ApplicationService.cs
+++ OutOfSchool/OutOfSchool.WebApi/Services/ApplicationService.cs
@@ -497,9 +497,9 @@ public class ApplicationService : IApplicationService, INotificationReciever
             var tempPredicate = PredicateBuilder.False<Application>();
             tempPredicate = tempPredicate
                 .Or(a => a.Workshop.Title == filter.SearchString)
-                .Or(a => a.Parent.User.FirstName.StartsWith(filter.SearchString, StringComparison.InvariantCultureIgnoreCase)
-                         || a.Parent.User.MiddleName.StartsWith(filter.SearchString, StringComparison.InvariantCultureIgnoreCase)
-                         || a.Parent.User.LastName.StartsWith(filter.SearchString, StringComparison.InvariantCultureIgnoreCase));
+                .Or(a => a.Child.FirstName.StartsWith(filter.SearchString, StringComparison.InvariantCultureIgnoreCase)
+                         || a.Child.MiddleName.StartsWith(filter.SearchString, StringComparison.InvariantCultureIgnoreCase)
+                         || a.Child.LastName.StartsWith(filter.SearchString, StringComparison.InvariantCultureIgnoreCase));
             predicate = predicate.And(tempPredicate);
         }
 
