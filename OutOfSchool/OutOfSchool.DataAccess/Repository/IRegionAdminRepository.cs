29c5262fe4056a0b2ba10cce962ac02a5040fe52
Andrii Tsylia <andrii.tsylia@outlook.com>
2023-01-06 17:32:29 +0200

    Add repository.

---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IRegionAdminRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.Services.Repository;
+
+public interface IRegionAdminRepository : IEntityRepository<(string, long), RegionAdmin>
+{
+    Task<RegionAdmin> GetByIdAsync(string userId);
+}
---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/RegionAdminRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.Services.Repository;
+
+public class RegionAdminRepository : EntityRepository<(string, long), RegionAdmin>, IRegionAdminRepository
+{
+    private readonly OutOfSchoolDbContext db;
+
+    public RegionAdminRepository(OutOfSchoolDbContext dbContext)
+        : base(dbContext)
+    {
+        db = dbContext;
+    }
+
+    public async Task<RegionAdmin> GetByIdAsync(string userId)
+    {
+        return await db.RegionAdmins
+            .SingleOrDefaultAsync(ra => ra.UserId == userId);
+    }
+}
