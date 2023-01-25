a67b49e39d7f862c7b4a648690e90d8374f2a910
Alexander Romanchuk <alrom@users.noreply.github.com>
2023-01-25 15:01:50 +0200

    Add headers for statistic reports (#976)

--- OutOfSchool/OutOfSchool.DataAccess/Enums/StatisticReportDataTypes.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Enums/StatisticReportDataTypes.cs
@@ -10,3 +10,28 @@ public enum StatisticReportDataTypes
     XLSX,
     HTML,
 }
+
+public static class StatisticReportDataTypesExtensions
+{
+    public static string GetContentType(this StatisticReportDataTypes value)
+    {
+        return value switch
+        {
+            StatisticReportDataTypes.CSV => "text/csv",
+            StatisticReportDataTypes.HTML => "text/html",
+            StatisticReportDataTypes.XLSX => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            _ => string.Empty
+        };
+    }
+
+    public static string GetFileFormat(this StatisticReportDataTypes value)
+    {
+        return value switch
+        {
+            StatisticReportDataTypes.CSV => ".csv",
+            StatisticReportDataTypes.HTML => ".html",
+            StatisticReportDataTypes.XLSX => ".xlsx",
+            _ => string.Empty
+        };
+    }
+}
