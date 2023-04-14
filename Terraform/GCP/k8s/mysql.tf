627a99db84d182b4244c3105b346d2194239a898
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2023-04-14 12:57:26 +0300

    Dmin/streamline dependencies (#1044)
    * begin to streamline dependencies
    
    * remove outdated readme
    
    * chart type fix

--- Terraform/GCP/k8s/mysql.tf
+++ Terraform/GCP/k8s/mysql.tf
@@ -18,6 +18,20 @@ resource "helm_release" "mysql" {
   depends_on = [
     kubernetes_secret.sql_credentials,
     kubernetes_secret.sql_api_credentials,
+    helm_release.ingress
+  ]
+}
+
+resource "helm_release" "redis" {
+  name          = "redis"
+  chart         = "../../k8s/infrastructure/charts/redis-17.9.3.tgz"
+  namespace     = data.kubernetes_namespace.oos.metadata[0].name
+  wait          = true
+  wait_for_jobs = true
+  values = [
+    "${file("${path.module}/../../../k8s/infrastructure/redis.yaml")}"
+  ]
+  depends_on = [
     kubernetes_secret.redis_credentials,
     helm_release.ingress
   ]
--- clear-hosts.ps1
+++
@@ -1,3 +0,0 @@
-$CurrentHosts = Get-Content -Path $env:windir\System32\drivers\etc\hosts
-$NewHosts = $CurrentHosts | Where-Object {$_ -notmatch 'oos.local'}
-Set-Content -Path $env:windir\System32\drivers\etc\hosts -Value $NewHosts -Force
