e7648fb51b91d72ccf308181ec2786a28a0daf7a
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2023-04-26 14:29:24 +0300

    Add validation to DateOfBirh

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Validators/RegisterViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using OutOfSchool.IdentityServer.ViewModels;
+
+namespace OutOfSchool.IdentityServer.Validators;
+
+public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel>
+{
+    public RegisterViewModelValidator(IStringLocalizer<SharedResource> localizer)
+    {
+        RuleFor(x => x.DateOfBirth)
+          .Must(BeLessThenToday).WithMessage(localizer["You must be at least 18 years old."]);
+    }
+
+    private bool BeLessThenToday(DateTime birthDate) => birthDate < DateTime.Now;
+}
--- OutOfSchool/OutOfSchool.IdentityServer/ViewModels/RegisterViewModel.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using OutOfSchool.Common;
+﻿using System.ComponentModel.DataAnnotations;
 using OutOfSchool.Services.Enums;
 
 namespace OutOfSchool.IdentityServer.ViewModels;
