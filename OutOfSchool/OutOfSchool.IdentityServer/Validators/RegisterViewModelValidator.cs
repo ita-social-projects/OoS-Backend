543ffd2ad0aa7f2074bab74f2feb93f2d67f2bb0
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2023-04-26 15:10:38 +0300

    MY mistake was fixed

--- OutOfSchool/OutOfSchool.IdentityServer/Validators/RegisterViewModelValidator.cs
+++ OutOfSchool/OutOfSchool.IdentityServer/Validators/RegisterViewModelValidator.cs
@@ -9,8 +9,8 @@ public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel>
     public RegisterViewModelValidator(IStringLocalizer<SharedResource> localizer)
     {
         RuleFor(x => x.DateOfBirth)
-          .Must(BeLessThenToday).WithMessage(localizer["You must be at least 18 years old."]);
+          .Must(BeLessThanToday).WithMessage(localizer["You must be at least 18 years old."]);
     }
 
-    private bool BeLessThenToday(DateTime birthDate) => birthDate < DateTime.Now;
+    private bool BeLessThanToday(DateTime birthDate) => birthDate < DateTime.Now;
 }
