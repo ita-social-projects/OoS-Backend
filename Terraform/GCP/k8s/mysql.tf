597c1f9a247a3daec69fc9815d20acafe4c2fdac
Dmytro M <dmytro.minochkin@gmail.com>
2023-04-28 20:27:45 +0300

    change MySQL to use operator

--- Terraform/GCP/k8s/mysql.tf
+++ Terraform/GCP/k8s/mysql.tf
@@ -1,6 +1,31 @@
+resource "helm_release" "mysql_operator" {
+  name             = "mysql-operator"
+  chart            = "../../k8s/infrastructure/charts/mysql-operator-2.0.9.tgz"
+  namespace        = "mysql-operator"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = true
+}
+
+resource "kubernetes_persistent_volume_claim" "backup_pvc" {
+  metadata {
+    name      = "mysql-backup-pvc"
+    namespace = data.kubernetes_namespace.oos.metadata[0].name
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+    storage_class_name = "standard"
+  }
+}
+
 resource "helm_release" "mysql" {
   name          = "mysql"
-  chart         = "../../k8s/database"
+  chart         = "../../k8s/infrastructure/charts/mysql-innodbcluster-2.0.9.tgz"
   namespace     = data.kubernetes_namespace.oos.metadata[0].name
   wait          = true
   wait_for_jobs = true
@@ -8,31 +33,12 @@ resource "helm_release" "mysql" {
     "${file("${path.module}/values/mysql.yaml")}"
   ]
   set {
-    name  = "phpmyadmin.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/whitelist-source-range"
-    value = join("\\,", var.admin_ips)
-  }
-  set {
-    name  = "phpmyadmin.ingress.hostname"
-    value = var.phpmyadmin_hostname
+    name  = "credentials.root.password"
+    value = var.sql_root_pass
   }
   depends_on = [
-    kubernetes_secret.sql_credentials,
-    kubernetes_secret.sql_api_credentials,
-    helm_release.ingress
-  ]
-}
-
-resource "helm_release" "redis" {
-  name          = "redis"
-  chart         = "../../k8s/infrastructure/charts/redis-17.9.3.tgz"
-  namespace     = data.kubernetes_namespace.oos.metadata[0].name
-  wait          = true
-  wait_for_jobs = true
-  values = [
-    "${file("${path.module}/../../../k8s/infrastructure/redis.yaml")}"
-  ]
-  depends_on = [
-    kubernetes_secret.redis_credentials,
-    helm_release.ingress
+    helm_release.mysql_operator,
+    helm_release.ingress,
+    kubernetes_persistent_volume_claim.backup_pvc
   ]
 }
