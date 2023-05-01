129bd8a2ad795842cf60ca7f8a5be64f37536933
Oleksandr Vovk <oleksand.vovk@gmail.com>
2023-05-01 21:09:13 +0300

    Move migrations to separate assembly

---
+++ OutOfSchool/OutOfSchool.Migrations/Program.cs
@@ -0,0 +1,93 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MySqlConnector;
+using OutOfSchool.Common;
+using OutOfSchool.Common.Config;
+using OutOfSchool.Common.Extensions;
+using OutOfSchool.Common.Extensions.Startup;
+using OutOfSchool.IdentityServer.KeyManagement;
+using OutOfSchool.Services;
+using OutOfSchool.Services.Models;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var config = context.Configuration;
+        // TODO: Move version check into an extension to reuse code across apps
+        var mySQLServerVersion = config["MySQLServerVersion"];
+        var serverVersion = new MySqlServerVersion(new Version(mySQLServerVersion));
+        if (serverVersion.Version.Major < Constants.MySQLServerMinimalMajorVersion)
+        {
+            throw new Exception("MySQL Server version should be 8 or higher.");
+        }
+
+        var connectionString = config.GetMySqlConnectionString<MySqlGuidConnectionOptions>(
+            "DefaultConnection",
+            options => new MySqlConnectionStringBuilder
+            {
+                Server = options.Server,
+                Port = options.Port,
+                UserID = options.UserId,
+                Password = options.Password,
+                Database = options.Database,
+                GuidFormat = options.GuidFormat.ToEnum(MySqlGuidFormat.Default),
+            });
+
+        var migrationsAssembly = "OutOfSchool.Migrations";
+        services
+            .AddDbContext<OutOfSchoolDbContext>(options => options
+                .UseMySql(
+                    connectionString,
+                    serverVersion,
+                    optionsBuilder =>
+                        optionsBuilder
+                            .EnableRetryOnFailure(3, TimeSpan.FromSeconds(5), null)
+                            .MigrationsAssembly(migrationsAssembly)));
+
+        services.AddIdentity<User, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 8;
+        })
+            .AddEntityFrameworkStores<OutOfSchoolDbContext>();
+
+        services.AddIdentityServer(options => { options.IssuerUri = config["Uri"]; })
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseMySql(
+                        connectionString,
+                serverVersion,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseMySql(
+                        connectionString,
+                serverVersion,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                options.EnableTokenCleanup = true;
+                options.TokenCleanupInterval = 3600;
+            })
+            .AddAspNetIdentity<User>()
+            .AddCustomKeyManagement<CertificateDbContext>(builder =>
+                builder.UseMySql(
+                    connectionString,
+            serverVersion,
+                    sql => sql.MigrationsAssembly(migrationsAssembly)));
+    })
+    .Build();
+
+using var scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+
+scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+scope.ServiceProvider.GetRequiredService<CertificateDbContext>().Database.Migrate();
+scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+scope.ServiceProvider.GetRequiredService<OutOfSchoolDbContext>().Database.Migrate();
