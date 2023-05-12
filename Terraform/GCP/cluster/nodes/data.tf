af277bb3ed9faefe367d13de1874eb7932b5d457
Dmytro M <dmytro.minochkin@gmail.com>
2023-05-12 15:47:15 +0300

    update ubuntu image

--- Terraform/GCP/cluster/nodes/data.tf
+++ Terraform/GCP/cluster/nodes/data.tf
@@ -1,5 +1,5 @@
 data "google_compute_image" "ubuntu" {
-  name = "ubuntu-2204-jammy-v20230302"
+  name = "ubuntu-2204-jammy-v20230429"
   # family = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud"
 }
