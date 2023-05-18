cd0e0ca197e1152c1e1782e4da96978f609dcbc9
Dmytro M <dmytro.minochkin@gmail.com>
2023-05-18 17:24:34 +0300

    fixed empty string treated as null in codeficator search

--- OutOfSchool/OutOfSchool.WebApi/Models/Codeficator/CodeficatorFilter.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/Codeficator/CodeficatorFilter.cs
@@ -1,9 +1,11 @@
-﻿using OutOfSchool.Common.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using OutOfSchool.Common.Enums;
 
 namespace OutOfSchool.WebApi.Models.Codeficator;
 
 public class CodeficatorFilter
 {
+    [DisplayFormat(ConvertEmptyStringToNull = false)]
     public string Name { get; set; } = string.Empty;
 
     /// <summary>
