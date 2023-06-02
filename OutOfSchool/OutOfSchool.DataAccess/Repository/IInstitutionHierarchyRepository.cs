97d79d47b90dcea229f9e9aa44865f60348c24f7
andriitsylia <55110945+andriitsylia@users.noreply.github.com>
2023-06-02 15:36:49 +0300

    Fix InstitutionHierarchy entity update. (#1117)

---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IInstitutionHierarchyRepository.cs
@@ -0,0 +1,12 @@
+﻿using OutOfSchool.Services.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using OutOfSchool.Services.Models.SubordinationStructure;
+
+namespace OutOfSchool.Services.Repository;
+
+public interface IInstitutionHierarchyRepository : IEntityRepositoryBase<Guid, InstitutionHierarchy>
+{
+    Task<InstitutionHierarchy> Update(InstitutionHierarchy entity, List<long> directionsIds);
+}
