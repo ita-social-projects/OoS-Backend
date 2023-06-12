6808e3d31db64ce1e9b6b721c59685304fdd33ca
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2023-06-12 23:16:30 +0300

    Fix smell

--- OutOfSchool/OutOfSchool.WebApi/Common/StatusPermissions/StatusPermissions.cs
+++ OutOfSchool/OutOfSchool.WebApi/Common/StatusPermissions/StatusPermissions.cs
@@ -38,19 +38,19 @@ public class StatusPermissions<T>
             && (p.SubRole == subRole || p.SubRole == "all")
             && (Convert.ToInt32(p.FromStatus) == Convert.ToInt32(from) || Convert.ToInt32(p.FromStatus) == 0)
             && (Convert.ToInt32(p.ToStatus) == Convert.ToInt32(to) || Convert.ToInt32(p.ToStatus) == 0)
-            && !p.Allowed
-        );
+            && !p.Allowed);
 
         if (denyResult)
+        {
             return false;
+        }
 
         var allowResult = statusPermissionsList.Any(p =>
             (p.Role == role || p.Role == "all")
             && (p.SubRole == subRole || p.SubRole == "all")
             && (Convert.ToInt32(p.FromStatus) == Convert.ToInt32(from) || Convert.ToInt32(p.FromStatus) == 0)
             && (Convert.ToInt32(p.ToStatus) == Convert.ToInt32(to) || Convert.ToInt32(p.ToStatus) == 0)
-            && p.Allowed
-        );
+            && p.Allowed);
 
         return allowResult;
     }
