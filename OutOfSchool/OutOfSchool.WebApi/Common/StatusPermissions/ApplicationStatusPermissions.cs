c032a5269d764d215434a5ce41ff23328b97e81f
Alexander Romanchuk <alexander.romanchuk@gmail.com>
2023-06-14 19:16:01 +0300

    Fix smth

--- OutOfSchool/OutOfSchool.WebApi/Common/StatusPermissions/ApplicationStatusPermissions.cs
+++ OutOfSchool/OutOfSchool.WebApi/Common/StatusPermissions/ApplicationStatusPermissions.cs
@@ -4,48 +4,10 @@ namespace OutOfSchool.WebApi.Common.StatusPermissions;
 
 public class ApplicationStatusPermissions : StatusPermissions<ApplicationStatus>
 {
-    public ApplicationStatusPermissions()
-    {
-        // allow statuses for Admin/TechAdmin (MinistryAdmin, RegionAdmin, AreaAdmin the same)
-        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Pending);
-        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
-        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
-        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
-
-        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.Pending);
-        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
-        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
-        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
-
-        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.Pending);
-        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
-        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
-        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
-
-        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.Pending);
-        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
-        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
-        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
-
-        // allow statuses for Parent
-        AllowStatusChange("parent", "all", fromStatus: ApplicationStatus.Pending, toStatus: ApplicationStatus.Left);
-        AllowStatusChange("parent", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Left);
-        AllowStatusChange("parent", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Left);
-
-        // allow statuses for Provider
-        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Completed);
-        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
-        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
-        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Rejected);
-
-        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.Approved);
-        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.StudyingForYears);
-        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.Completed);
-        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Left, toStatus: ApplicationStatus.StudyingForYears);
-    }
-
     public void InitDefaultPermissions()
     {
+        InitCommonPermissions();
+
         DenyStatusChange("all", "all", toStatus: ApplicationStatus.AcceptedForSelection);
 
         // allow statuses for Admin/TechAdmin
@@ -96,6 +58,8 @@ public class ApplicationStatusPermissions : StatusPermissions<ApplicationStatus>
 
     public void InitCompetitiveSelectionPermissions()
     {
+        InitCommonPermissions();
+
         // allow statuses for Admin/TechAdmin
         AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.Pending);
         AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.AcceptedForSelection, toStatus: ApplicationStatus.Approved);
@@ -140,5 +104,45 @@ public class ApplicationStatusPermissions : StatusPermissions<ApplicationStatus>
         AllowStatusChange("provider", "providerddmin", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Pending);
         AllowStatusChange("provider", "providerddmin", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Pending);
         AllowStatusChange("provider", "providerddmin", fromStatus: ApplicationStatus.AcceptedForSelection, toStatus: ApplicationStatus.StudyingForYears);
+    }
+
+    private void InitCommonPermissions()
+    {
+        // allow statuses for Admin/TechAdmin (MinistryAdmin, RegionAdmin, AreaAdmin the same)
+        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Pending);
+        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
+        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
+        AllowStatusChange("techadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
+
+        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.Pending);
+        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
+        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
+        AllowStatusChange("ministryadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
+
+        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.Pending);
+        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
+        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
+        AllowStatusChange("regionadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
+
+        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.Pending);
+        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.StudyingForYears);
+        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
+        AllowStatusChange("areaadmin", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
+
+        // allow statuses for Parent
+        AllowStatusChange("parent", "all", fromStatus: ApplicationStatus.Pending, toStatus: ApplicationStatus.Left);
+        AllowStatusChange("parent", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Left);
+        AllowStatusChange("parent", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Left);
+
+        // allow statuses for Provider
+        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Completed);
+        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.Approved, toStatus: ApplicationStatus.Rejected);
+        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Completed);
+        AllowStatusChange("provider", "all", fromStatus: ApplicationStatus.StudyingForYears, toStatus: ApplicationStatus.Rejected);
+
+        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.Approved);
+        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.StudyingForYears);
+        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Rejected, toStatus: ApplicationStatus.Completed);
+        AllowStatusChange("provider", "none", fromStatus: ApplicationStatus.Left, toStatus: ApplicationStatus.StudyingForYears);
     }
 }
