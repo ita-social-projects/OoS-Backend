c5da7a041f3613966be42eb97ad7a373228feb9e
Andrii Tsylia <andrii.tsylia@outlook.com>
2023-06-25 20:24:33 +0300

    Accepting the terms of use and processing of personal data during registration.

---
+++ OutOfSchool/OutOfSchool.IdentityServer/Controllers/GDPRController.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using OutOfSchool.Services.Enums;
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.IdentityServer.Controllers;
+public class GDPRController : Controller
+{
+    public IActionResult PersonalData()
+    {
+        return View();
+    }
+
+    public IActionResult ParentTerms()
+    {
+        return View();
+    }
+
+    public IActionResult ProviderTerms()
+    {
+        return View();
+    }
+}
