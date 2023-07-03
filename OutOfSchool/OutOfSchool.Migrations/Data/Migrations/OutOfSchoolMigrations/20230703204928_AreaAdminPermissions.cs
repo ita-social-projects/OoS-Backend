666b1772b5cb6980d1fcb5ce69e45a2c79c6d9e2
ZdorovenkoKyrylo <92879521+ZdorovenkoKyrylo@users.noreply.github.com>
2023-07-03 23:51:18 +0300

    AreaAdminPermissionsMigration

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20230703204928_AreaAdminPermissions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
+{
+    public partial class AreaAdminPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PermissionsForRoles",
+                columns: new[] { "Id", "Description", "PackedPermissions", "RoleName" },
+                values: new object[] { 7L, "area admin permissions", "ef\n257(PQFT[", "AreaAdmin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 7L);
+        }
+    }
+}
