8f346982f344192c6a1b9c34fbadec3f80264491
Alexander Romanchuk <alrom@users.noreply.github.com>
2023-07-03 10:15:58 +0300

    Provider filter was expanded (#1137)

--- OutOfSchool/OutOfSchool.WebApi/Models/Providers/ProviderFilter.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/Providers/ProviderFilter.cs
@@ -6,6 +6,10 @@ namespace OutOfSchool.WebApi.Models.Providers;
 
 public class ProviderFilter : SearchStringFilter
 {
+    public Guid InstitutionId { get; set; } = Guid.Empty;
+
+    public long CATOTTGId { get; set; } = 0;
+
     public IReadOnlyCollection<ProviderStatus> Status { get; set; } = new List<ProviderStatus>();
 
     public IReadOnlyCollection<ProviderLicenseStatus> LicenseStatus { get; set; } = new List<ProviderLicenseStatus>();
