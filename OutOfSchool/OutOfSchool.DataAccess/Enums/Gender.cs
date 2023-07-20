68271545a20292a5b0d19fee87f996dc4fa58400
ZdorovenkoKyrylo <92879521+ZdorovenkoKyrylo@users.noreply.github.com>
2023-07-20 17:14:37 +0300

    Fix children creating

--- OutOfSchool/OutOfSchool.DataAccess/Enums/Gender.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Enums/Gender.cs
@@ -1,5 +1,9 @@
-﻿namespace OutOfSchool.Services.Enums;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
+namespace OutOfSchool.Services.Enums;
+
+[JsonConverter(typeof(StringEnumConverter))]
 public enum Gender
 {
     Male,
