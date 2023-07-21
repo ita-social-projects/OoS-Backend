24e240d3c293286b96f7b1e29f147278bf8ed55e
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2023-07-21 13:41:20 +0300

    updated registration to use jquery (#1158)
    * updated registration to use jquery
    
    * add versioning to js/css

--- OutOfSchool/OutOfSchool.AuthCommon/wwwroot/js/login.js
+++ OutOfSchool/OutOfSchool.AuthCommon/wwwroot/js/login.js
@@ -1,15 +1,16 @@
-let loginPasswordEye = document.getElementById('login_password_eye');
-let loginPassword = document.getElementById('login_password');
-let check_loginPasswordEye = false;
+$(function() {
+    let check_loginPasswordEye = false;
+    const $loginPassword = $("#login_password");
 
-loginPasswordEye.addEventListener('click', function () {
-    if (check_loginPasswordEye) {
-        loginPasswordEye.src = "../_content/auth/icons/ic_eye.svg";
-        check_loginPasswordEye = false;
-        loginPassword.setAttribute("type", "Password");  
-    } else {
-        loginPasswordEye.src = "../_content/auth/icons/eye.svg";
-        check_loginPasswordEye = true;
-        loginPassword.setAttribute("type", "Text");  
-    }
-})
+    $("#login_password_eye").on("click", function () {
+        if (check_loginPasswordEye) {
+            $(this).attr("src", "../_content/auth/icons/ic_eye.svg");
+            check_loginPasswordEye = false;
+            $loginPassword.attr("type", "Password");
+        } else {
+            $(this).attr("src", "../_content/auth/icons/eye.svg");
+            check_loginPasswordEye = true;
+            $loginPassword.attr("type", "Text");
+        }
+    });
+});
