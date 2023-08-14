dfd09673b252ea8a7348951389be9e2e1b19bc94
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2023-08-15 00:21:38 +0300

    Add IChatMessageRepository, ChatMessageRepository

---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/ChatMessageRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.Models.ChatWorkshop;
+
+namespace OutOfSchool.Services.Repository;
+
+public class ChatMessageRepository : SensitiveEntityRepository<ChatMessageWorkshop>, IChatMessageRepository
+{
+    private readonly OutOfSchoolDbContext db;
+
+    public ChatMessageRepository(OutOfSchoolDbContext dbContext)
+        : base(dbContext)
+    {
+        db = dbContext;
+    }
+
+    public async Task<int> CountUnreadMessages(Guid workshopId)
+    {
+        return await db.ChatMessageWorkshops
+            .Include(chr => chr.ChatRoom)
+            .Where(x => x.ChatRoom.WorkshopId == workshopId && x.ReadDateTime == null && !x.SenderRoleIsProvider)
+            .CountAsync();
+    }
+}
---
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IChatMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Models.ChatWorkshop;
+
+namespace OutOfSchool.Services.Repository;
+
+public interface IChatMessageRepository : ISensitiveEntityRepository<ChatMessageWorkshop>
+{
+    Task<int> CountUnreadMessages(Guid workshopId);
+}
