63881593bcc89acc673bfda541eef83b99f528d2
Andrii Tsylia <andrii.tsylia@outlook.com>
2023-08-22 09:55:14 +0300

    Fixed bug

--- OutOfSchool/OutOfSchool.AuthCommon/Services/ProviderAdminService.cs
+++ OutOfSchool/OutOfSchool.AuthCommon/Services/ProviderAdminService.cs
@@ -507,7 +507,7 @@ public class ProviderAdminService : IProviderAdminService
             .GetByFilter(x => x.ProviderId == providerId && x.BlockingType != BlockingType.Manually)
             .ConfigureAwait(false);
 
-        providerAdmins.Select(async providerAdmin =>
+        foreach (var providerAdmin in providerAdmins)
         {
             var response = await BlockProviderAdminAsync(providerAdmin.UserId, userId, requestId, isBlocked);
 
@@ -531,7 +531,7 @@ public class ProviderAdminService : IProviderAdminService
                     response.HttpStatusCode,
                     requestId);
             }
-        });
+        }
 
         return mainResponse;
     }
