964b1432119533c6529f40c237d19e9f7884cf14
Semkiv Andriy <semkivandriy@gmail.com>
2023-09-22 13:33:59 +0300

    Added ChatRoomId parameter to messageCreateDto

--- OutOfSchool/OutOfSchool.WebApi/Models/ChatWorkshop/ChatMessageWorkshopCreateDto.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/ChatWorkshop/ChatMessageWorkshopCreateDto.cs
@@ -7,12 +7,16 @@ namespace OutOfSchool.WebApi.Models.ChatWorkshop;
 public class ChatMessageWorkshopCreateDto
 {
     [Required]
-    [JsonProperty("WorkshopId", Required = Required.Always)]
+    [JsonProperty("ChatRoomId", Required = Required.Always)]
     public Guid WorkshopId { get; set; }
 
     [Required]
     [JsonProperty("ParentId", Required = Required.Always)]
     public Guid ParentId { get; set; }
+
+    [Required]
+    [JsonProperty("ChatRoomId", Required = Required.Always)]
+    public Guid ChatRoomId { get; set; }
 
     [Required]
     [MaxLength(Constants.ChatMessageTextMaxLength)]
