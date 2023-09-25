1ecff271e373534f33fba3442e01b88a30655b4b
vitaliyyuras <vitaliyyuras@gmail.com>
2023-09-25 16:52:01 +0300

    Removed all validations in BlockPhoneNumber.

--- OutOfSchool/OutOfSchool.WebApi/Models/Providers/ProviderDto.cs
+++ OutOfSchool/OutOfSchool.WebApi/Models/Providers/ProviderDto.cs
@@ -9,12 +9,5 @@ public class ProviderDto : ProviderBaseDto
     [EnumDataType(typeof(OwnershipType), ErrorMessage = Constants.EnumErrorMessage)]
     public OwnershipType Ownership { get; set; }
 
-    [DataType(DataType.PhoneNumber)]
-    [RegularExpression(
-       Constants.PhoneNumberRegexModel,
-       ErrorMessage = Constants.PhoneErrorMessage)]
-    [DisplayFormat(DataFormatString = Constants.PhoneNumberFormat)]
-    [MaxLength(Constants.UnifiedPhoneLength)]
-    [Required(ErrorMessage = "PhoneNumber is required")]
     public string BlockPhoneNumber { get; set; } = string.Empty;
 }
