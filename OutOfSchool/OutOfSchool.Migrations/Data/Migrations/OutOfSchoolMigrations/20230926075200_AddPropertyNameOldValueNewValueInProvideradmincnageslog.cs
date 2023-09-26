4b565620cbf5e716041a343f8016fbf0472aa5ef
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2023-09-26 11:09:34 +0300

    Add migration to add columns PropertyName, OldValue, NewValue in ProviderAdminChangesLog table

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20230926075200_AddPropertyNameOldValueNewValueInProvideradmincnageslog.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations;
+
+public partial class AddPropertyNameOldValueNewValueInProvideradmincnageslog : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "PropertyName",
+            table: "ProviderAdminChangesLog",
+            type: "varchar(128)",
+            maxLength: 128,
+            nullable: false)
+            .Annotation("MySql:CharSet", "utf8mb4");
+        migrationBuilder.AddColumn<string>(
+            name: "OldValue",
+            table: "ProviderAdminChangesLog",
+            type: "varchar(500)",
+            maxLength: 500,
+            nullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4");
+        migrationBuilder.AddColumn<string>(
+            name: "NewValue",
+            table: "ProviderAdminChangesLog",
+            type: "varchar(500)",
+            maxLength: 500,
+            nullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4");
+    }
+}
