0d770215013dbf048a37d3b156f792a2cec9ddfa
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2023-11-13 13:33:09 +0200

    Add IParentBlockedByAdminLogService

---
+++ OutOfSchool/OutOfSchool.WebApi/Services/IParentBlockedByAdminLogService.cs
@@ -0,0 +1,20 @@
+﻿namespace OutOfSchool.WebApi.Services;
+
+public interface IParentBlockedByAdminLogService
+{
+    /// <summary>
+    /// Create changes log for the given ParentId entity.
+    /// </summary>
+    /// <param name="parentId"></param>
+    /// <param name="userId">ID of user that performs the change.</param>
+    /// <param name="reason"></param>
+    /// <param name="oldValue">Old value of the property that is changing.</param>
+    /// <param name="newValue">New value of the property that is changing.</param>
+    /// <returns>Number of the added log records.</returns>
+    Task<int> SaveChangesLogAsync(
+        Guid parentId,
+        string userId,
+        string reason,
+        string oldValue,
+        string newValue);
+}
