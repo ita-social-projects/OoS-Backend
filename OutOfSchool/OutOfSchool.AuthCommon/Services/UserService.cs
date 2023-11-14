01e4f8ae1ead05b92767c5709d44fcb412b76d0d
Semkiv Andriy <semkivandriy@gmail.com>
2023-11-14 11:12:00 +0200

    added token revoke after deleting user

--- OutOfSchool/OutOfSchool.AuthCommon/Services/UserService.cs
+++ OutOfSchool/OutOfSchool.AuthCommon/Services/UserService.cs
@@ -1,35 +1,39 @@
-﻿namespace OutOfSchool.AuthCommon.Services;
+﻿using OpenIddict.Abstractions;
+
+namespace OutOfSchool.AuthCommon.Services;
 public class UserService : IUserService
 {
     private readonly UserManager<User> userManager;
-    private readonly SignInManager<User> signInManager;
-    //private readonly IAuthenticationManager authenticationManager; це якийсь наче старий менеджер
+    private readonly ILogger<UserService> logger;
+    private readonly IOpenIddictTokenManager tokenManager;
 
-    public UserService(UserManager<User> userManager, SignInManager<User> signInManager/*, AuthenticationManager authenticationManager*/)
+    public UserService(UserManager<User> userManager, ILogger<UserService> logger, IOpenIddictTokenManager tokenManager)
     {
         this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
-        this.signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
-        //this.authenticationManager = authenticationManager ?? throw new ArgumentNullException(nameof(authenticationManager));
+        this.logger = logger;
+        this.tokenManager = tokenManager ?? throw new ArgumentNullException(nameof(tokenManager));
     }
 
     public async Task<ResponseDto> DeleteUserById(string userId)
     {
         var user = await userManager.FindByIdAsync(userId);
-        var result = await userManager.DeleteAsync(user); // цього достатньо, чи робити як у себе?
-        await signInManager.SignOutAsync(); // це карент юзер
-        //authenticationManager.
 
-        if (!result.Succeeded)
+        if (user is null)
         {
-            // error
+            logger.LogError("User with id : {userId} not found", userId);
+            return new ResponseDto() { HttpStatusCode = HttpStatusCode.NotFound };
+        }
+
+        var tokens = await tokenManager.FindBySubjectAsync(userId).ToListAsync();
+        foreach (var token in tokens)
+        {
+            await tokenManager.TryRevokeAsync(token);
         }
 
         return new ResponseDto()
         {
             HttpStatusCode = HttpStatusCode.OK,
             IsSuccess = true,
-            Result = result,
-            Message = "Success",
         };
     }
 }
