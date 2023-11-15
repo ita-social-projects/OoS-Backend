e5a54b79f4211ba929c512c572d55d9f1cd2cd59
vitaliyyuras <vitaliyyuras@gmail.com>
2023-11-15 16:26:12 +0200

    Revert "Added ProviderWorkshopDto"
    This reverts commit 84412b7b19e606150339956f3ddb8f69350cccb6.

--- OutOfSchool/OutOfSchool.WebApi/Models/ProviderWorkshopDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using OutOfSchool.WebApi.Models.Providers;
-using OutOfSchool.WebApi.Models.Workshops;
-
-namespace OutOfSchool.WebApi.Models;
-
-public class ProviderWorkshopDto: ProviderDto
-{
-    public List<WorkshopDto> Workshops { get; set; }
-}
