84412b7b19e606150339956f3ddb8f69350cccb6
vitaliyyuras <vitaliyyuras@gmail.com>
2023-11-15 16:23:18 +0200

    Added ProviderWorkshopDto

---
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/V1/ProviderWorkshopController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OutOfSchool.WebApi.Models;
+
+namespace OutOfSchool.WebApi.Controllers.V1;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]/[action]")]
+public class ProviderWorkshopController : ControllerBase
+{
+    private readonly IProviderWorkshopService externalProviderService;
+
+    public ProviderWorkshopController(IProviderWorkshopService externalProviderService)
+    {
+        this.externalProviderService = externalProviderService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<SearchResult<ProviderWorkshopDto>>> GetByFilter([FromQuery] DateTime updatedAfter, [FromQuery] int pageSize = 100)
+    {
+        try
+        {
+            var result = await externalProviderService.GetProvidersWithWorkshops(updatedAfter, pageSize);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
+    }
+
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ProviderWorkshopDto.cs
@@ -0,0 +1,9 @@
+﻿using OutOfSchool.WebApi.Models.Providers;
+using OutOfSchool.WebApi.Models.Workshops;
+
+namespace OutOfSchool.WebApi.Models;
+
+public class ProviderWorkshopDto: ProviderDto
+{
+    public List<WorkshopDto> Workshops { get; set; }
+}
