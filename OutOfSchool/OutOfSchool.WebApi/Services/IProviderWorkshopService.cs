4f9a08b5e6626d70d05bbf25d7dc39053c1e327f
vitaliyyuras <vitaliyyuras@gmail.com>
2023-11-15 16:23:04 +0200

    Added IProviderWorkshopService and ProviderWorkshopService

---
+++ OutOfSchool/OutOfSchool.WebApi/Services/IProviderWorkshopService.cs
@@ -0,0 +1,8 @@
+﻿using OutOfSchool.WebApi.Models;
+
+namespace OutOfSchool.WebApi.Services;
+
+public interface IProviderWorkshopService
+{
+    Task<SearchResult<ProviderWorkshopDto>> GetProvidersWithWorkshops(DateTime updatedAfter, int pageSize);
+}
