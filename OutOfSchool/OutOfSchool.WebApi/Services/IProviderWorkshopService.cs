730c6a003db064ff8ce16d0324ed7216ca4b3166
vitaliyyuras <vitaliyyuras@gmail.com>
2023-11-17 17:27:26 +0200

    Deleted unnecessary service

--- OutOfSchool/OutOfSchool.WebApi/Services/IProviderWorkshopService.cs
+++
@@ -1,8 +0,0 @@
-﻿using OutOfSchool.WebApi.Models;
-
-namespace OutOfSchool.WebApi.Services;
-
-public interface IProviderWorkshopService
-{
-    Task<SearchResult<ProviderWorkshopDto>> GetProvidersWithWorkshops(DateTime updatedAfter, int pageSize);
-}
