588e5ff33a201f3dec41b5758a6d449446e6e56e
vitaliyyuras <vitaliyyuras@gmail.com>
2023-11-23 17:58:13 +0200

    Fixed excessive 'using' directives.

--- OutOfSchool/OutOfSchool.DataAccess/Repository/IProviderRepository.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/IProviderRepository.cs
@@ -1,8 +1,7 @@
-using OutOfSchool.Services.Models;
-using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
+using System.Threading.Tasks;
+using OutOfSchool.Services.Models;
 
 namespace OutOfSchool.Services.Repository;
 
