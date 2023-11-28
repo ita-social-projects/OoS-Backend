2fb8e2666e38d3a1f37add6083dc7ae5cd601a01
Taranko Taras <tarastarnko2@gmail.com>
2023-11-28 20:52:29 +0200

    Aded models for errors

---
+++ OutOfSchool/OutOfSchool.Common/Responses/ApiError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OutOfSchool.Common.Responces;
+public class ApiError
+{
+    public ApiError(string code, string message)
+    {
+        if (string.IsNullOrEmpty(code))
+        {
+            throw new ArgumentException(@"Code must be non-empty value", nameof(code));
+        }
+
+        if (string.IsNullOrEmpty(message))
+        {
+            throw new ArgumentException(@"Message must be non-empty value", nameof(message));
+        }
+
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+
+    public string Message { get; }
+}
---
+++ OutOfSchool/OutOfSchool.Common/Responses/ApiErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace OutOfSchool.Common.Responces;
+public class ApiErrorResponse
+{
+    private List<ApiError> apiErrors;
+
+    public ApiErrorResponse(List<ApiError> apiErrors)
+    {
+        apiErrors = new List<ApiError>(apiErrors);
+    }
+
+    public List<ApiError> ApiErrors => apiErrors;
+
+    public void AddApiError(ApiError apiError)
+    {
+        apiErrors.Add(apiError);
+    }
+}
