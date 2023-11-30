b66414709dc157c8001f7179d4a524c0b8ca09e1
DmytroKymachynskyi <107883170+DKymachynskyi@users.noreply.github.com>
2023-11-30 17:41:27 +0200

    Kymachynskyi/ Add history of changes for Parent blocked by TechAdmin, MinistryAdmin (#1309)
    * Add DTO, request models, add GetParentBlockedByAdminChangesLogAsync method to IChangesLogService
    
    * Add method to ChangeLogController, ChangeLogService, enum ShowParents, fix Dto, request
    
    * Fix tests
    
    * Fix code smells
    
    * Minor fixes
    
    * Add tests for ChangesLogController
    
    * Add tests for ChangesLogService
    
    * Add tests for ChangesLogService, fix DateTo with DateTime.MaxValue issue
    
    * Add DateTime extension method NextDayStart, tests for it

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/Changes/ParentBlockedByAdminChangesLogDto.cs
@@ -0,0 +1,16 @@
+﻿namespace OutOfSchool.WebApi.Models.Changes;
+
+public class ParentBlockedByAdminChangesLogDto
+{
+    public Guid ParentId { get; set; }
+
+    public string ParentFullName { get; set; }
+
+    public ShortUserDto User { get; set; }
+
+    public DateTime OperationDate { get; set; }
+
+    public string Reason { get; set; }
+
+    public bool IsBlocked { get; set; }
+}
