5523c6bfc22b206f22d3b947db547419b31eee76
Taranko Taras <tarastarnko2@gmail.com>
2023-12-11 01:42:45 +0200

    Fixed structure of ApiErrorsTypes

--- OutOfSchool/OutOfSchool.Common/Responses/ApiErrorsTypes.cs
+++ OutOfSchool/OutOfSchool.Common/Responses/ApiErrorsTypes.cs
@@ -1,51 +1,14 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using OutOfSchool.Common.Responces;
+﻿using OutOfSchool.Common.Responces;
 
 namespace OutOfSchool.Common.Responses;
-public abstract class ApiErrorsTypes
+public static class ApiErrorsTypes
 {
-    private static string GetGroup()
+    public static class ProviderAdmin
     {
-        StackTrace stackTrace = new StackTrace();
-        var frames = stackTrace.GetFrames();
-        MethodBase method;
-
-        try
-        {
-            method = frames[1].GetMethod();
-        }
-        catch
-        {
-            return "undefined";
-        }
-
-        Type declaringType = method.DeclaringType;
-
-        string subGroup = declaringType.Name;
-        string group = declaringType.DeclaringType.Name;
-
-        return $"{group}/{subGroup}";
+        public static ApiError UserDontHavePermissionToCreate(string userId) =>
+        new ApiError(
+            $"{nameof(UserDontHavePermissionToCreate)}",
+            $"User(id): {userId} doesn't have permission to create provider admin",
+            $"{nameof(ProviderAdmin)}");
     }
-
-    public abstract class ProviderAdmin
-    {
-        private static int entityCode = 10;
-
-        public abstract class Creation
-        {
-            private static int actionCode = 1;
-
-            public static ApiError UserDontHavePermission(string userId) =>
-            new ApiError(
-                $"{entityCode}_{actionCode}",
-                $"User(id): {userId} doesn't have permission to create provider admin",
-                GetGroup());
-        }
-
-        // To be continued for another ProviderAdmin service actions
-    }
-
-    // To be continued for another entities
 }
