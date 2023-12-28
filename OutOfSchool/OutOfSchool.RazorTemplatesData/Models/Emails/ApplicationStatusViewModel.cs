5ce9cb7b9370da4528e1b8a128260ec53830f417
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2023-12-28 23:26:46 +0200

    Fix email code duplication issue

--- OutOfSchool/OutOfSchool.RazorTemplatesData/Models/Emails/ApplicationStatusViewModel.cs
+++ OutOfSchool/OutOfSchool.RazorTemplatesData/Models/Emails/ApplicationStatusViewModel.cs
@@ -1,19 +1,40 @@
-﻿
-using OutOfSchool.Services.Enums;
+﻿using OutOfSchool.Services.Enums;
 
 namespace OutOfSchool.RazorTemplatesData.Models.Emails;
 public class ApplicationStatusViewModel
 {
+    public const string UaMaleEnding = "ий";
+    public const string UaFemaleEnding = "а";
+    public const string UaUnspecifiedGenderEnding = "ий/a";
+
     public ApplicationStatus ApplicationStatus { get; set; }
 
     public string ChildFullName { get; set; }
 
     public Gender? ChildGender { get; set; }
 
+    public string UaEnding
+    {
+        get
+        {
+            if (ChildGender == Gender.Male)
+            {
+                return UaMaleEnding;
+            }
+            else if (ChildGender == Gender.Female)
+            {
+                return UaFemaleEnding;
+            }
+            else
+            {
+                return UaUnspecifiedGenderEnding;
+            };
+        } 
+    }
+
     public string WorkshopTitle { get; set; }
 
     public string WorkshopUrl { get; set; }
 
     public string RejectionMessage { get; set; } = string.Empty;
-
 }
