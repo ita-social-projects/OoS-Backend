25234e8efc72f8645505c8e7e87f8e2812ad832a
oleksSavchenko <oleksandr.savchenko12@gmail.com>
2024-02-19 14:02:54 +0200

    Changed healthcheck port of auth server to 9000

--- OutOfSchool/OutOfSchool.AuthorizationServer/Startup.cs
+++ OutOfSchool/OutOfSchool.AuthorizationServer/Startup.cs
@@ -226,6 +226,24 @@ public static class Startup
 
     public static void Configure(this WebApplication app)
     {
+        app.Use(async (context, next) =>
+        {
+            var httpRequest = context.Request;
+            var httpResponse = context.Response;
+
+            bool healthCheck = httpRequest.Path.Equals("/healthz/ready");
+
+            int healthPort = app.Configuration.GetValue<int>("ApplicationPorts:HealthPort");
+
+            if (httpRequest.HttpContext.Connection.LocalPort == healthPort && !healthCheck)
+            {
+                httpResponse.StatusCode = 404;
+                return;
+            }
+
+            await next();
+        });
+
         var proxyOptions = app.Configuration.GetSection(ReverseProxyOptions.Name).Get<ReverseProxyOptions>();
         app.UseProxy(proxyOptions);
         app.UseCors("AllowAllOrigins");
@@ -292,6 +310,7 @@ public static class Startup
                 Predicate = healthCheck => healthCheck.Tags.Contains("readiness"),
                 AllowCachingResponses = false,
             })
+            .RequireHost($"*:{app.Configuration.GetValue<int>("ApplicationPorts:HealthPort")}")
             .WithMetadata(new AllowAnonymousAttribute());
 
         app.UseEndpoints(endpoints =>
