a3cdefb04a422d8edb14516db63f586abff013f2
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2024-02-21 15:21:27 +0200

    Minor fix test

--- OutOfSchool/OutOfSchool.WebApi.Tests/Services/BlockedProviderParentServiceTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/BlockedProviderParentServiceTests.cs
@@ -120,6 +120,7 @@ public class BlockedProviderParentServiceTests
 
         // Assert
         blockedProviderParentRepositoryMock.VerifyAll();
+        parentRepositoryMock.VerifyAll();
         notificationServiceMock
             .Verify(
                 x => x.Create(
