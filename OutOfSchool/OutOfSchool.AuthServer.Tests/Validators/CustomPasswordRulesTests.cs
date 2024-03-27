a31164e2717f10ddbf8882622cee261c1a2ee351
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2024-03-27 12:24:00 +0200

    Add tests

---
+++ OutOfSchool/OutOfSchool.AuthServer.Tests/Validators/CustomPasswordRulesTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OutOfSchool.AuthCommon.Validators;
+
+namespace OutOfSchool.AuthServer.Tests.Validators;
+
+[TestFixture]
+public class CustomPasswordRulesTests
+{
+    [TestCase(null, false)]
+    [TestCase("", false)]
+    [TestCase("aaa", false)]
+    [TestCase("AAAAAAAA", false)]
+    [TestCase("Abbbbbbb", false)]
+    [TestCase("Ab1bbbbb", false)]
+    [TestCase("Ab1bbbb%", true)]
+    public void IsValidPassword(string password, bool expectedResult)
+    {
+        // Act
+        var result = CustomPasswordRules.IsValidPassword(password);
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+}
