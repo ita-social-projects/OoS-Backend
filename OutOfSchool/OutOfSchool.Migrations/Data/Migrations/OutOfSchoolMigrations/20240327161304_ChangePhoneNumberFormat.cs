0cc5157280399163ed21f459ba4ba806ab70796d
MrVenik <vladp1998@gmail.com>
2024-03-27 18:17:23 +0200

    Add migrations and fix phone number length
    - `HasMaxLength(Constants.MaxPhoneNumberLengthWithPlusSign);` - is actually redundant inside ProviderConfiguration, maybe it should be removed, because we already have MaxLength attribute.
    - Migrations contains a lot of `migrationBuilder.AlterColumn<DateTimeOffset>`, why this is here idk

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20240327161304_ChangePhoneNumberFormat.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
+{
+    /// <inheritdoc />
+    public partial class ChangePhoneNumberFormat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Workshops",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreationTime",
+                table: "Ratings",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastSuccessLaunch",
+                table: "QuartzJobs",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Providers",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BlockPhoneNumber",
+                table: "Providers",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "EventDateTime",
+                table: "OperationsWithObjects",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ReadDateTime",
+                table: "Notifications",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDateTime",
+                table: "Notifications",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "OperationDate",
+                table: "ElasticsearchSyncRecords",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ReadDateTime",
+                table: "ChatMessageWorkshops",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDateTime",
+                table: "ChatMessageWorkshops",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTimeTo",
+                table: "BlockedProviderParents",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTimeFrom",
+                table: "BlockedProviderParents",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastLogin",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatingTime",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreationTime",
+                table: "Applications",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ApprovedTime",
+                table: "Applications",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Workshops",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)",
+                oldMaxLength: 16)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreationTime",
+                table: "Ratings",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastSuccessLaunch",
+                table: "QuartzJobs",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Providers",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)",
+                oldMaxLength: 16)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BlockPhoneNumber",
+                table: "Providers",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)",
+                oldMaxLength: 16)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "EventDateTime",
+                table: "OperationsWithObjects",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ReadDateTime",
+                table: "Notifications",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDateTime",
+                table: "Notifications",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "OperationDate",
+                table: "ElasticsearchSyncRecords",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ReadDateTime",
+                table: "ChatMessageWorkshops",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDateTime",
+                table: "ChatMessageWorkshops",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTimeTo",
+                table: "BlockedProviderParents",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTimeFrom",
+                table: "BlockedProviderParents",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastLogin",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatingTime",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreationTime",
+                table: "Applications",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ApprovedTime",
+                table: "Applications",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
