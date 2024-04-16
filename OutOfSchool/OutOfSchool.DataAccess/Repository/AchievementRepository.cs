8664a0247952bf975c2ed52468fb18aed443f46b
Alexander Romanchuk <alrom@users.noreply.github.com>
2024-04-16 13:30:26 +0300

    Fix changing of achievement's teachers (#1453)
    * Fix changing of achievement's teachers
    
    * Add test
    
    * Fix test
    
    * Fix
    
    * Fix name

--- OutOfSchool/OutOfSchool.DataAccess/Repository/AchievementRepository.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/AchievementRepository.cs
@@ -77,7 +77,15 @@ public class AchievementRepository : EntityRepositorySoftDeleted<Guid, Achieveme
 
         newAchievement.Teachers.RemoveAll(x => !teachers.Contains(x.Title));
 
+        var teachersToRestore = newAchievement.Teachers.Where(x => teachers.Contains(x.Title) && x.IsDeleted);
+
+        foreach (var teacher in teachersToRestore)
+        {
+            teacher.IsDeleted = false;
+        }
+
         var exceptTeachers = teachers.Where(p => newAchievement.Teachers.TrueForAll(x => !x.Title.Equals(p)));
+
         foreach (var teacher in exceptTeachers)
         {
             newAchievement.Teachers.Add(new AchievementTeacher { Title = teacher, Achievement = newAchievement });
---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/Database/AchievementRepositoryTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using OutOfSchool.ElasticsearchData.Models;
+using OutOfSchool.Services;
+using OutOfSchool.Services.Models;
+using OutOfSchool.Services.Repository;
+using OutOfSchool.Tests.Common.TestDataGenerators;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.WebApi.Tests.Services.Database;
+
+[TestFixture]
+public class AchievementRepositoryTests
+{
+    private DbContextOptions<OutOfSchoolDbContext> dbContextOptions;
+    private Achievement achievement;
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<OutOfSchoolDbContext>()
+            .UseInMemoryDatabase(databaseName: "OutOfSchoolTestDB")
+            .UseLazyLoadingProxies()
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        using var context = GetContext();
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+
+    #region Update
+
+    [Test]
+    public async Task Update_WhenExistsDeletedTeachers_RestoreIntersected()
+    {
+        // Arrange
+        achievement = AchievementGenerator.Generate()
+            .WithAchievementTeachers();
+
+        var teachers = AchievementTeacherGenerator.Generate(3);
+        teachers[0].IsDeleted = true;
+        teachers[1].IsDeleted = true;
+        achievement.Teachers = teachers;
+
+        var children = ChildGenerator.Generate(2);
+        achievement.Children = children;
+
+        using var context = GetContext();
+        context.Add(achievement);
+        await context.SaveChangesAsync();
+
+        var achievementRepository = GetAchievementRepository(context);
+        var childrenIds = children.Select(x => x.Id).ToList();
+
+        var teacherTitles = new List<string>() { teachers[1].Title, teachers[2].Title, "New Test Title" };
+
+        // Act
+        var expectedAchievement = await achievementRepository.Update(achievement, childrenIds, teacherTitles);
+
+        // Assert
+        Assert.AreEqual(3, expectedAchievement.Teachers.Count);
+
+        var teachersFromDB = context.AchievementTeachers.Where(x => x.AchievementId == expectedAchievement.Id).ToList();
+        Assert.AreEqual(1, teachersFromDB.Count(x => x.IsDeleted));
+        Assert.AreEqual(3, teachersFromDB.Count(x => !x.IsDeleted));
+        Assert.AreEqual(expectedAchievement.Teachers.Select(x => x.Title), teacherTitles);
+    }
+
+    #endregion
+
+    #region private
+
+    private OutOfSchoolDbContext GetContext() => new OutOfSchoolDbContext(dbContextOptions);
+
+    private IAchievementRepository GetAchievementRepository(OutOfSchoolDbContext dbContext)
+        => new AchievementRepository(dbContext);
+
+    #endregion
+}
