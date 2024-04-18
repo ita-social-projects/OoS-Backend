58d155ff7b567fcd0e28f60fa71bd7f65e3f3ee4
Dmytro Kymacnynskyi <dskpochta@gmail.com>
2024-04-18 23:40:23 +0300

    Add test

---
+++ OutOfSchool/OutOfSchool.AuthServer.Tests/Services/Password/PasswordGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OutOfSchool.AuthCommon.Services.Password;
+using System.Linq;
+
+namespace OutOfSchool.AuthServer.Tests.Services.Password;
+
+[TestFixture]
+public class PasswordGeneratorTests
+{
+    [Test]
+    public void PasswordGenerator_ShouldReturnValidPassword()
+    {
+        // Act
+        var result = PasswordGenerator.GenerateRandomPassword();
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.That(result, Does.Match(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8}$"));
+    }
+}
