512dd71efe53d64e307bc24c0b789bc366c4e1c8
DmytroKymachynskyi <107883170+DKymachynskyi@users.noreply.github.com>
2024-04-24 15:40:50 +0300

    Kymachynskyi/ Fix password generation (#1459)
    * Fix GenerateRandomPassword
    
    * Use RandomNumberGenerator.GetInt32
    
    * Add test
    
    * Remove unnecessary using from test
    
    * Improve computational hardness
    
    * Fix test logic and readability
    
    * Improve randomness, memory usage, test
    
    * Improve performance
    
    * Minor fix
    
    * Add test
    
    * Fix test design and excessive using, add AdminGenerator.GenerateAreaAdminBaseDto

--- OutOfSchool/OutOfSchool.AuthCommon/Services/Password/PasswordGenerator.cs
+++ OutOfSchool/OutOfSchool.AuthCommon/Services/Password/PasswordGenerator.cs
@@ -1,65 +1,37 @@
-﻿namespace OutOfSchool.AuthCommon.Services.Password;
+﻿using System.Security.Cryptography;
+
+namespace OutOfSchool.AuthCommon.Services.Password;
 
 public static class PasswordGenerator
 {
+    private const string Uppercase = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+    private const string Lowercase = "abcdefghijkmnopqrstuvwxyz";
+    private const string Digits = "0123456789";
+    private const string AllPasswordChars =
+        Uppercase + Lowercase + Digits + Constants.ValidationSymbols;
+
+    private static readonly string[] AllowedCharSets =
+    [
+        Uppercase, Lowercase, Digits, Constants.ValidationSymbols,
+    ];
+
     /// <summary>
-    /// Generates a Random Password
-    /// respecting the given strength requirements.
+    /// Generates a Random Password.
     /// </summary>
-    /// <param name="passwordOptions">A valid PasswordOptions object
-    /// containing the password strength requirements.</param>
     /// <returns>A random password.</returns>
-    public static string GenerateRandomPassword(PasswordOptions passwordOptions)
+    public static string GenerateRandomPassword()
     {
-        string[] randomChars = new[]
+        Span<char> password = stackalloc char[Constants.PasswordMinLength];
+        int index = Constants.PasswordMinLength - AllowedCharSets.Length;
+        RandomNumberGenerator.GetItems(AllPasswordChars, password[..index]);
+        foreach (var charSet in AllowedCharSets)
         {
-            "ABCDEFGHJKLMNOPQRSTUVWXYZ",    // uppercase
-            "abcdefghijkmnopqrstuvwxyz",    // lowercase
-            "0123456789",                   // digits
-            "!@$?_-",                       // non-alphanumeric
-        };
-
-        CryptoRandom rand = new CryptoRandom();
-
-        List<char> chars = new List<char>();
-
-        if (passwordOptions.RequireUppercase)
-        {
-            chars.Insert(
-                rand.Next(0, chars.Count),
-                randomChars[0][rand.Next(0, randomChars[0].Length)]);
+            var rndIndex = RandomNumberGenerator.GetInt32(index);
+            (password[rndIndex], password[index]) =
+                (charSet[RandomNumberGenerator.GetInt32(charSet.Length)], password[rndIndex]);
+            index++;
         }
 
-        if (passwordOptions.RequireLowercase)
-        {
-            chars.Insert(
-                rand.Next(0, chars.Count),
-                randomChars[1][rand.Next(0, randomChars[1].Length)]);
-        }
-
-        if (passwordOptions.RequireDigit)
-        {
-            chars.Insert(
-                rand.Next(0, chars.Count),
-                randomChars[2][rand.Next(0, randomChars[2].Length)]);
-        }
-
-        if (passwordOptions.RequireNonAlphanumeric)
-        {
-            chars.Insert(
-                rand.Next(0, chars.Count),
-                randomChars[3][rand.Next(0, randomChars[3].Length)]);
-        }
-
-        for (int i = chars.Count; i < passwordOptions.RequiredLength
-                                  || chars.Distinct().Count() < passwordOptions.RequiredUniqueChars; i++)
-        {
-            string rcs = randomChars[rand.Next(0, randomChars.Length)];
-            chars.Insert(
-                rand.Next(0, chars.Count),
-                rcs[rand.Next(0, rcs.Length)]);
-        }
-
-        return new string(chars.ToArray());
+        return new string(password);
     }
 }
