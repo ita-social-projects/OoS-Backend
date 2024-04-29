eda9f8dd55b6d0666fcfd3fb2f2fa3c47dc13f8e
MrVenik <vladp1998@gmail.com>
2024-04-29 18:04:45 +0300

    Fix name of param and test for authcontroller

--- OutOfSchool/OutOfSchool.DataAccess/Models/Parent.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Parent.cs
@@ -40,12 +40,12 @@ public class Parent : IKeyedEntity<Guid>, ISoftDeleted, IHasUser
     /// Creates draft parent, that is later will be edited through API.
     /// </summary>
     /// <param name="userId">User id.</param>
-    /// <param name="currentTime">Current time.</param>
+    /// <param name="utcNow">Current UTC time.</param>
     /// <returns>New draft parent.</returns>
-    public static Parent CreateDraft(string userId, DateTime currentTime) => new()
+    public static Parent CreateDraft(string userId, DateTime utcNow) => new()
     {
         UserId = userId,
         Gender = Enums.Gender.Male,
-        DateOfBirth = currentTime.AddYears(-Constants.AdultAge),
+        DateOfBirth = utcNow.AddYears(-Constants.AdultAge),
     };
 }
