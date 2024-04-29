2c45945cc8f0fbc5072d786db22d3a13311730b9
MrVenik <vladp1998@gmail.com>
2024-04-29 14:41:49 +0300

    Add tests for Parent.CreateDraft method

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Models/ParentDraftCreationTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using OutOfSchool.Common;
+using OutOfSchool.Services.Enums;
+using OutOfSchool.Services.Models;
+
+namespace OutOfSchool.WebApi.Tests.Models;
+
+[TestFixture]
+public class ParentDraftCreationTests
+{
+    [Test]
+    public void CreateDraft_ReturnsMaleGender()
+    {
+        // Assert
+        Assert.AreEqual(Gender.Male, Parent.CreateDraft("test").Gender);
+    }
+
+    [Test]
+    public void CreateDraft_ReturnsMaleValidAge()
+    {
+        // Arrange
+        var expectedDate = DateTime.Now.AddYears(-Constants.AdultAge).Date;
+
+        // Assert
+        Assert.AreEqual(expectedDate, Parent.CreateDraft("test").DateOfBirth.GetValueOrDefault().Date);
+    }
+
+    [Test]
+    public void CreateDraft_ReturnsMaleValidUserId()
+    {
+        // Assert
+        Assert.AreEqual("test", Parent.CreateDraft("test").UserId);
+    }
+}
