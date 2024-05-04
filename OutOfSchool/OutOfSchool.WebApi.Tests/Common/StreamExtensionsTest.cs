a7d8478609f33112b0052759f58d1c1d1b2f9164
MrVenik <vladp1998@gmail.com>
2024-05-04 16:48:04 +0300

    Add more tests

--- OutOfSchool/OutOfSchool.WebApi.Tests/Common/StreamExtensionsTest.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Common/StreamExtensionsTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using OutOfSchool.BusinessLogic.Extensions;
 
@@ -9,6 +11,8 @@ namespace OutOfSchool.WebApi.Tests.Common;
 [TestFixture]
 public class StreamExtensionsTest
 {
+    private sealed record TestObject(string Property);
+
     [Test]
     public void ReadAndDeserializeFromJson_WhenStreamIsNull_ThrowArgumentNullException()
     {
@@ -27,10 +31,33 @@ public class StreamExtensionsTest
 
         streamMock.SetupGet(x => x.CanRead).Returns(false);
 
-        var stream = streamMock.Object;
-
         // Act & Assert
-        Assert.Throws<NotSupportedException>(() => stream.ReadAndDeserializeFromJson<object>());
+        Assert.Throws<NotSupportedException>(() => streamMock.Object.ReadAndDeserializeFromJson<object>());
+    }
+
+    [Test]
+    public void ReadAndDeserializeFromJson_WhenWhenJsonIsValid_ReturnsDeserializedObject()
+    {
+        // Arrange
+        const int BufferSize = 1024;
+
+        var objectToWrite = new TestObject("test");
+
+        var bytes = new byte[BufferSize];
+
+        using var streamWriter = new StreamWriter(new MemoryStream(bytes), new UTF8Encoding(), BufferSize, true);
+        using var jsonTextWriter = new JsonTextWriter(streamWriter);
+
+        var jsonSerializer = new JsonSerializer();
+
+        jsonSerializer.Serialize(jsonTextWriter, objectToWrite);
+        jsonTextWriter.Flush();
+
+        // Act
+        var deserializedObject = new MemoryStream(bytes).ReadAndDeserializeFromJson<TestObject>();
+
+        // Assert
+        Assert.AreEqual(objectToWrite, deserializedObject);
     }
 
     [Test]
@@ -44,16 +71,33 @@ public class StreamExtensionsTest
     }
 
     [Test]
-    public void SerializeToJsonAndWrite_WhenStreamIsNotReadable_ThrowNotSupportedException()
+    public void SerializeToJsonAndWrite_WhenStreamIsNotWritable_ThrowNotSupportedException()
     {
         // Arrange
         var streamMock = new Mock<Stream>();
 
         streamMock.SetupGet(x => x.CanWrite).Returns(false);
 
-        var stream = streamMock.Object;
-
         // Act & Assert
-        Assert.Throws<NotSupportedException>(() => stream.SerializeToJsonAndWrite(It.IsAny<object>()));
+        Assert.Throws<NotSupportedException>(() => streamMock.Object.SerializeToJsonAndWrite(It.IsAny<object>()));
+    }
+
+    [Test]
+    public void SerializeToJsonAndWrite_WhenWhenObjectIsValid_ReturnsValidJson()
+    {
+        // Arrange
+        const string ExpectedJsonString = "{\"Property\":\"test\"}";
+        const int BufferSize = 1024;
+
+        var bytes = new byte[BufferSize];
+
+        // Act
+        new MemoryStream(bytes).SerializeToJsonAndWrite(new TestObject("test"));
+
+        using var streamReader = new StreamReader(new MemoryStream(bytes));
+        var jsonString = streamReader.ReadToEnd()[..ExpectedJsonString.Length];
+
+        // Assert
+        Assert.AreEqual(ExpectedJsonString, jsonString);
     }
 }
