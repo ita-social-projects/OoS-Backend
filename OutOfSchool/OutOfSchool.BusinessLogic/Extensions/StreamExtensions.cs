6273c9593117da9450b1c98b1bfa277d737a2556
Vlad Pustovoitov <vladp1998@gmail.com>
2024-05-09 15:45:27 +0300

    Move extensions to extensions folder (#1473)
    * Move ext to ext folder
    
    * Fix using of invalid property and add tests
    
    * Add more tests
    
    * Move test record to end of test class

---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Extensions/StreamExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+using Newtonsoft.Json;
+
+namespace OutOfSchool.BusinessLogic.Extensions;
+
+public static class StreamExtensions
+{
+    public static T ReadAndDeserializeFromJson<T>(this Stream stream)
+    {
+        ArgumentNullException.ThrowIfNull(stream);
+
+        if (!stream.CanRead)
+        {
+            throw new NotSupportedException("Can't read this stream");
+        }
+
+        using var streamReader = new StreamReader(stream);
+
+        return JsonConvert.DeserializeObject<T>(streamReader.ReadToEnd());
+    }
+
+    public static void SerializeToJsonAndWrite<T>(this Stream stream, T objectToWrite)
+    {
+        ArgumentNullException.ThrowIfNull(stream);
+
+        if (!stream.CanWrite)
+        {
+            throw new NotSupportedException("Can't write to this stream");
+        }
+
+        using var streamWriter = new StreamWriter(stream, new UTF8Encoding(), CommunicationConstants.BufferSize, true);
+        using var jsonTextWriter = new JsonTextWriter(streamWriter);
+
+        var jsonSerializer = new JsonSerializer();
+
+        jsonSerializer.Serialize(jsonTextWriter, objectToWrite);
+        jsonTextWriter.Flush();
+    }
+}
