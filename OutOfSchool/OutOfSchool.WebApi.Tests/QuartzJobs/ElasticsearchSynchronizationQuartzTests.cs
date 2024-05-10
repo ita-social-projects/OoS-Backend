d3eb18e36f36d9609c2b2d55bc34130f13aa5b28
MrVenik <vladp1998@gmail.com>
2024-05-10 16:15:36 +0300

    Add test for es sync quartz bg job

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/QuartzJobs/ElasticsearchSynchronizationQuartzTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using OutOfSchool.BackgroundJobs.Jobs;
+using OutOfSchool.BusinessLogic.Services;
+using Quartz;
+
+namespace OutOfSchool.WebApi.Tests.QuartzJobs;
+
+[TestFixture]
+public class ElasticsearchSynchronizationQuartzTests
+{
+    private readonly Mock<IElasticsearchSynchronizationService> elasticsearchSynchronizationServiceMock = new();
+    private readonly Mock<IElasticPinger> elasticPingerMock = new();
+    private readonly Mock<IJobExecutionContext> jobExecutionContextMock = new();
+
+    [Test]
+    public async Task Execute_WhenElasticPingerIsHealthy_ShouldCallElasticsearchSynchronizationServiceSynchronize()
+    {
+        // Arrange
+        elasticPingerMock.SetupGet(x => x.IsHealthy).Returns(true);
+        jobExecutionContextMock.SetupGet(x => x.CancellationToken).Returns(It.IsAny<CancellationToken>());
+
+        var job = new ElasticsearchSynchronizationQuartz(GetServiceProvider());
+
+        // Act
+        await job.Execute(jobExecutionContextMock.Object);
+
+        // Assert
+        elasticsearchSynchronizationServiceMock.Verify(x => x.Synchronize(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public async Task Execute_WhenElasticPingerIsNotHealthy_ShouldNotCallElasticsearchSynchronizationServiceSynchronize()
+    {
+        // Arrange
+        elasticPingerMock.SetupGet(x => x.IsHealthy).Returns(false);
+        jobExecutionContextMock.SetupGet(x => x.CancellationToken).Returns(It.IsAny<CancellationToken>());
+
+        var job = new ElasticsearchSynchronizationQuartz(GetServiceProvider());
+
+        // Act
+        await job.Execute(jobExecutionContextMock.Object);
+
+        // Assert
+        elasticsearchSynchronizationServiceMock.Verify(x => x.Synchronize(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    private IServiceProvider GetServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddTransient(sp => elasticsearchSynchronizationServiceMock.Object);
+        serviceCollection.AddSingleton(sp => elasticPingerMock.Object);
+
+        return serviceCollection.BuildServiceProvider();
+    }
+}
