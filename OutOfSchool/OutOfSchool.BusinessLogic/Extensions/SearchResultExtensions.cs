3f9c37d06d89ae5f6385314fa69743bb2d415d49
Gleb Shpuryk <stalkergleb74@gmail.com>
2024-06-10 09:36:55 +0300

    Fixed SearchResultExtensions. Droped comment + method now is a lambda.

--- OutOfSchool/OutOfSchool.BusinessLogic/Extensions/SearchResultExtensions.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Extensions/SearchResultExtensions.cs
@@ -3,17 +3,6 @@
 namespace OutOfSchool.BusinessLogic.Extensions;
 public static class SearchResultExtensions
 {
-    /// <summary>
-    /// Method, that checks SearchResult for null and his entities for empty.
-    /// </summary>
-    /// <typeparam name="T">Typeof entity.</typeparam>
-    /// <param name="searchResult">Instance of SearchResult.</param>
-    /// <returns>
-    /// True - searchResult is null or searchResult.Entities is empty.
-    /// False - searchResult is not null or searchResult.Entities is not empty.
-    /// </returns>
     public static bool IsNullOrEntitiesEmpty<T>(this SearchResult<T> searchResult)
-    {
-        return searchResult == null || searchResult.Entities.Count == 0;
-    }
+        => searchResult == null || searchResult.Entities.Count == 0;
 }
