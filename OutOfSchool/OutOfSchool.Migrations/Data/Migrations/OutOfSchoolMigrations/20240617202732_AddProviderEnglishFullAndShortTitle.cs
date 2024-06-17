c91e39d6e7c1a67ec7772030ecdbd3e70c0285bd
MrVenik <vladp1998@gmail.com>
2024-06-17 23:28:19 +0300

    Add migrations

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20240617202732_AddProviderEnglishFullAndShortTitle.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
+{
+    /// <inheritdoc />
+    public partial class AddProviderEnglishFullAndShortTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderTitle",
+                table: "Workshops",
+                type: "varchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(120)",
+                oldMaxLength: 120)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProviderTitleEn",
+                table: "Workshops",
+                type: "varchar(60)",
+                maxLength: 60,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullTitleEn",
+                table: "Providers",
+                type: "varchar(60)",
+                maxLength: 60,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShortTitleEn",
+                table: "Providers",
+                type: "varchar(60)",
+                maxLength: 60,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderTitleEn",
+                table: "Workshops");
+
+            migrationBuilder.DropColumn(
+                name: "FullTitleEn",
+                table: "Providers");
+
+            migrationBuilder.DropColumn(
+                name: "ShortTitleEn",
+                table: "Providers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderTitle",
+                table: "Workshops",
+                type: "varchar(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(60)",
+                oldMaxLength: 60)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
