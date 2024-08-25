be4faed0ae1f3a98a8162f4553483a8551fbc3a2
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-08-25 19:59:41 +0300

    Added DataAnnotation attributes to RequiredWorkshopMemento entity for validation its properties.

--- OutOfSchool/OutOfSchool.BusinessLogic/Services/Memento/Models/RequiredWorkshopMemento.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Services/Memento/Models/RequiredWorkshopMemento.cs
@@ -1,28 +1,49 @@
-﻿using OutOfSchool.Common.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using OutOfSchool.Common.Enums;
+using OutOfSchool.Common.Validators;
 
 namespace OutOfSchool.BusinessLogic.Services.Memento.Models;
 
 public class RequiredWorkshopMemento
 {
+    [Required(ErrorMessage = "Workshop title is required")]
+    [MinLength(Constants.MinWorkshopTitleLength)]
+    [MaxLength(Constants.MaxWorkshopTitleLength)]
     public string Title { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Workshop short title is required")]
+    [MinLength(Constants.MinWorkshopShortTitleLength)]
+    [MaxLength(Constants.MaxWorkshopShortTitleLength)]
     public string ShortTitle { get; set; } = string.Empty;
 
+    [DataType(DataType.PhoneNumber)]
+    [Required(ErrorMessage = "Phone number is required")]
+    [CustomPhoneNumber(ErrorMessage = Constants.PhoneErrorMessage)]
+    [DisplayFormat(DataFormatString = Constants.PhoneNumberFormat)]
+    [MaxLength(Constants.MaxPhoneNumberLengthWithPlusSign)]
     public string Phone { get; set; } = string.Empty;
 
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Email is required")]
+    [MaxLength(256)]
     public string Email { get; set; } = string.Empty;
 
+    [Range(0, 120, ErrorMessage = "Min age should be a number from 0 to 120")]
     public int MinAge { get; set; }
 
+    [Range(0, 120, ErrorMessage = "Max age should be a number from 0 to 120")]
     public int MaxAge { get; set; }
 
+    [MaxLength(Constants.MaxProviderFullTitleLength)]
     public string ProviderTitle { get; set; } = string.Empty;
 
+    [EnumDataType(typeof(PayRateType))]
     public PayRateType PayRate { get; set; }
 
     public Guid ProviderId { get; set; }
 
     public long AddressId { get; set; }
 
+    [EnumDataType(typeof(FormOfLearning))]
     public FormOfLearning FormOfLearning { get; set; }
 }
