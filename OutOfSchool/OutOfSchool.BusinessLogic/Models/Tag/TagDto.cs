91f646a60d33e6821f2fad995c49e9991c0d9653
AndrD1406 <andriy.dolishniy2005@gmail.com>
2024-09-09 00:28:00 +0300

    Added a Tag Dto

---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Tag/TagDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.BusinessLogic.Models.Tag;
+public class TagDto
+{
+    public long Id { get; set; }
+
+    [MaxLength(60)]
+    public string Name { get; set; } = string.Empty;
+}
