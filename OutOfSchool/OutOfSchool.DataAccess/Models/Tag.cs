c82e6e3fa96ae131e97db679ad7cc83456ca2144
AndrD1406 <andriy.dolishniy2005@gmail.com>
2024-09-13 12:22:42 +0300

    Simplified the tags,

--- OutOfSchool/OutOfSchool.DataAccess/Models/Tag.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Tag.cs
@@ -1,21 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace OutOfSchool.Services.Models;
 public class Tag : IKeyedEntity<long>
 {
     public long Id { get; set; }
 
-    [Required]
-    [DataType(DataType.Text)]
-    [MaxLength(60)]
-    [MinLength(1)]
     public string Name { get; set; } = string.Empty;
 
-    [Required]
-    [DataType(DataType.Text)]
-    [MaxLength(60)]
-    [MinLength(1)]
     public string NameEn { get; set; } = string.Empty;
 
     public virtual List<Workshop> Workshops { get; set; }
