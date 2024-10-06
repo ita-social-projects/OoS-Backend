22eb87242cbf6ad4bf8f527c2aef7549abd65180
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-10-06 22:34:42 +0300

    Added migration - 20241006193114_UpdateWorkshopEntity.

--- OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20241006193114_UpdateWorkshopEntity.Designer.cs
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20241006193114_UpdateWorkshopEntity.Designer.cs
@@ -11,7 +11,7 @@ using OutOfSchool.Services;
 namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
 {
     [DbContext(typeof(OutOfSchoolDbContext))]
-    [Migration("20241006192707_UpdateWorkshopEntity")]
+    [Migration("20241006193114_UpdateWorkshopEntity")]
     partial class UpdateWorkshopEntity
     {
         /// <inheritdoc />
