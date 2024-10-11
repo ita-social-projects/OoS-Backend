c29d93f1592bcb3d1f4b44520842b5ab3123f697
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-10-11 12:21:26 +0300

    Removed Newtonsoft.Json.PreserveReferencesHandling from Startup class.

--- OutOfSchool/OutOfSchool.WebApi/Startup.cs
+++ OutOfSchool/OutOfSchool.WebApi/Startup.cs
@@ -108,10 +108,10 @@ public static class Startup
         app.UseHeaderPropagation();
 
         app.MapHealthChecks("/healthz/ready", new HealthCheckOptions
-            {
-                Predicate = healthCheck => healthCheck.Tags.Contains("readiness"),
-                AllowCachingResponses = false,
-            })
+        {
+            Predicate = healthCheck => healthCheck.Tags.Contains("readiness"),
+            AllowCachingResponses = false,
+        })
             .RequireHost($"*:{app.Configuration.GetValue<int>("ApplicationPorts:HealthPort")}")
             .WithMetadata(new AllowAnonymousAttribute());
 
