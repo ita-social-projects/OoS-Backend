e2c1f3b78852341c21bbf361e3839d48ff056b55
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-10-17 17:24:52 +0300

    Changes for merging with develop

--- OutOfSchool/OutOfSchool.BusinessLogic/Services/Database/WorkshopService.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Services/Database/WorkshopService.cs
@@ -99,20 +99,8 @@ public class WorkshopService : IWorkshopService, ISensitiveWorkshopsService
     /// <exception cref="ArgumentNullException">If <see cref="WorkshopBaseDto"/> is null.</exception>
     public async Task<WorkshopBaseDto> Create(WorkshopBaseDto dto)
     {
-        ArgumentNullException.ThrowIfNull(dto);
+        _ = dto ?? throw new ArgumentNullException(nameof(dto));
         logger.LogInformation("Workshop creating was started.");
-
-        if (dto.MemberOfWorkshopId.HasValue && !await Exists((Guid)dto.MemberOfWorkshopId).ConfigureAwait(false))
-        {
-            var errorMessage = $"The main workshop (with id = {dto.MemberOfWorkshopId}) for the workshop being created was not found.";
-            throw new InvalidOperationException(errorMessage);
-        }
-
-        if (dto.MemberOfWorkshopId.HasValue && (await GetById((Guid)dto.MemberOfWorkshopId).ConfigureAwait(false)).MemberOfWorkshopId.HasValue)
-        {
-            var errorMessage = $"The main workshop (with ID = {dto.MemberOfWorkshopId}) for the workshop being created is a member of another workshop, so it cannot be the main workshop.";
-            throw new InvalidOperationException(errorMessage);
-        }
 
         if (dto.AvailableSeats is 0 or null)
         {
@@ -123,9 +111,14 @@ public class WorkshopService : IWorkshopService, ISensitiveWorkshopsService
         workshop.Provider = await providerRepository.GetById(workshop.ProviderId).ConfigureAwait(false);
         workshop.ProviderOwnership = workshop.Provider.Ownership;
 
+        if (dto.DefaultTeacher is not null)
+        {
+            workshop.DefaultTeacher = mapper.Map<Teacher>(dto.DefaultTeacher);
+        }
+
         if (dto.Teachers is not null)
         {
-            workshop.Teachers = dto.Teachers.Select(mapper.Map<Teacher>).ToList();
+            workshop.Teachers = dto.Teachers.Select(dtoTeacher => mapper.Map<Teacher>(dtoTeacher)).ToList();
         }
 
         workshop.Status = WorkshopStatus.Open;
@@ -135,7 +128,7 @@ public class WorkshopService : IWorkshopService, ISensitiveWorkshopsService
 
         var newWorkshop = await workshopRepository.RunInTransaction(operation).ConfigureAwait(false);
 
-        logger.LogInformation("Workshop with Id = {newWorkshopId} created successfully.", newWorkshop.Id);
+        logger.LogInformation($"Workshop with Id = {newWorkshop?.Id} created successfully.");
 
         return mapper.Map<WorkshopBaseDto>(newWorkshop);
     }
@@ -881,7 +874,10 @@ public class WorkshopService : IWorkshopService, ISensitiveWorkshopsService
         {
             var tempPredicate = PredicateBuilder.False<Workshop>();
 
-            foreach (var word in filter.SearchText.Split([' ', ','], StringSplitOptions.RemoveEmptyEntries))
+            // Fix Rider ambiguous method with either char or string args
+            // ReSharper disable once UseCollectionExpression
+            // ReSharper disable once RedundantExplicitArrayCreation
+            foreach (var word in filter.SearchText.Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries))
             {
                 tempPredicate = tempPredicate.Or(x => EF.Functions.Like(x.Keywords, $"%{word}%"));
             }
