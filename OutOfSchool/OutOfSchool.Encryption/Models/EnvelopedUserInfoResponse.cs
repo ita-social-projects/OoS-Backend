2d4f5153241aab13d59d6b552345e3b69c3f710b
Dmytro M <dmytro.minochkin@gmail.com>
2024-10-24 12:45:00 +0300

    remove error from DTO as it should be validated in AuthServer

--- OutOfSchool/OutOfSchool.Encryption/Models/EnvelopedUserInfoResponse.cs
+++ OutOfSchool/OutOfSchool.Encryption/Models/EnvelopedUserInfoResponse.cs
@@ -3,8 +3,4 @@ namespace OutOfSchool.Encryption.Models;
 public class EnvelopedUserInfoResponse
 {
     public string EncryptedUserInfo { get; set; }
-
-    public int Error { get; set; }
-
-    public string Message { get; set; }
 }
