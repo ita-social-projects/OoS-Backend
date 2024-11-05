e3420aabdcfac3f2d064f03e62539908460c86f0
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-11-05 16:46:24 +0200

    Reordered methods of TimespanConverter class.

--- OutOfSchool/OutOfSchool.BusinessLogic/Util/JsonTools/TimespanConverter.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Util/JsonTools/TimespanConverter.cs
@@ -10,18 +10,17 @@ public class TimespanConverter : JsonConverter<TimeSpan>
     /// </summary>
     public const string TimeSpanFormatString = @"hh\:mm";
 
-
-    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        TimeSpan.TryParseExact(reader.GetString(), TimeSpanFormatString, null, out var parsedTimeSpan);
-        return parsedTimeSpan;
-    }
-
     public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
     {
         ArgumentNullException.ThrowIfNull(writer);
 
         var timespanFormatted = $"{value.ToString(TimeSpanFormatString)}";
         writer.WriteStringValue(timespanFormatted);
+    }
+
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        TimeSpan.TryParseExact(reader.GetString(), TimeSpanFormatString, null, out var parsedTimeSpan);
+        return parsedTimeSpan;
     }
 }
