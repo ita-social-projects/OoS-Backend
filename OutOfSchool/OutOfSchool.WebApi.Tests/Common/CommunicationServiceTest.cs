e23bb20c08ccfe91e512183d1182f810afb5d2fa
Dmytro M <dmytro.minochkin@gmail.com>
2024-11-06 21:58:55 +0200

    add simple tests

---
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Common/CommunicationServiceTest.cs
@@ -0,0 +1,168 @@
+#nullable enable
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Moq.Language.Flow;
+using Moq.Protected;
+using NUnit.Framework;
+using OutOfSchool.Common.Communication;
+using OutOfSchool.Common.Config;
+using OutOfSchool.Common.Models;
+
+namespace OutOfSchool.WebApi.Tests.Common;
+
+[TestFixture]
+public class CommunicationServiceTest
+{
+    private Mock<IOptions<CommunicationConfig>> communicationOptions;
+    private Mock<IHttpClientFactory> httpClientFactory;
+    private Mock<ILogger<CommunicationService>> logger;
+    private Mock<HttpMessageHandler> handler;
+    private HttpClient client;
+    private readonly Uri uri = new("https://example.com");
+    private CommunicationService communicationService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        handler = new Mock<HttpMessageHandler>();
+        client = new HttpClient(handler.Object);
+        var communicationConfig = new CommunicationConfig
+        {
+            ClientName = "test",
+            MaxNumberOfRetries = 1,
+            TimeoutInSeconds = 1,
+        };
+        communicationOptions = new Mock<IOptions<CommunicationConfig>>();
+        communicationOptions.Setup(x => x.Value).Returns(communicationConfig);
+        httpClientFactory = new Mock<IHttpClientFactory>();
+        httpClientFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(client);
+        logger = new Mock<ILogger<CommunicationService>>();
+        communicationService = new CommunicationService(httpClientFactory.Object, communicationOptions.Object, logger.Object);
+    }
+
+    [Test]
+    public async Task SendRequest_WithCorrectRequest_ReturnsOkResponse()
+    {
+        // Arrange
+        var request = new Request
+        {
+            HttpMethodType = HttpMethodType.Post,
+            Data = new TestRequestData
+            {
+                Content = "test",
+            },
+            Url = uri,
+        };
+        var response = new TestResponse
+        {
+            Content = "OK",
+        };
+        var setup = SetupSendAsync(handler, HttpMethod.Post, uri.ToString());
+        ReturnsHttpResponseAsync(setup, response, HttpStatusCode.OK);
+
+        // Act
+        var result = await communicationService.SendRequest<TestResponse, ErrorResponse>(request);
+
+        result.DoRight(r =>
+        {
+            Assert.AreEqual("OK", r.Content);
+        });
+    }
+
+    [Test]
+    public async Task SendRequest_WithEmptyRequest_ReturnsErrorResponse()
+    {
+        // Arrange
+        var setup = SetupSendAsync(handler, HttpMethod.Get, uri.ToString());
+        ReturnsHttpResponseAsync(setup, null, HttpStatusCode.OK);
+
+        // Act
+        var result = await communicationService.SendRequest<TestResponse, ErrorResponse>(null);
+
+        result.Match<object?>(
+            error =>
+            {
+                Assert.AreEqual(HttpStatusCode.BadRequest, error.HttpStatusCode);
+                return null;
+            },
+            _ =>
+            {
+                Assert.Fail();
+                return null;
+            });
+    }
+
+    [Test]
+    public async Task SendRequest_WithServerError_ReturnsErrorResponse()
+    {
+        // Arrange
+        var request = new Request
+        {
+            HttpMethodType = HttpMethodType.Get,
+            Url = uri,
+        };
+        var setup = SetupSendAsync(handler, HttpMethod.Get, uri.ToString());
+        ReturnsHttpResponseAsync(setup, null, HttpStatusCode.Unauthorized);
+
+        // Act
+        var result = await communicationService.SendRequest<TestResponse, ErrorResponse>(request);
+
+        result.Match<object?>(
+            error =>
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, error.HttpStatusCode);
+                return null;
+            },
+            _ =>
+            {
+                Assert.Fail();
+                return null;
+            });
+    }
+
+    private static ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SetupSendAsync(
+        Mock<HttpMessageHandler> handler, HttpMethod requestMethod, string requestUrl)
+    {
+        return handler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+            ItExpr.Is<HttpRequestMessage>(r =>
+                r.Method == requestMethod &&
+                r.RequestUri != null &&
+                r.RequestUri.ToString() == requestUrl),
+            ItExpr.IsAny<CancellationToken>());
+    }
+
+    private static IReturnsResult<HttpMessageHandler> ReturnsHttpResponseAsync(
+        ISetup<HttpMessageHandler, Task<HttpResponseMessage>> moqSetup,
+        object? responseBody,
+        HttpStatusCode responseCode)
+    {
+        var serializedResponse = JsonSerializer.Serialize(responseBody);
+        var stringContent = new StringContent(serializedResponse ?? string.Empty);
+
+        var responseMessage = new HttpResponseMessage
+        {
+            StatusCode = responseCode,
+            Content = stringContent,
+        };
+
+        return moqSetup.ReturnsAsync(responseMessage);
+    }
+
+    private class TestRequestData
+    {
+        public string? Content { get; set; }
+    }
+
+    private class TestResponse : IResponse
+    {
+        public string? Content { get; set; }
+    }
+}
