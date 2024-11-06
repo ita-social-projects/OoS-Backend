ca3eab411c4f9d28b22963814a5bfffbb36e1ea5
Dmytro M <dmytro.minochkin@gmail.com>
2024-11-06 12:32:40 +0200

    update appsettings

--- OutOfSchool/OutOfSchool.Encryption/Program.cs
+++ OutOfSchool/OutOfSchool.Encryption/Program.cs
@@ -13,7 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .AddJsonFile("appsettings.jsonc", false, true)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.jsonc", true, true);
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.jsonc", true, true)
+    .AddEnvironmentVariables();
 
 builder.Host.UseSerilog((ctx, lc) => lc
     .ReadFrom.Configuration(ctx.Configuration)
