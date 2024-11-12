b70698e87171c305f44d9633054896abf801bce4
Dmytro M <dmytro.minochkin@gmail.com>
2024-11-12 17:40:40 +0200

    fix issues found during testing

---
+++ OutOfSchool/OutOfSchool.AuthCommon/Config/IdServerPaths.cs
@@ -0,0 +1,10 @@
+namespace OutOfSchool.AuthCommon.Config;
+
+public class IdServerPaths
+{
+    public string Authorize { get; set; }
+
+    public string Token { get; set; }
+
+    public string UserInfo { get; set; }
+}
