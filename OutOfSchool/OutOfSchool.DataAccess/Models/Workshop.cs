526e3871ecb03bcb1fb2cbe3a52b8a83a9cb51a5
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-11-23 09:25:21 +0200

    1) Replaced 'uint.MinValue' with '0' for uint properties of Workshop class. 2) Added migration - 20241122215448_CreateWorkshop.

--- OutOfSchool/OutOfSchool.DataAccess/Models/Workshop.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Workshop.cs
@@ -81,7 +81,7 @@ public class Workshop : BusinessEntity, IImageDependentEntity<Workshop>, IHasEnt
     [Required(ErrorMessage = "Property IsSpecial is required")]
     public bool IsSpecial { get; set; } = default;
 
-    public uint SpecialNeedsId { get; set; } = uint.MinValue;
+    public uint SpecialNeedsId { get; set; } = 0;
 
     [Required(ErrorMessage = "Property IsInclusive is required")]
     public bool IsInclusive { get; set; } = default;
@@ -95,24 +95,24 @@ public class Workshop : BusinessEntity, IImageDependentEntity<Workshop>, IHasEnt
     public string PreferentialTermsOfParticipation { get; set; }
 
     [Required(ErrorMessage = "Educational shift is required")]
-    public uint EducationalShiftId { get; set; } = uint.MinValue;
+    public uint EducationalShiftId { get; set; } = 0;
 
     [Required(ErrorMessage = "Language of education is required")]
-    public uint LanguageOfEducationId { get; set; } = uint.MinValue;
+    public uint LanguageOfEducationId { get; set; } = 0;
 
     [Required(ErrorMessage = "Type of age composition is required")]
-    public uint TypeOfAgeCompositionId { get; set; } = uint.MinValue;
+    public uint TypeOfAgeCompositionId { get; set; } = 0;
 
     [Required(ErrorMessage = "Educational disciplines is required")]
     public Guid EducationalDisciplines { get; set; } = Guid.Empty;
 
     [Required(ErrorMessage = "Category is required")]
-    public uint CategoryId { get; set; } = uint.MinValue;
+    public uint CategoryId { get; set; } = 0;
 
     [Required(ErrorMessage = "GropeType is required")]
-    public uint GropeTypeId { get; set; } = uint.MinValue;
+    public uint GropeTypeId { get; set; } = 0;
 
-    public uint CoverageId { get; set; } = uint.MinValue;
+    public uint CoverageId { get; set; } = 0;
 
     [Required(ErrorMessage = "Provider is required")]
     public Guid ProviderId { get; set; }
