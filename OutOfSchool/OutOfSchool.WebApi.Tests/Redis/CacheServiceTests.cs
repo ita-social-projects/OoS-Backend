2e48d65753acb272ec8d3ea73964b65c485cddc6
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-12-05 11:40:36 +0200

    Added tests to CacheServiceTests class.

--- OutOfSchool/OutOfSchool.WebApi.Tests/Redis/CacheServiceTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Redis/CacheServiceTests.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
+using Bogus;
 using FluentAssertions;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Options;
@@ -16,13 +16,20 @@ namespace OutOfSchool.WebApi.Tests.Redis;
 [TestFixture]
 public class CacheServiceTests
 {
+    private const int RANDOMSTRINGSIZE = 50;
+
+    private string expectedValue;
+    private string expectedKey;
     private Mock<IDistributedCache> distributedCacheMock;
     private Mock<IOptions<RedisConfig>> redisConfigMock;
     private ICacheService cacheService;
+    private IReadWriteCacheService readWriteCacheService;
 
     [SetUp]
     public void SetUp()
     {
+        expectedValue = new string(new Faker().Random.Chars(min: (char)0, max: (char)127, count: RANDOMSTRINGSIZE));
+        expectedKey = new string(new Faker().Random.Chars(min: (char)0, max: (char)127, count: RANDOMSTRINGSIZE));
         distributedCacheMock = new Mock<IDistributedCache>();
         redisConfigMock = new Mock<IOptions<RedisConfig>>();
         redisConfigMock.Setup(c => c.Value).Returns(new RedisConfig
@@ -32,6 +39,7 @@ public class CacheServiceTests
             SlidingExpirationInterval = TimeSpan.FromMinutes(1),
         });
         cacheService = new CacheService(distributedCacheMock.Object, redisConfigMock.Object);
+        readWriteCacheService = new CacheService(distributedCacheMock.Object, redisConfigMock.Object);
     }
 
     [Test]
@@ -43,7 +51,14 @@ public class CacheServiceTests
             {"ExpectedKey", "ExpectedValue"},
         };
         distributedCacheMock.Setup(c => c.Get(It.IsAny<string>()))
-            .Returns(Encoding.UTF8.GetBytes(JsonSerializerHelper.Serialize(expected)));
+            .Returns(Encoding.UTF8.GetBytes(JsonSerializerHelper.Serialize(expected)))
+            .Verifiable(Times.Once);
+
+        distributedCacheMock.Setup(c => c.Set(
+                It.IsAny<string>(),
+                It.IsAny<byte[]>(),
+                It.IsAny<DistributedCacheEntryOptions>()))
+            .Verifiable(Times.Never);
 
         // Act
         var result = await cacheService.GetOrAddAsync("Example", () => Task.FromResult(expected));
@@ -51,12 +66,7 @@ public class CacheServiceTests
         // Assert
         result.Keys.Should().Contain("ExpectedKey");
         result.Values.Should().Contain("ExpectedValue");
-        distributedCacheMock.Verify(
-            c => c.Set(
-                It.IsAny<string>(),
-                It.IsAny<byte[]>(),
-                It.IsAny<DistributedCacheEntryOptions>()),
-            Times.Never);
+        distributedCacheMock.VerifyAll();
     }
 
     [Test]
@@ -68,7 +78,13 @@ public class CacheServiceTests
             {"ExpectedKey", "ExpectedValue"},
         };
         distributedCacheMock.Setup(c => c.Get(It.IsAny<string>()))
-            .Returns((byte[]) null);
+            .Returns((byte[])null)
+            .Verifiable(Times.Once);
+        distributedCacheMock.Setup(c => c.Set(
+                It.IsAny<string>(),
+                It.IsAny<byte[]>(),
+                It.IsAny<DistributedCacheEntryOptions>()))
+            .Verifiable(Times.Once);
 
         // Act
         var result = await cacheService.GetOrAddAsync("Example", () => Task.FromResult(expected));
@@ -76,23 +92,66 @@ public class CacheServiceTests
         // Assert
         result.Keys.Should().Contain("ExpectedKey");
         result.Values.Should().Contain("ExpectedValue");
-        distributedCacheMock.Verify(
-            c => c.Set(
-                It.IsAny<string>(),
-                It.IsAny<byte[]>(),
-                It.IsAny<DistributedCacheEntryOptions>()),
-            Times.Once);
+        distributedCacheMock.VerifyAll();
     }
 
     [Test]
-    public async Task RemoveAsync_ShouldCallCacheRemove()
+    public async Task RemoveAsync_ShouldCallCacheRemoveOnce()
     {
-        // Arrange & Act
-        await cacheService.RemoveAsync("Example");
+        // Arrange
+        distributedCacheMock.Setup(c => c.Remove(expectedKey))
+            .Verifiable(Times.Once);
+
+        // Act
+        await cacheService.RemoveAsync(expectedKey);
 
         // Assert
-        distributedCacheMock.Verify(
-            c => c.RemoveAsync("Example", It.IsAny<CancellationToken>()),
-            Times.Once);
+        distributedCacheMock.VerifyAll();
+    }
+
+    [Test]
+    public async Task ReadAsync_WhenDataExistsInCacheAndNotExpired_ShouldReturnData()
+    {
+        // Arrange
+        distributedCacheMock.Setup(c => c.Get(expectedKey))
+            .Returns(Encoding.UTF8.GetBytes(expectedValue))
+            .Verifiable(Times.Once);
+
+        // Act
+        var result = await readWriteCacheService.ReadAsync(expectedKey);
+
+        // Assert
+        result.Should().Be(expectedValue);
+        distributedCacheMock.VerifyAll();
+    }
+
+    [Test]
+    public async Task ReadAsync_WhenDataNotExistsOrExpired_ShouldReturnNull()
+    {
+        // Arrange
+        distributedCacheMock.Setup(c => c.Get(expectedKey))
+            .Returns(Encoding.UTF8.GetBytes(string.Empty))
+            .Verifiable(Times.Once);
+
+        // Act
+        var result = await readWriteCacheService.ReadAsync(expectedKey);
+
+        // Assert
+        result.Should().Be(string.Empty);
+        distributedCacheMock.VerifyAll();
+    }
+
+    [Test]
+    public async Task WriteAsync_ShouldCallCacheSetOnce()
+    {
+        // Arrange
+        distributedCacheMock.Setup(c => c.Set(expectedKey, Encoding.UTF8.GetBytes(expectedValue), It.IsAny<DistributedCacheEntryOptions>()))
+            .Verifiable(Times.Once);
+
+        // Act
+        await readWriteCacheService.WriteAsync(expectedKey, expectedValue);
+
+        // Assert
+        distributedCacheMock.VerifyAll();
     }
 }
