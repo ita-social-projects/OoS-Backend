74696f6ee3fe601c6df0abc1e9f348184e47d5ea
Tetyana-K <tanya.kyryk@gmail.com>
2025-01-09 16:57:43 +0200

    change CompetitiveEventDto : add Rating and and NumberOfRating

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/CompetitiveEventDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/CompetitiveEventDto.cs
@@ -4,5 +4,7 @@ public class CompetitiveEventDto : CompetitiveEventBaseDto
 {
     public Guid Id { get; set; }
     public bool IsDeleted { get; set; }
+    public uint Rating { get; set; } = 0;
+    public uint NumberOfRatings { get; set; } = 0;
     public List<CompetitiveEventCoverageDto> Coverage { get; set; }
 }
