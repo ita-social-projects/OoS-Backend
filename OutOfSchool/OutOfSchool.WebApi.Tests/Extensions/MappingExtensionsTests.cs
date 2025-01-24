ab76c866a8e8d2f61ab0cee1d6ae0be279d2903b
Dmytro M <dmytro.minochkin@gmail.com>
2025-01-24 18:52:23 +0200

    fix test after rebase

--- OutOfSchool/OutOfSchool.WebApi.Tests/Extensions/MappingExtensionsTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Extensions/MappingExtensionsTests.cs
@@ -31,13 +31,14 @@ public class MappingExtensionsTests
         // assert
         configuration.AssertConfigurationIsValid();
     }
-    
+
     [Test]
     public void Mapping_ExternalExportMappingProfile_ConfigurationIsCorrect()
     {
         // act
         var configuration = new MapperConfiguration(cfg =>
-            cfg.UseProfile<CommonProfile>().UseProfile<MappingProfile>().UseProfile<ExternalExportMappingProfile>());
+            cfg.UseProfile<CommonProfile>().UseProfile<MappingProfile>().UseProfile<ContactsProfile>()
+                .UseProfile<ExternalExportMappingProfile>());
 
         // assert
         configuration.AssertConfigurationIsValid();
