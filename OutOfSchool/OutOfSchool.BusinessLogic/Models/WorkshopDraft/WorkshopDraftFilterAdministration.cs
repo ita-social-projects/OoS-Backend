efcfb4040de9552c9887f8b1a2d0b1e865d03ba9
Andriy <andreg593@gmail.com>
2025-01-27 17:16:41 +0200

    add WorkshopDraftFilterAdministration

---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/WorkshopDraft/WorkshopDraftFilterAdministration.cs
@@ -0,0 +1,12 @@
+﻿using OutOfSchool.BusinessLogic.Models.Workshops;
+using OutOfSchool.BusinessLogic.Util.CustomValidation;
+using OutOfSchool.Services.Enums.WorkshopStatus;
+using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.BusinessLogic.Models.WorkshopDraft;
+public class WorkshopDraftFilterAdministration : WorkshopFilterAdministration
+{
+    [EnumDataType(typeof(WorkshopDraftStatus), ErrorMessage = Constants.EnumErrorMessage)]
+    [WorkshopDraftStatusValidation]
+    public WorkshopDraftStatus WorkshopDraftStatus { get; set; } = WorkshopDraftStatus.PendingModeration;
+}
