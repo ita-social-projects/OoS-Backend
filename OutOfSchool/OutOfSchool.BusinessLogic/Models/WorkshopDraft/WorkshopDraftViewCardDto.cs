93bc5600c3ce7296cf636b9195ef4dcc97784c20
annygumeniuk <anny.gumeniuk@gmail.com>
2025-02-13 13:34:27 +0200

    Added RejectionMessage to the view card

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/WorkshopDraft/WorkshopDraftViewCardDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/WorkshopDraft/WorkshopDraftViewCardDto.cs
@@ -6,5 +6,6 @@ public class WorkshopDraftViewCardDto
 {
     public Guid WorkshopDraftId { get; set; }
     public WorkshopDraftStatus DraftStatus { get; set; }
+    public string? RejectionMessage { get; set; }
     public string Title { get; set; }
 }
--- OutOfSchool/OutOfSchool.BusinessLogic/Util/Mapping/WorkshopDraftMappingProfile.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Util/Mapping/WorkshopDraftMappingProfile.cs
@@ -66,7 +66,9 @@ public class WorkshopDraftMappingProfile : Profile
         CreateMap<WorkshopDraft, WorkshopDraftViewCardDto>()
             .ForMember(dest => dest.WorkshopDraftId, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.DraftStatus, opt => opt.MapFrom(src => src.DraftStatus))
-            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.WorkshopDraftContent.Title));
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.WorkshopDraftContent.Title))
+            .ForMember(dest => dest.RejectionMessage, opt => opt.MapFrom(src => src.RejectionMessage));
+        
         CreateMap<WorkshopDraft, WorkshopDraftRejectionDto>()            
             .ForMember(dest => dest.RejectionMessage, opt => opt.MapFrom(scr => scr.RejectionMessage));
          
