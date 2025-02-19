750f5cbf1698bbe5bd2f6a965bf27325bcdb123c
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-02-19 21:49:09 +0200

    1) Added UploadEmployeesRequestDto class for use as a parameter in the Upload ProviderController action. 2) Added Attribute[RegularExpression] to properties of UploadEmployeeRequestDto class for validation. 3) Changed the parameter type from UploadEmployeeRequestDto[] to UploadEmployeesRequestDto in the Upload ProviderController action.

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Individual/UploadEmployeeRequestDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Individual/UploadEmployeeRequestDto.cs
@@ -7,22 +7,27 @@ public class UploadEmployeeRequestDto
     [Required(ErrorMessage = "FirstName is required")]
     [MinLength(Constants.MinIndividualNameLength)]
     [MaxLength(Constants.MaxIndividualNameLength)]
+    [RegularExpression("^[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ'-]+$", ErrorMessage = "FirstName is not valid. It contains invalid characters.")]
     public string FirstName { get; set; }
 
     [Required(ErrorMessage = "MiddleName is required")]
     [MinLength(Constants.MinIndividualNameLength)]
     [MaxLength(Constants.MaxIndividualNameLength)]
+    [RegularExpression("^[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ'-]+$", ErrorMessage = "MiddleName is not valid. It contains invalid characters.")]
     public string MiddleName { get; set; }
 
     [Required(ErrorMessage = "LastName is required")]
     [MinLength(Constants.MinIndividualNameLength)]
     [MaxLength(Constants.MaxIndividualNameLength)]
+    [RegularExpression("^[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ'-]+$", ErrorMessage = "LastName is not valid. It contains invalid characters.")]
     public string LastName { get; set; }
 
     [Required(ErrorMessage = "Rnokpp is required")]
+    [RegularExpression("[0-9]{10}|[АБВГДЕЄЖЗИІКЛМНОПРСТУФХЦЧШЩЮЯ]{2}[0-9]{6}|[0-9]{9}", ErrorMessage = "Rnokpp is not valid")]
     public string Rnokpp { get; set; }
 
     [Required(ErrorMessage = "AssignedRole is required")]
     [MaxLength(60)]
+    [RegularExpression("^[-а-щА-ЩЬьЮюЯяЇїІіЄєҐґ'0-9 ]+$", ErrorMessage = "AssignedRole is not valid. It contains invalid characters.")]
     public string AssignedRole { get; set; }
 }
---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Individual/UploadEmployeesRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using OutOfSchool.BusinessLogic.Util.JsonTools;
+
+namespace OutOfSchool.BusinessLogic.Models.Individual;
+
+public class UploadEmployeesRequestDto
+{
+    [Required(ErrorMessage = "Employee list for uploading is required")]
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
+    public UploadEmployeeRequestDto[] Employees { get; set; }
+}
