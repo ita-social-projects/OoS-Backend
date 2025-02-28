9783d39a15165b3cef8391374fcba6a2465bd4a6
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-02-28 12:03:02 +0200

    Added 'error' parameter (type bool) to the [Obsolet] attribute in FileInDbStorageBase classes.

--- OutOfSchool/OutOfSchool.DataAccess/Repository/Files/FileInDbStorageBase.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/Files/FileInDbStorageBase.cs
@@ -11,7 +11,7 @@ using OutOfSchool.Services.Repository.Api.Files;
 
 namespace OutOfSchool.Services.Repository.Files;
 
-[Obsolete("This class is obsolete. Use FileStorage classes for the specific storage instead.")]
+[Obsolete("This class is obsolete. Use FileStorage classes for the specific storage instead.", false)]
 public abstract class FileInDbStorageBase<TFile> : IFilesStorage<TFile, string>
     where TFile : FileModel, new()
 {
