5efc60c9e37274027d44ef0c023691ce382ffefc
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-03-11 23:45:15 +0200

    Some refactoring

--- OutOfSchool/OutOfSchool.ElasticsearchData/Models/PriceRangeES.cs
+++ OutOfSchool/OutOfSchool.ElasticsearchData/Models/PriceRangeES.cs
@@ -1,6 +1,6 @@
 ﻿namespace OutOfSchool.ElasticsearchData.Models;
 public class PriceRangeES
 {
-    public decimal MaxPrice { get; set; }
     public decimal MinPrice { get; set; }
+    public decimal MaxPrice { get; set; }
 }
