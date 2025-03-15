a337e81cbeac4d88a20c3fad000a9ee95aa0061b
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-03-15 22:04:11 +0200

    Added ResponseCaching for PriceRange endpoint

--- OutOfSchool/OutOfSchool.WebApi/Startup.cs
+++ OutOfSchool/OutOfSchool.WebApi/Startup.cs
@@ -111,6 +111,8 @@ public static class Startup
         // Enable extracting token from QueryString for Hub-connection authorization
         app.UseMiddleware<AuthorizationTokenMiddleware>();
 
+        app.UseResponseCaching();
+
         app.UseAuthentication();
         app.UseAuthorization();
 
@@ -242,6 +244,7 @@ public static class Startup
 
         services.AddRazorPages();
         services.AddHttpContextAccessor();
+        services.AddResponseCaching();
         services.AddScoped<IEmployeeService, EmployeeService>();
         services.AddScoped<IMinistryAdminService, MinistryAdminService>();
         services.AddScoped<ISensitiveMinistryAdminService, MinistryAdminService>();
