1ccc598e992ffbd1c3282f371190ad696cc8c04f
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-03-16 23:08:44 +0200

    Fixed issue with tests

--- OutOfSchool/OutOfSchool.WebApi.Tests/Services/Database/WorkshopServiceTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/Database/WorkshopServiceTests.cs
@@ -1276,8 +1276,10 @@ public class WorkshopServiceTests
 
     private void SetupGetPriceRange(IEnumerable<Workshop> workshops)
     {
-        var queryableWorkshops = workshops.AsQueryable().BuildMock();
-
+        var queryableWorkshops = workshops.AsQueryable().BuildMockDbSet().Object;
+        workshopRepository.Setup(w => w
+                .Count(It.IsAny<Expression<Func<Workshop, bool>>>()))
+            .ReturnsAsync(workshops.Count());
         workshopRepository.Setup(w => w
             .Get(
                 It.IsAny<int>(),
