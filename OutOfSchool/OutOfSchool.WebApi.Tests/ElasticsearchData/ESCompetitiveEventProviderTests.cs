4d547e22b8534d4a0df0a5afb35563972fd5f5bb
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-03-16 22:39:37 +0200

    Added more tests

--- OutOfSchool/OutOfSchool.WebApi.Tests/ElasticsearchData/ESCompetitiveEventProviderTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/ElasticsearchData/ESCompetitiveEventProviderTests.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System;
 using OutOfSchool.Common.Enums;
 using OutOfSchool.Common.Enums.CompetitiveEvent;
+using Elastic.Clients.Elasticsearch.Aggregations;
 
 namespace OutOfSchool.WebApi.Tests.ElasticsearchData;
 
@@ -130,6 +131,43 @@ public class ESCompetitiveEventProviderTests
         Assert.IsInstanceOf<SearchResultES<CompetitiveEventES>>(result);
         Assert.AreEqual(expectedTotal, result.TotalAmount);
         Assert.AreEqual(expectedEntities, result.Entities.Count);
+    }
+
+    [Test]
+    public async Task GetPriceRange_WhenFilterIsNull_ShouldReturnPriceRange()
+    {
+        // Arrange
+        var expectedMinPrice = 100;
+        var expectedMaxPrice = 200;
+        CompetitiveEventFilterES filter = null;
+
+        var aggregations = new Dictionary<string, IAggregate>
+        {
+            { "min_price", new MinAggregate { Value = expectedMinPrice } },
+            { "max_price", new MaxAggregate { Value = expectedMaxPrice } }
+        };
+
+        var response = new SearchResponse<CompetitiveEventES>
+        {
+            Aggregations = new AggregateDictionary(aggregations)
+        };
+
+        elasticClientMock.Setup(
+            x => x.SearchAsync<CompetitiveEventES>(
+                It.IsAny<SearchRequest<CompetitiveEventES>>(), CancellationToken.None))
+            .ReturnsAsync(response);
+
+        // Act
+        var result = await provider.GetPriceRangeAsync(filter).ConfigureAwait(false);
+
+        // Assert
+        elasticClientMock.Verify(
+            x => x.SearchAsync<CompetitiveEventES>(
+                It.IsAny<SearchRequest<CompetitiveEventES>>(), CancellationToken.None),
+            Times.Once);
+        Assert.IsInstanceOf<PriceRangeES>(result);
+        Assert.AreEqual(expectedMinPrice, result.MinPrice);
+        Assert.AreEqual(expectedMaxPrice, result.MaxPrice);
     }
 
     private static SearchResponse<CompetitiveEventES> CreateSuccessfulSearchResponse(int totalHits, int entities)
