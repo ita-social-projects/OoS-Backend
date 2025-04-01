757c764d0a67dc8126044165ed18a103d5efdf35
Hlib Bondarev <Hlib.Bondarev@gmail.com>
2025-04-01 23:27:05 +0300

    Review lazy/eager loading in ParentService, ParentBlockedByAdminLogService and AchievementTypeService (#1756)
    * 1) Added TODO comments (about using Individual entity) to the Create() method of ParentRepository class;
    2) Used  [RequiredIf(nameof(IsBlocked)...] attribute instead of [RequiredIf('IsBlocked'...)] for the Reason property of BlockUnblockParentDto class;
    3) Added TODO comments (about replacing GetById() with GetByFilter() and about transaction) to the Create() method of ParentService class;
    4) Fixed GetByUserId() method of ParentService class;
    5) Used the 'includeExpression' parameter insted of 'includeProperties' for eager loading in methods GetPersonalInfoByUserId(), Update() and BlockUnblockParent of ParentService class;
    6) Added TODO comment (about transaction) to the BlockUnblockParent() method of ParentService class.
    7) Added coment - '// No nested entities in use – eager loading not required.' to ParentBlockedByAdminLogService and AchievementTypeService class.
    
    * 1) Fixed tests for Create() and Delete() methods of ParentService class;
    2) Created tests for GetByUserId(), GetPersonalInfoByUserId() and Update() methods of of ParentService class.
    
    * Fixed tests for GetByUserId(), GetPersonalInfoByUserId() and Update() methods of of ParentService class.
    
    * 1) Wrapped the 'Create Parent Operation' in a transaction in Create() method of the ParentService class;
    2) Wrapped 'repositoryParent.SaveChangesAsync()' and 'parentBlockedByAdminLogService.SaveChangesLogAsync()' operations in a transaction in BlockUnblockParent() method of the ParentService class;
    3) Used the property 'parent.User' instead of calling 'db.Users.FirstOrDefaultAsync(x => x.Id == entity.UserId).ConfigureAwait(false)' method in Create() method of the ParentRepository class.
    4) Fixed tests for Create() method of  the ParentService class.
    
    * Fixed tests for BlockUblockParent() method of the ParentService class.
    
    * Fixed the bugs mentioned CodeRabbit.
    
    * Added a try-catch block to the transaction in BlockUnblockParent() method of the ParentService class.
    
    * Fixed a catch block in BlockUnblockParent() method of the ParentService class.
    
    * Fixed a catch block in BlockUnblockParent() method of the ParentService class.
    
    * Fixed a catch block in BlockUnblockParent() method of the ParentService class.
    
    * Added tests for the exception cases in BlockUblockParent() method of the ParentService class.
    
    * Fixed the issues mentioned CodeRabbit.

--- OutOfSchool/OutOfSchool.DataAccess/Repository/ParentRepository.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Repository/ParentRepository.cs
@@ -29,11 +29,11 @@ public class ParentRepository : EntityRepositorySoftDeleted<Guid, Parent>, IPare
         await db.Parents.AddAsync(entity);
         await db.SaveChangesAsync();
 
-        var user = await db.Users.FirstOrDefaultAsync(x => x.Id == entity.UserId).ConfigureAwait(false);
+        var user = entity.User;
         user.IsRegistered = true;
         db.Entry(user).State = EntityState.Modified;
         await db.SaveChangesAsync();
-
+        // TODO: we should use user.Individual.[FirstName/MiddleName/LastName] instead of user.[FirstName/MiddleName/LastName] after removing these properties from User entity.
         var child = new Child()
         {
             Id = Guid.Empty,
