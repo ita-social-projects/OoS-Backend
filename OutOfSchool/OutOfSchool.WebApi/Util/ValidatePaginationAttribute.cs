2c22f62dc7bacedf675bbd71c570b829aef034a8
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-04-07 02:43:14 +0300

    Add ValidatePaginationAttribute for API pagination checks
    Introduces a new `ValidatePaginationAttribute` class that validates the `page` and `pageSize` parameters in API methods. Ensures both values are greater than 0, returning a `BadRequestObjectResult` with an error message if validation fails.

---
+++ OutOfSchool/OutOfSchool.WebApi/Util/ValidatePaginationAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OutOfSchool.WebApi.Util;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ValidatePaginationAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var args = context.ActionArguments;
+
+        if (args.TryGetValue("page", out var pageObj) &&
+            pageObj is int page && page < 1)
+        {
+            context.Result = new BadRequestObjectResult("Page number must be greater than 0.");
+            return;
+        }
+
+        if (args.TryGetValue("pageSize", out var pageSizeObj) &&
+            pageSizeObj is int pageSize && pageSize < 1)
+        {
+            context.Result = new BadRequestObjectResult("Page size must be greater than 0.");
+            return;
+        }
+    }
+}
+
