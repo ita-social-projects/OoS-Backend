d94e3936b30e33277aa3a2047018591350dc32c6
AndriyYehorov <107318614+AndriyYehorov@users.noreply.github.com>
2025-04-08 19:28:43 +0300

    Fix directionIds (#1776)
    * fix directionIds
    
    * fix tests

--- OutOfSchool/OutOfSchool.BusinessLogic/Services/WorkshopDrafts/WorkshopDraftService.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Services/WorkshopDrafts/WorkshopDraftService.cs
@@ -157,14 +157,11 @@ public class WorkshopDraftService : IWorkshopDraftService, ISensitiveWorkshopDra
         await workshopDraftRepository.SaveChangesAsync()
             .ConfigureAwait(false);
 
-        var createdDraftDto = mapper.Map<WorkshopDraftResponseDto>(createdDraftWithAssociatedTeachers);
-        createdDraftDto.WorkshopDetails.DirectionIds = await GetDirectionIdsForWorkshopDraft(createdDraftWithAssociatedTeachers);
-
         logger.LogDebug("WorkshopDraft created successfully.");
 
         return new WorkshopDraftResultDto
         {
-            WorkshopDraft = createdDraftDto,
+            WorkshopDraft = await MapWorkshopDraftWithDetails(createdDraftWithAssociatedTeachers),
             UploadingCoverImgWorkshopResult = uploadImagesResult.WorkshopCoverImageUploadingResult,
             UploadingImagesResults = uploadImagesResult.WorkshopImagesUploadingResult?.MultipleKeyValueOperationResult,
             TeachersCreateUpdateResult = uploadImagesResult.TeacherImagesUploadingResults
@@ -249,12 +246,9 @@ public class WorkshopDraftService : IWorkshopDraftService, ISensitiveWorkshopDra
         var (updatedDraft, coverImageResult, imagesResult, teacherCreateUpdateResult) = await workshopDraftRepository
             .RunInTransaction(UpdateDraftWithDependencies).ConfigureAwait(false);
 
-        var workshopDraftResponse = mapper.Map<WorkshopDraftResponseDto>(updatedDraft);
-        workshopDraftResponse.WorkshopDetails.DirectionIds = await GetDirectionIdsForWorkshopDraft(updatedDraft);
-
         return new WorkshopDraftResultDto()
         {
-            WorkshopDraft = workshopDraftResponse,
+            WorkshopDraft = await MapWorkshopDraftWithDetails(updatedDraft),
             UploadingCoverImgWorkshopResult = coverImageResult?.UploadingResult?.OperationResult,
             UploadingImagesResults = imagesResult?.UploadedMultipleResult?.MultipleKeyValueOperationResult,
             TeachersCreateUpdateResult = teacherCreateUpdateResult
@@ -388,7 +382,7 @@ public class WorkshopDraftService : IWorkshopDraftService, ISensitiveWorkshopDra
                 .FirstOrDefault(i => 
                     i.Id == draft.WorkshopDraftContent.InstitutionHierarchyId)
                 ?.SubDirections
-                .Select(d => d.Id)
+                .Select(d => d.DirectionId)
                 .ToList();
 
             workshopDraftResponseDtos.Add(responseDto);
@@ -749,7 +743,7 @@ public class WorkshopDraftService : IWorkshopDraftService, ISensitiveWorkshopDra
             id: (Guid) workshopDraft.WorkshopDraftContent.InstitutionHierarchyId,
             includeExpression: includeDirectionsFunc);
 
-        return institutionHierarchyDto.SubDirections.Select(d => d.Id).ToList();
+        return institutionHierarchyDto.SubDirections.Select(d => d.DirectionId).ToList();
     }
 
     private async Task<WorkshopDraftResponseDto> MapWorkshopDraftWithDetails(WorkshopDraft draft)
@@ -815,7 +809,7 @@ public class WorkshopDraftService : IWorkshopDraftService, ISensitiveWorkshopDra
                 .FirstOrDefault(i => i.Id == draft.WorkshopDraftContent.InstitutionHierarchyId);
 
             responseDto.WorkshopDetails.DirectionIds = institutionHierarchy?.SubDirections
-                .Select(d => d.Id)
+                .Select(d => d.DirectionId)
                 .ToList();
 
             responseDto.WorkshopDetails.Contacts
--- OutOfSchool/OutOfSchool.WebApi.Tests/Services/WorkshopDraftServiceTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/WorkshopDraftServiceTests.cs
@@ -48,6 +48,7 @@ public class WorkshopDraftServiceTests
     private Mock<IEntityRepository<long, Tag>> tagRepositoryMoq;
     private Mock<IWorkshopServicesCombinerV2> workshopServiceCombinerV2Moq;
     private Mock<IInstitutionHierarchyRepository> institutionHierarchyRepositoryMoq;
+    private Mock<ICodeficatorRepository> codeficatorRepositoryMoq;
 
     private string userId;
 
@@ -69,6 +70,7 @@ public class WorkshopDraftServiceTests
         tagRepositoryMoq = new Mock<IEntityRepository<long, Tag>>();
         workshopServiceCombinerV2Moq = new Mock<IWorkshopServicesCombinerV2>();
         institutionHierarchyRepositoryMoq = new Mock<IInstitutionHierarchyRepository>();
+        codeficatorRepositoryMoq = new Mock<ICodeficatorRepository>();
 
         var options = new Mock<IOptions<UploadConcurrencySettings>>();
         var settings = new UploadConcurrencySettings();
@@ -80,8 +82,7 @@ public class WorkshopDraftServiceTests
         var regionAdminService = new Mock<IRegionAdminService>();
         var ministryAdminService = new Mock<IMinistryAdminService>();
         var codeficatorService = new Mock<ICodeficatorService>();
-        var searchStringService = new Mock<ISearchStringService>();
-        var codeficatorRepository = new Mock<ICodeficatorRepository>();        
+        var searchStringService = new Mock<ISearchStringService>();              
 
         userId = "someUserId";
 
@@ -101,7 +102,7 @@ public class WorkshopDraftServiceTests
                    codeficatorService.Object,
                    searchStringService.Object,
                    institutionHierarchyRepositoryMoq.Object,
-                   codeficatorRepository.Object);
+                   codeficatorRepositoryMoq.Object);
     }
 
     #region Create
@@ -133,7 +134,12 @@ public class WorkshopDraftServiceTests
                 It.IsAny<string>(),
                 It.IsAny<Func<IQueryable<Tag>, IQueryable<Tag>>>()))
             .ReturnsAsync(Enumerable.Empty<Tag>()).Verifiable(Times.Once);
-        
+        codeficatorRepositoryMoq.Setup(x => x.Get(It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<Expression<Func<CATOTTG, bool>>>(),
+                    It.IsAny<Dictionary<Expression<Func<CATOTTG, object>>, SortDirection>>()))
+            .Returns(new List<CATOTTG>().AsQueryable().BuildMock());
+
         // Act 
         var result = await service.Create(workshopV2Dto).ConfigureAwait(false);
 
@@ -188,6 +194,11 @@ public class WorkshopDraftServiceTests
                 MultipleImageChangingResult,
                 List<TeacherCreateUpdateResultDto>)>> f) => f.Invoke())
             .Verifiable(Times.Once);
+        codeficatorRepositoryMoq.Setup(x => x.Get(It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<Expression<Func<CATOTTG, bool>>>(),
+                    It.IsAny<Dictionary<Expression<Func<CATOTTG, object>>, SortDirection>>()))
+            .Returns(new List<CATOTTG>().AsQueryable().BuildMock());
 
         //Act
         var result = await service.Update(workshopUpdateDto).ConfigureAwait(false);
@@ -521,6 +532,11 @@ public class WorkshopDraftServiceTests
             .Returns(workshopDrafts.AsQueryable().BuildMock()).Verifiable(Times.Once);
         workshopDraftRepoMoq.Setup(x => x.RunInTransaction(It.IsAny<Func<Task<WorkshopDraft>>>()))
             .ReturnsAsync(workshopDraft);
+        codeficatorRepositoryMoq.Setup(x => x.Get(It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<Expression<Func<CATOTTG, bool>>>(),
+                    It.IsAny<Dictionary<Expression<Func<CATOTTG, object>>, SortDirection>>()))
+            .Returns(new List<CATOTTG>().AsQueryable().BuildMock());
 
         // Act
         var result = await service.UpdateWorkshop(workshopV2Dto).ConfigureAwait(false);
