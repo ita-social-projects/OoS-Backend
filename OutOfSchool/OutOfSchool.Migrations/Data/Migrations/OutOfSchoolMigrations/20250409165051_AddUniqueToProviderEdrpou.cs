6ba42739cf4600ad7043b0afde6bd80f7d1b0b1c
Dmytro M <dmytro.minochkin@gmail.com>
2025-04-09 19:51:44 +0300

    add unique constraint for Edrpou

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20250409165051_AddUniqueToProviderEdrpou.cs
@@ -0,0 +1,37 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueToProviderEdrpou : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Providers_Edrpou",
+                table: "Providers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Providers_Edrpou",
+                table: "Providers",
+                column: "Edrpou",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Providers_Edrpou",
+                table: "Providers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Providers_Edrpou",
+                table: "Providers",
+                column: "Edrpou");
+        }
+    }
+}
