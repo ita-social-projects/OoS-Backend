3348878a43679f83e8473abc31c320bba53b3298
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-04-13 20:22:51 +0300

    Update `ToStudyPeriodDate` documentation and update `WorkshopGenerator` for tests

--- OutOfSchool/OutOfSchool.BusinessLogic/Extensions/DateOnlyExtensions.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Extensions/DateOnlyExtensions.cs
@@ -2,10 +2,10 @@
 public static class DateOnlyExtensions
 {
     /// <summary>
-    /// Converts any DateOnly to a StudyPeriod date by preserving Month and Day, setting Year to 0001.
+    /// Converts any DateOnly to a StudyPeriod date by preserving Month and Day, setting Year to 2000.
     /// </summary>
     /// <param name="date">The date to convert.</param>
-    /// <returns>A new DateOnly instance with the year set to 0001 and the month and day preserved.</returns>
+    /// <returns>A new DateOnly instance with the year set to 2000 and the month and day preserved.</returns>
     public static DateOnly ToStudyPeriodDate(this DateOnly date) 
         => new DateOnly(2000, date.Month, date.Day);
 }
