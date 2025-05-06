68cb8fc30e003b1d98eb83ddea4646dfb25ab680
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2025-05-06 14:33:47 +0300

    Add explicit sorting to get provider section items (#1793)

--- OutOfSchool/OutOfSchool.BusinessLogic/Services/ProviderServices/ProviderService.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Services/ProviderServices/ProviderService.cs
@@ -190,7 +190,7 @@ public class ProviderService(
         }
 
         Func<IQueryable<Provider>, IQueryable<Provider>> includeFunc =
-            p => p.Include(p => p.ProviderSectionItems)
+            p => p.Include(p => p.ProviderSectionItems.OrderBy(psi => psi.Name))
                   .Include(p => p.Type)
                   .Include(p => p.Institution)
                   .Include(p => p.Images)
