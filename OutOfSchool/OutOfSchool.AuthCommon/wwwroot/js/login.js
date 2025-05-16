e0a62b6dd23b8d94caaeaed120af2575371a32a2
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-05-16 13:43:27 +0300

    Fixed the issue mentioned CodeRabbit for js-code in login.js: - Reduce code duplication in role selection handlers; - Improve the user type toggle implementation.

--- OutOfSchool/OutOfSchool.AuthCommon/wwwroot/js/login.js
+++ OutOfSchool/OutOfSchool.AuthCommon/wwwroot/js/login.js
@@ -1,51 +1,23 @@
 $(function () {
-    $('#login_role_select_provider').on('click', function () {
-        $('#login_role_select_employee').removeClass('login-role-select--active');
-        $(this).addClass('login-role-select--active');
+    function setupRoleSelection(selector, oppositeSelector, inputId) {
+        $(selector).on('click', function () {
+            $(oppositeSelector).removeClass('login-role-select--active');
+            $(this).addClass('login-role-select--active');
 
-        // Update the hidden input with the selected value
-        let selectedValue = $(this).data('value');
-        $('#role').val(selectedValue);
-    });
+            // Update the hidden input with the selected value
+            let selectedValue = $(this).data('value');
+            $(inputId).val(selectedValue);
+        });
+    }
 
-    $('#login_role_select_employee').on('click', function () {
-        $('#login_role_select_provider').removeClass('login-role-select--active');
-        $(this).addClass('login-role-select--active');
-
-        // Update the hidden input with the selected value
-        let selectedValue = $(this).data('value');
-        $('#role').val(selectedValue);
-    });
-
-    $('#login_role_select_moderator').on('click', function () {
-        $('#login_role_select_techadmin').removeClass('login-role-select--active');
-        $(this).addClass('login-role-select--active');
-
-        // Update the hidden input with the selected value
-        let selectedValue = $(this).data('value');
-        $('#technical-staff-role').val(selectedValue);
-    });
-
-    $('#login_role_select_techadmin').on('click', function () {
-        $('#login_role_select_moderator').removeClass('login-role-select--active');
-        $(this).addClass('login-role-select--active');
-
-        // Update the hidden input with the selected value
-        let selectedValue = $(this).data('value');
-        $('#technical-staff-role').val(selectedValue);
-    });
+    // Setup role selections
+    setupRoleSelection('#login_role_select_provider', '#login_role_select_employee', '#role');
+    setupRoleSelection('#login_role_select_employee', '#login_role_select_provider', '#role');
+    setupRoleSelection('#login_role_select_moderator', '#login_role_select_techadmin', '#technical-staff-role');
+    setupRoleSelection('#login_role_select_techadmin', '#login_role_select_moderator', '#technical-staff-role');
 
     $("#switch-user-type").on("click", function () {
-        var x = document.getElementById("for_technical_staff");
-        var y = document.getElementById("for_providers");
-
-        if (x.style.display === "none") {
-            x.style.display = "block";
-            y.style.display = "none";
-        } else {
-            x.style.display = "none";
-            y.style.display = "block";
-        }
+        $("#for_technical_staff, #for_providers").toggleClass("hidden");
     });
 
     let check_loginPasswordEye = false;
