fdbf6679d4ae21f124bcfaeec2a2abbc1b61223d
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-05-22 23:42:57 +0300

    Fix mapping in SetToDraft method in WorkshopV2DtoExtensions

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/WorkshopV2Dto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/WorkshopV2Dto.cs
@@ -69,12 +69,11 @@ public static class WorkshopV2DtoExtensions
             Instagram = dto.Instagram,
         };
 
-    public static OutOfSchool.Services.Models.WorkshopDrafts.WorkshopDraft SetToDraft(this WorkshopV2Dto dto, OutOfSchool.Services.Models.WorkshopDrafts.WorkshopDraft model)
+    public static void SetToDraft(this WorkshopV2Dto dto, OutOfSchool.Services.Models.WorkshopDrafts.WorkshopDraft model)
     {
         model.ProviderId = dto.ProviderId;
         model.WorkshopId = dto.Id == Guid.Empty ? (Guid?)null : dto.Id;
-
-        return model;
+        model.WorkshopDraftContent = dto.ToDraftContent();
     }
 
     public static OutOfSchool.Services.Models.WorkshopDrafts.WorkshopDraft ToDraft(this WorkshopV2Dto dto)
