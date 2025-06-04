09bce74b355681020e8ab4e9566355661d28bb18
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-06-04 23:56:59 +0300

    Added tests for GetAccountStatus() of UserService to UserServiceTest class.

--- OutOfSchool/OutOfSchool.WebApi.Tests/Services/UserServiceTest.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/UserServiceTest.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using OutOfSchool.BusinessLogic;
+using OutOfSchool.BusinessLogic.Enums;
 using OutOfSchool.BusinessLogic.Models;
 using OutOfSchool.BusinessLogic.Services;
 using OutOfSchool.Services;
@@ -148,6 +149,28 @@ public class UserServiceTest
         Assert.ThrowsAsync<ArgumentException>(() => service.Delete(invalidId));
     }
 
+    [Test]
+    [TestCase("CVc4a6876a-77fb-4ecnne-9c78-a0880286ae33")]
+    public async Task GetAccountStatus_WhenLastLoginIsNotDefault_ReturnsAccountStatusIsAccepted(string id)
+    {
+        // Act
+        var accountStatus = await service.GetAccountStatus(id);
+
+        // Assert
+        Assert.AreEqual(AccountStatus.Accepted, accountStatus);
+    }
+
+    [Test]
+    [TestCase("CVc4a6876a-77fb-4ecnne-9c78-a0880286ae3c")]
+    public async Task GetAccountStatus_WhenLastLoginIsDefault_ReturnsAccountStatusIsNeverLogged(string id)
+    {
+        // Act
+        var accountStatus = await service.GetAccountStatus(id);
+
+        // Assert
+        Assert.AreEqual(AccountStatus.NeverLogged, accountStatus);
+    }
+
     private void SeedDatabase()
     {
         using var context = new TestOutOfSchoolDbContext(options);
@@ -254,6 +277,29 @@ public class UserServiceTest
                 Id = "CVc4a6876a-77fb-4ecnne-9c78-a0880286ae3c",
                 CreatingTime = default,
                 LastLogin = default,
+                MiddleName = "MiddleName5",
+                FirstName = "FirstName5",
+                LastName = "LastName5",
+                UserName = "user5@gmail.com",
+                NormalizedUserName = "USER5@GMAIL.COM",
+                Email = "user5@gmail.com",
+                NormalizedEmail = "USER5@GMAIL.COM",
+                EmailConfirmed = false,
+                PasswordHash = "AQAAAAEAACcQAAAAEPXMPMbzuDZIKJUN4pBhRWMtf35Q3RN4QOll7UfnTdmfXHEcgswabznBezJmeTMvEw==",
+                SecurityStamp = "WGWJIYDFRG236HXFKGYS7H6QT2DE2LFF",
+                ConcurrencyStamp = "cb54f60f-6282-4416-926c-d1edce844d07",
+                PhoneNumber = "0965889312",
+                Role = "provider",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+            },
+            new User()
+            {
+                Id = "CVc4a6876a-77fb-4ecnne-9c78-a0880286ae33",
+                CreatingTime = default,
+                LastLogin = DateTimeOffset.Now,
                 MiddleName = "MiddleName5",
                 FirstName = "FirstName5",
                 LastName = "LastName5",
