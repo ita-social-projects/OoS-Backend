6de44a083698d00513a961d009be7c2432d302ab
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-06-09 13:06:52 +0300

    Added XML documentation for the new class - DirectionSubDirectionIdsDto.

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/DirectionSubDirectionIdsDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/DirectionSubDirectionIdsDto.cs
@@ -1,7 +1,17 @@
 ﻿namespace OutOfSchool.BusinessLogic.Models.CompetitiveEvent;
 
+/// <summary>
+/// Represents a pairing of direction and sub-direction identifiers.
+/// </summary>
 public class DirectionSubDirectionIdsDto
 {
+    /// <summary>
+    /// Gets or sets the direction identifier.
+    /// </summary>
     public long DirectionId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sub-direction identifier.
+    /// </summary>
     public long SubDirectionId { get; set; }
 }
