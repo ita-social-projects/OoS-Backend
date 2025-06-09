ed8967515a3d4e3e30b3fe65bc7fcf5529841509
Hlib Bondarev <Hlib.Bondarev@gmail.com>
2025-06-09 13:16:39 +0300

    Added DirectionSubDirectionIds to CompetitiveEventDto (#1854)
    * 1) Created a new Dto-class - DirectionSubDirectionIdsDto;
    2) Changed CompetitiveEventDto class:
    - a new property is added - Directionsubdirectionids;
    - added mapping for this new property in the exstension method - ToDto().
    
    * Added XML documentation for the new class - DirectionSubDirectionIdsDto.

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/CompetitiveEventDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/CompetitiveEventDto.cs
@@ -13,6 +13,7 @@ public class CompetitiveEventDto : CompetitiveEventBaseDto
     public string CoverImageId { get; set; } = string.Empty;
     public IList<string> ImageIds { get; set; }
     public CompetitiveEventCoverageDto Coverage { get; set; }
+    public IList<DirectionSubDirectionIdsDto> DirectionSubDirectionIds { get; set; } = [];
 }
 
 public static class CompetitiveEventDtoExtensions
@@ -52,6 +53,13 @@ public static class CompetitiveEventDtoExtensions
             Contacts = model.Contacts?.ToDto(),
             SubDirectionIds = model.SubDirections?.Where(s => !s.IsDeleted).Select(s => s.Id).ToList() ?? [],
             Coverage = model.Coverage?.ToDto(),
+            DirectionSubDirectionIds = model.SubDirections?.Where(s => !s.IsDeleted).Select(
+                s => new DirectionSubDirectionIdsDto
+                {
+                    DirectionId = s.DirectionId,
+                    SubDirectionId = s.Id
+                })
+            .ToList() ?? [],
         };
 
     public static List<CompetitiveEventDto> ToDto(this IEnumerable<OutOfSchool.Services.Models.CompetitiveEvents.CompetitiveEvent> list)
