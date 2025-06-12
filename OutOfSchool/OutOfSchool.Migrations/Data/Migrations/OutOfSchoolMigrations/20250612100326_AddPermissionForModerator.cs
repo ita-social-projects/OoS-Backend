5dc0aeebaed1c3039df17b2330b80c101e4b5a8c
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-06-12 13:04:46 +0300

    Add migration

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20250612100326_AddPermissionForModerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissionForModerator : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "PackedPermissions",
+                value: "MjdaW15mVA==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "PackedPermissions",
+                value: "MjdaW15U");
+        }
+    }
+}
