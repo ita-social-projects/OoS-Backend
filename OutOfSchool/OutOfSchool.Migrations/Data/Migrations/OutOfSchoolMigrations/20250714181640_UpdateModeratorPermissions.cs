e620780ede01cf87d4acf8338fafe2b0f8f9c281
Dmytro M <dmytro.minochkin@gmail.com>
2025-07-14 21:18:43 +0300

    Update admin initializer to include moderators and moderator permissions

---
+++ OutOfSchool/OutOfSchool.Migrations/Data/Migrations/OutOfSchoolMigrations/20250714181640_UpdateModeratorPermissions.cs
@@ -0,0 +1,32 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfSchool.Migrations.Data.Migrations.OutOfSchoolMigrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModeratorPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "PackedPermissions",
+                value: "ZTI3OVpbXmZU");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionsForRoles",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "PackedPermissions",
+                value: "Mjc5WlteZlQ=");
+        }
+    }
+}
