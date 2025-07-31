b61c43c8ffad3e402a96c0a60311b6ccd28443e3
YuriiPatriiuk <yuriipatriuk@gmail.com>
2025-07-31 10:01:23 +0300

    Handle different InstitutionId error

---
+++ OutOfSchool/OutOfSchool.DataAccess/Common/Exceptions/InstitutionIdDifferenceException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OutOfSchool.Services.Common.Exceptions;
+public class InstitutionIdDifferenceException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InstitutionIdDifferenceException"/> class.
+    /// </summary>
+    public InstitutionIdDifferenceException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InstitutionIdDifferenceException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">A message describing the InstitutionId difference error.</param>
+    public InstitutionIdDifferenceException(string? message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InstitutionIdDifferenceException"/> class with a specified 
+    /// error message and an inner exception.
+    /// </summary>
+    /// <param name="message">A message describing the InstitutionId difference error.</param>
+    /// <param name="innerException">The original exception that caused this conflict.</param>
+    public InstitutionIdDifferenceException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
