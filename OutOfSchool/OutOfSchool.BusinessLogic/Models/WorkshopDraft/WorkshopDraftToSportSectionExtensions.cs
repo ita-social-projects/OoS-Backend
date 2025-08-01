708508796080c9693c16f67da6e73ada641db836
Tetyana-K <tanya.kyryk@gmail.com>
2025-08-01 22:10:56 +0300

    update request models and validation; add mapping from WorkshopDraft to SportsSectionPostRequest

---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/WorkshopDraft/WorkshopDraftToSportSectionExtensions.cs
@@ -0,0 +1,89 @@
+﻿using OutOfSchool.Common.Enums;
+using OutOfSchool.SportsRegistryApiClient.Models.Enums;
+using OutOfSchool.SportsRegistryApiClient.Models.Requests;
+
+namespace OutOfSchool.BusinessLogic.Models.WorkshopDraft;
+public static class WorkshopDraftToSportSectionExtensions
+{
+    public static SportsSectionPostRequest ToSportSectionPostRequest(this OutOfSchool.Services.Models.WorkshopDrafts.WorkshopDraft draft)
+    {
+        var content = draft.WorkshopDraftContent;
+        var defaultContact = content.Contacts?.FirstOrDefault(c => c.IsDefault);
+        return new SportsSectionPostRequest
+        {
+            OrganizationCode = draft.Provider.Edrpou,
+            SectionName = content.Title,
+            
+            //SectionSportKindDictIdCode = 1,  не мапити //Ідентифікатор виду спорту з довідника 
+
+            SectionAgeFrom = content.MinAge,
+            SectionAgeTo = content.MaxAge,
+
+            SectionIsInShlyahProject = content.IsChampionPath,
+
+            SectionAddressLocalityDictIdCode = defaultContact?.Address?.CATOTTGId.ToString(), // is it OK?
+            SectionAddressStreet = defaultContact?.Address?.Street,
+            SectionAddressHouse = defaultContact?.Address?.BuildingNumber,
+
+            SectionDescription = string.Join("\n", content.WorkshopDescriptionItems.Select(x => x.Description)),
+
+            SectionRegistrationFlow = content.EnrollmentProcedureDescription,
+
+            SectionPhones = defaultContact?.Phones.Select(p => p.Number).Distinct().ToList()?? [],
+
+            SectionEmail = content.Contacts.Where(c => c.IsDefault).FirstOrDefault().Emails.FirstOrDefault().Address,
+            
+            SectionRegistrationFormUrl = null, // what field does it map from?
+
+            SectionUrl = defaultContact?.SocialNetworks
+            .FirstOrDefault(s => s.Type == SocialNetworkContactType.Website)?.Url,
+
+            SectionFacebookUrl = defaultContact?.SocialNetworks
+            .FirstOrDefault(s => s.Type == SocialNetworkContactType.Facebook)?.Url,
+
+            SectionInstagramUrl = defaultContact?.SocialNetworks
+            .FirstOrDefault(s => s.Type == SocialNetworkContactType.Instagram)?.Url,
+            
+            SectionPracticeFormat = content.FormOfLearning.ToSectionPracticeFormat(), // ONLINE / OFFLINE / HYBRID
+
+            SectionSelectionCriteria = content.CompetitiveSelectionDescription,
+
+            SectionPracticeCost = content.Price,
+
+            SectionMaxStudentsAmount = content.AvailableSeats == uint.MaxValue 
+            ? 1000 : (int)content.AvailableSeats,
+
+            //SectionTitlePhoto = string.IsNullOrEmpty(draft.CoverImageId) // TODO
+            //? null
+            //: $"{baseUrl}{draft.CoverImageId}", //  add full url (baseurl)
+
+            SectionPhotos = draft.Images? // TODO
+            .Select(img => img.ExternalStorageId)
+            .ToList() ?? new(),
+
+            SectionTrainers = [], // TODO: make mapping when teachers will be added to the draft
+
+            SectionPracticePeriodDateFrom = content.StudyPeriodStartDate.ToString("dd:MM"),
+            SectionPracticePeriodDateTo = content.StudyPeriodEndDate.ToString("dd:MM"),
+
+            SectionSchedule = content.DateTimeRanges?
+            .SelectMany(r => r.Workdays.Select(day => new SectionScheduleRequest
+            {
+                SectionScheduleWeekday = Enum.Parse<Weekday>(day.ToString(), ignoreCase: true),
+                SectionScheduleTimeFrom = r.StartTime.ToString("HH:mm:ss"),
+                SectionScheduleTimeTo = r.EndTime.ToString("HH:mm:ss"),
+            }))
+            .ToList() ?? new(),
+        };
+    }
+    public static SectionPracticeFormat ToSectionPracticeFormat(this FormOfLearning formOfLearning)
+    {
+        return formOfLearning switch
+        {
+            FormOfLearning.Online => SectionPracticeFormat.ONLINE,
+            FormOfLearning.Offline => SectionPracticeFormat.OFFLINE,
+            FormOfLearning.Mixed => SectionPracticeFormat.HYBRID,
+            _ => throw new ArgumentOutOfRangeException(nameof(formOfLearning), formOfLearning, "Unsupported learning format. Must be Online, Offline or Mixed."),
+        };
+    }
+}
