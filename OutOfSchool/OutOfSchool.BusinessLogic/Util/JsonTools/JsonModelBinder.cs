b5552707b7c7d1d1e885194c1a540477d8d974ae
Anny <98740947+annygumeniuk@users.noreply.github.com>
2025-08-12 17:01:22 +0300

    Back to previous model binder (#1929)

--- OutOfSchool/OutOfSchool.BusinessLogic/Util/JsonTools/JsonModelBinder.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Util/JsonTools/JsonModelBinder.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.Collections;
 
 namespace OutOfSchool.BusinessLogic.Util.JsonTools;
 
 public class JsonModelBinder : IModelBinder
-{    
+{
     public Task BindModelAsync(ModelBindingContext bindingContext)
     {
         if (bindingContext == null)
@@ -17,76 +16,12 @@ public class JsonModelBinder : IModelBinder
         {
             bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
 
-            // get all values
-            var allValues = valueProviderResult.Values.Where(v => !string.IsNullOrEmpty(v)).ToArray();
-
-            if (allValues.Any())
+            var valueAsString = valueProviderResult.FirstValue;
+            var result = JsonSerializerHelper.Deserialize(valueAsString, bindingContext.ModelType);
+            if (result != null)
             {
-                object result;
-
-                // handle List<long>
-                if (bindingContext.ModelType == typeof(List<long>))
-                {
-                    var longList = new List<long>();
-                    foreach (var value in allValues)
-                    {
-                        if (long.TryParse(value, out var longValue))
-                        {
-                            longList.Add(longValue);
-                        }
-                    }
-                    result = longList;
-                }
-                // handle List<int>
-                else if (bindingContext.ModelType == typeof(List<int>))
-                {
-                    var intList = new List<int>();
-                    foreach (var value in allValues)
-                    {
-                        if (int.TryParse(value, out var intValue))
-                        {
-                            intList.Add(intValue);
-                        }
-                    }
-                    result = intList;
-                }
-                // handle complex object lists
-                else if (bindingContext.ModelType.IsGenericType &&
-                         bindingContext.ModelType.GetGenericTypeDefinition() == typeof(List<>))
-                {
-                    var listType = bindingContext.ModelType;
-                    var elementType = listType.GetGenericArguments()[0];
-                    var list = (IList)Activator.CreateInstance(listType);
-
-                    foreach (var value in allValues)
-                    {
-                        try
-                        {
-                            var deserializedItem = JsonSerializerHelper.Deserialize(value, elementType);
-                            if (deserializedItem != null)
-                            {
-                                list.Add(deserializedItem);
-                            }
-                        }
-                        catch
-                        {
-                            // Skip invalid items, continue with others
-                        }
-                    }
-                    result = list;
-                }
-                
-                else
-                {
-                    var valueAsString = valueProviderResult.FirstValue;
-                    result = JsonSerializerHelper.Deserialize(valueAsString, bindingContext.ModelType);
-                }
-
-                if (result != null)
-                {
-                    bindingContext.Result = ModelBindingResult.Success(result);
-                    return Task.CompletedTask;
-                }
+                bindingContext.Result = ModelBindingResult.Success(result);
+                return Task.CompletedTask;
             }
         }
         return Task.CompletedTask;
