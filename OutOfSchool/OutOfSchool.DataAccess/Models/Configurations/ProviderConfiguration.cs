982740b8718e091134c651e5af9875ba9df416ef
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-08-13 19:38:42 +0300

    Changed back ProviderSectionItem's entity configuration

--- OutOfSchool/OutOfSchool.DataAccess/Models/Configurations/ProviderConfiguration.cs
+++ OutOfSchool/OutOfSchool.DataAccess/Models/Configurations/ProviderConfiguration.cs
@@ -44,10 +44,5 @@ internal class ProviderConfiguration : BusinessEntityWithContactsConfiguration<P
 
         builder.Property(x => x.UpdatedAt)
                 .ValueGeneratedOnAddOrUpdate();
-
-        builder.HasMany(x => x.ProviderSectionItems)
-            .WithOne(x => x.Provider)
-            .HasForeignKey(x => x.ProviderId)
-            .OnDelete(DeleteBehavior.ClientCascade);
     }
 }
