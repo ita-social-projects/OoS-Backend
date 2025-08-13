d2b4b0375372afa2cd1dbecd2dff090128c5d0e1
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2025-08-13 15:17:07 +0300

    Update regex to meet more possible website parts

--- OutOfSchool/OutOfSchool.Common/Constants.cs
+++ OutOfSchool/OutOfSchool.Common/Constants.cs
@@ -28,7 +28,8 @@ public static class Constants
 
     public const string EmailRegexViewModel = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
     
-    public const string SocialNetworkUrlRegex = @"^https:\/\/(?:[A-Za-z0-9-]+\.)+[A-Za-z]{2,}(?:\/[A-Za-z0-9._%\-]+)+\/?$";
+    public const string SocialNetworkUrlRegex =
+        @"^(?<scheme>https?):\/\/(?<host>(?:[A-Za-z0-9-]+\.)+[A-Za-z]{2,})(?::(?<port>\d{1,5}))?(?<path>\/[A-Za-z0-9._~!$&'()*+,;=:@%\-\/]*)?(?:\?(?<query>[A-Za-z0-9._~!$&'()*+,;=:@%\/?\-]*))?(?:\#(?<fragment>[A-Za-z0-9._~!$&'()*+,;=:@%\/?\-]*))?$";
 
 
     public const string PasswordValidationErrorMessage = "Password must be at least 8 characters long, including upper and lower case letters, digits and special characters (@$!%*?&)";
