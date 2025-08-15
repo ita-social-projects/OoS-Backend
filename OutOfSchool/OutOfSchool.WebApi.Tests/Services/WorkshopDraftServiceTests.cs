e54cc75c52da30dc6d257c544f90b42af2218532
Oleksandr Maslov <163436443+synapsetron@users.noreply.github.com>
2025-08-15 14:44:26 +0300

    fix (tests): fix missed   sportsRegistryApiService mock in draft tests
    # Conflicts:
    #	OutOfSchool/OutOfSchool.WebApi.Tests/Services/SensitiveWorkshopDraftServiceTests.cs
    #	OutOfSchool/OutOfSchool.WebApi.Tests/Services/WorkshopDraftServiceTests.cs

--- OutOfSchool/OutOfSchool.WebApi.Tests/Services/WorkshopDraftServiceTests.cs
+++ OutOfSchool/OutOfSchool.WebApi.Tests/Services/WorkshopDraftServiceTests.cs
@@ -35,6 +35,12 @@ using OutOfSchool.Services.Models.WorkshopDrafts;
 using OutOfSchool.Services.Repository.Api;
 using OutOfSchool.Tests.Common;
 using OutOfSchool.Tests.Common.TestDataGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using OutOfSchool.SportsRegistryApiClient.Interfaces;
 
 namespace OutOfSchool.WebApi.Tests.Services;
 
@@ -54,6 +60,7 @@ public class WorkshopDraftServiceTests
     private Mock<ICodeficatorRepository> codeficatorRepositoryMoq;
     private Mock<IChangesLogService> changesLogServiceMock;
     private Mock<IOptions<InstitutionOptions>> institutionOptionsMock;
+    private Mock<ISportsRegistryApiService> sportsRegistryApiService;
 
     private string userId;
 
@@ -71,7 +78,7 @@ public class WorkshopDraftServiceTests
         codeficatorRepositoryMoq = new Mock<ICodeficatorRepository>();
         languageServiceMoq = new Mock<ILanguageService>();
         changesLogServiceMock = new Mock<IChangesLogService>();
-
+        sportsRegistryApiService = new Mock<ISportsRegistryApiService>();
         institutionHierarchyRepositoryMoq.Setup(x => x.GetByIdWithDetails(
             It.IsAny<Guid>(),
             It.IsAny<string>(),
@@ -101,6 +108,7 @@ public class WorkshopDraftServiceTests
         userId = "someUserId";
         service = new WorkshopDraftService(
                    logger.Object,
+                   sportsRegistryApiService.Object,
                    languageServiceMoq.Object,
                    workshopDraftRepoMoq.Object,
                    workshopDraftImagesService.Object,
@@ -968,6 +976,7 @@ public class WorkshopDraftServiceTests
 
         var service = new WorkshopDraftService(
                    logger.Object,
+                   sportsRegistryApiService.Object,
                    new Mock<ILanguageService>().Object,
                    workshopDraftRepoMoq.Object,
                    workshopDraftImagesService.Object,
