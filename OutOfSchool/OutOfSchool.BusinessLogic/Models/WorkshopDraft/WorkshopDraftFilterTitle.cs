0308860ec6fbb3bd5e2a741d1a981570a56aa41a
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-08-26 22:58:43 +0300

    Added new title filters

---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/WorkshopDraft/WorkshopDraftFilterTitle.cs
@@ -0,0 +1,8 @@
+﻿namespace OutOfSchool.BusinessLogic.Models.WorkshopDraft;
+public class WorkshopDraftFilterTitle : ExcludeIdFilter
+{
+    /// <summary>
+    /// Gets all entries by entered substring from current workshop drafts
+    /// </summary>
+    public string SearchText { get; set; } = string.Empty;
+}
