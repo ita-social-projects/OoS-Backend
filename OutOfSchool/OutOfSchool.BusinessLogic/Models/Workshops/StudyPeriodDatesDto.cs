f6b2e3879e4508511f0a37528a60ca554511c0c5
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-10-02 01:06:14 +0300

    Some fixes

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/StudyPeriodDatesDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/StudyPeriodDatesDto.cs
@@ -22,8 +22,8 @@ public class StudyPeriodDatesDto : IValidatableObject
             yield return new ValidationResult("Study period end date is required. The data type in the 'EndDate' field must be in the format YYYY-MM-DD.",
                 new[] { nameof(EndDate) });
 
-        if (StartDate > EndDate)
-            yield return new ValidationResult("End date must be greater than or equal to start date.", new[] { nameof(StartDate), nameof(EndDate) });
+        if (StartDate != default && EndDate != default && StartDate > EndDate)
+            yield return new ValidationResult("The end date cannot be earlier than the start date.", new[] { nameof(StartDate), nameof(EndDate) });
     }
 }
 
