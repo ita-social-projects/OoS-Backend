ca99940f9af3f9f0f6debf31403cab50215c6483
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-10-08 23:08:52 +0300

    Added TempSave feature to CompetitiveEvent: 1) Added TempSave dtos for CompetitiveEvent - CompetitiveEventAboutDto, CompetitiveEventDescriptionDto and CompetitiveEventContactsDto. 2) Added CompetitiveEventTempSaveController, inherited from TempSaveController<CompetitiveEventAboutDto>.

---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/CompetitiveEvent/TempSave/CompetitiveEventContactsDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using OutOfSchool.BusinessLogic.Models.ContactInfo;
+using OutOfSchool.BusinessLogic.Util.JsonTools;
+
+namespace OutOfSchool.BusinessLogic.Models.CompetitiveEvent.TempSave;
+
+public class CompetitiveEventContactsDto : CompetitiveEventDescriptionDto
+{
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
+    public List<ContactsDto> Contacts { get; set; }
+
+    public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        // Run validations from CompetitiveEventBaseDto
+        foreach (var error in base.Validate(validationContext))
+            yield return error;
+    }
+}
---
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/V1/CompetitiveEventTempSaveController.cs
@@ -0,0 +1,17 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using OutOfSchool.BusinessLogic.Models.CompetitiveEvent.TempSave;
+using OutOfSchool.BusinessLogic.Services.TempSave;
+
+namespace OutOfSchool.WebApi.Controllers.V1;
+
+/// <summary>The abstract controller with operations for temporarily saving a CompetitiveEvent in cache when it is created.</summary>
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]/[action]")]
+[HasPermission(Permissions.CompetitiveEventAddNew)]
+
+public class CompetitiveEventTempSaveController(ITempSaveService<CompetitiveEventAboutDto> tempSaveService)
+    : TempSaveController<CompetitiveEventAboutDto>(tempSaveService)
+{
+}
