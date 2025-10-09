baba9c4c4beca4ca540249f08a6629e3933bf50b
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-10-09 17:00:22 +0300

    Removed date comparison in validation

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/StudyPeriodDatesDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/StudyPeriodDatesDto.cs
@@ -21,9 +21,6 @@ public class StudyPeriodDatesDto : IValidatableObject
         if (EndDate == default)
             yield return new ValidationResult("Study period end date is required. The data type in the 'EndDate' field must be in the format YYYY-MM-DD.",
                 new[] { nameof(EndDate) });
-
-        if (StartDate != default && EndDate != default && StartDate > EndDate)
-            yield return new ValidationResult("The end date cannot be earlier than the start date.", new[] { nameof(StartDate), nameof(EndDate) });
     }
 }
 
