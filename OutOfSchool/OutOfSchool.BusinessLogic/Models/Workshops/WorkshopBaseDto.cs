cb6585d4b383594b9396a73253729d864d738d02
Земляков Сергій <seriyzemlyakov@gmail.com>
2025-10-22 01:35:08 +0300

    Added MaxDecimalPlacesAttribute and validation for price

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/WorkshopBaseDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/WorkshopBaseDto.cs
@@ -47,6 +47,7 @@ public class WorkshopBaseDto : IValidatableObject, IHasContactsDto<Workshop>
     public bool IsPaid { get; set; } = false;
 
     [Column(TypeName = "decimal(18,2)")]
+    [MaxDecimalPlaces(2, ErrorMessage = "Price field must have maximum two decimal places.")]
     [Range(0, 100000, ErrorMessage = "Field value should be in a range from 0 to 100 000")]
     public decimal? Price { get; set; } = default;
 
---
+++ OutOfSchool/OutOfSchool.BusinessLogic/Validators/MaxDecimalPlacesAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfSchool.BusinessLogic.Validators;
+public class MaxDecimalPlacesAttribute : ValidationAttribute
+{
+    private readonly int maxDecimalPlaces;
+
+    public MaxDecimalPlacesAttribute(int maxDecimalPlaces)
+    {
+        this.maxDecimalPlaces = maxDecimalPlaces;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is decimal d)
+        {
+            if (GetDecimalPlaces(d) > maxDecimalPlaces)
+                return new ValidationResult(ErrorMessage ?? $"The field must not have more than {maxDecimalPlaces} decimal places.");
+        }
+
+        return ValidationResult.Success;
+    }
+
+    private static int GetDecimalPlaces(decimal d)
+    {
+        int[] bits = decimal.GetBits(d);
+        return (bits[3] & 0x00FF0000) >> 16;
+    }
+}
