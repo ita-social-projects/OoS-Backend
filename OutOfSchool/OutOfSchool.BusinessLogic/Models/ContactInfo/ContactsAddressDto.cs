25bd9407ac70ca9377a4fa09397a1980d214fe1e
synapsetron <aleksandr.maslov.job@gmail.com>
2025-10-27 15:58:29 +0200

    small changes

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/ContactInfo/ContactsAddressDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/ContactInfo/ContactsAddressDto.cs
@@ -21,7 +21,7 @@ public sealed class ContactsAddressDto : IContentComparable<ContactsAddress>, IE
     [MinLength(1)]
     [MaxLength(15)]
     [MustContain(RequiredCharacterType.Digit, ErrorMessage = "Building number must contain at least one number.")]
-    [RegularExpression(@"^[0-9А-Яа-яЇїІіЄєҐґA-Za-z\-\/]+$", ErrorMessage = "Only Cyrillic or Latin letters, digits, '-' and '/' are allowed, without spaces.")]
+    [RegularExpression(@"^[0-9]+[0-9А-Яа-яЇїІіЄєҐґA-Za-z\-\/]*$", ErrorMessage = "Building number must start with a digit and may only contain Cyrillic or Latin letters, digits, '-' and '/', without spaces.")]
     public string BuildingNumber { get; set; } = string.Empty;
 
     public double Latitude { get; set; }
