513ee876f071c693e15f5405ef5589f28169d775
HlibBondarev <Hlib.Bondarev@gmail.com>
2025-11-13 16:12:56 +0200

    Fixed the problem mentioned by SonarCloud.

--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/TempSave/WorkshopDescriptionDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/TempSave/WorkshopDescriptionDto.cs
@@ -72,8 +72,12 @@ public class WorkshopDescriptionDto : WorkshopRequiredPropertiesDto
                 }
 
                 string trimmedKeyword = keyword?.Trim();
-                cleanedKeywords.Add(trimmedKeyword);
-                totalLength += trimmedKeyword.Length;
+
+                if ((trimmedKeyword ?? string.Empty).Length > 0)
+                {
+                    cleanedKeywords.Add(trimmedKeyword);
+                    totalLength += trimmedKeyword.Length;
+                }
 
                 // Check 2: Single keyword max length
                 if (trimmedKeyword.Length > Constants.MaxLengthOfOneKeyword)
--- OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/WorkshopBaseDto.cs
+++ OutOfSchool/OutOfSchool.BusinessLogic/Models/Workshops/WorkshopBaseDto.cs
@@ -238,8 +238,12 @@ public class WorkshopBaseDto : IValidatableObject, IHasContactsDto<Workshop>
                 }
 
                 string trimmedKeyword = keyword?.Trim();
-                cleanedKeywords.Add(trimmedKeyword);
-                totalLength += trimmedKeyword.Length;
+
+                if ((trimmedKeyword ?? string.Empty).Length > 0)
+                {
+                    cleanedKeywords.Add(trimmedKeyword);
+                    totalLength += trimmedKeyword.Length;
+                }
 
                 // Check 2: Single keyword max length
                 if (trimmedKeyword.Length > Constants.MaxLengthOfOneKeyword)
